dd4c15cd3ba558e65e0f744257c0c148a15e24cb
Sam El-Husseini <samelh@microsoft.com>
2017-04-06 16:19:04 -0700

    Dup sizeof in pins

--- libs/core/i2c.ts
+++ libs/core/i2c.ts
@@ -19,27 +19,4 @@ namespace pins {
         buf.setNumber(format, 0, value)
         pins.i2cWriteBuffer(address, buf, repeated)
     }
-
-    /**
-     * Get the size in bytes of specified number format.
-     */
-    //%
-    export function sizeOf(format: NumberFormat) {
-        switch (format) {
-            case NumberFormat.Int8LE:
-            case NumberFormat.UInt8LE:
-            case NumberFormat.Int8BE:
-            case NumberFormat.UInt8BE:
-                return 1;
-            case NumberFormat.Int16LE:
-            case NumberFormat.UInt16LE:
-            case NumberFormat.Int16BE:
-            case NumberFormat.UInt16BE:
-                return 2;
-            case NumberFormat.Int32LE:
-            case NumberFormat.Int32BE:
-                return 4;
-        }
-        return 0;
-    }    
 }
