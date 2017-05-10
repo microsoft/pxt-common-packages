f5dff7ef6c4ed7eeb17319c84dbe941dceb7fa0f
ganicke <v-ganick@microsoft.com>
2017-05-10 10:35:14 -0700

    Change 'loudness' to 'sound' for microphone ids
    * Doc path and block ids change to reflect the new function names.
    
    Change the enum for 'LoudnessCondition' to 'SoundCondition' also???

--- libs/microphone/shims.d.ts
+++ libs/microphone/shims.d.ts
@@ -10,15 +10,15 @@ declare namespace input {
      * Registers an event that runs when particular lighting conditions (dark, bright) are encountered.
      * @param condition the condition that event triggers on
      */
-    //% help=input/on-loudness-condition-changed weight=97
-    //% blockId=input_on_loudness_condition_changed block="on sound %condition"
+    //% help=input/on-sound-condition-changed weight=97
+    //% blockId=input_on_sound_condition_changed block="on sound %condition"
     //% parts="microphone" blockGap=8 shim=input::onSoundConditionChanged
     function onSoundConditionChanged(condition: LoudnessCondition, handler: () => void): void;
 
     /**
      * Reads the loudness through the microphone from 0 (silent) to 255 (very loud)
      */
-    //% help=input/loudness weight=75
+    //% help=input/sound-level weight=75
     //% blockId=device_get_sound_level block="sound level" blockGap=8
     //% parts="microphone" shim=input::soundLevel
     function soundLevel(): number;
