4b03a3354dd34ce83cedcef1c7bf853de7cc07c4
Peli de Halleux <jhalleux@microsoft.com>
2017-05-11 14:00:17 -0700

    updated pink color

--- libs/light/neopixel.ts
+++ libs/light/neopixel.ts
@@ -18,8 +18,8 @@ enum Colors {
     Violet = 0x8a2be2,
     //% block=purple blockIdentity=light.colors
     Purple = 0xFF00FF,
-    //% block=purple blockIdentity=light.colors
-    Pink = 0xFF69B4,
+    //% block=pink blockIdentity=light.colors
+    Pink = 0xFFC0CB,
     //% block=white blockIdentity=light.colors
     White = 0xFFFFFF,
     //% block=black  blockIdentity=light.colors
