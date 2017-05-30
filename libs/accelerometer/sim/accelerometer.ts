b793cd773f9b6fa13cb0235e0be5bca39692c413
Richard Knoll <riknoll@microsoft.com>
2017-05-30 13:19:26 -0700

    Removing CODAL enum

--- libs/accelerometer/sim/accelerometer.ts
+++ libs/accelerometer/sim/accelerometer.ts
@@ -1,60 +1,13 @@
-const enum CODAL {
-    REDLED = 13,
-    NEOPIXEL_PIN = 17,
-    SLIDE_SWITCH_PIN = 21,
-    LEFT_BUTTON = 4,
-    RIGHT_BUTTON = 19,
-    //LIGHT_SENSOR = A5,
-    LIS3DH_CS = 8,
-    //THERMISTOR_PIN = A0,
-    BUZZER = 5,
-
-    ID_BUTTON_A = 1,
-    ID_BUTTON_B = 2,
-    ID_BUTTON_RESET = 3,
-    ID_ACCELEROMETER = 4,
-    ID_BUTTON_AB = 26,
-    ID_GESTURE = 27,
-    ID_THERMOMETER = 28,
-
-    CAPSENSE_SHARED = 30,
-    ACCELEROMETER_EVT_DATA_UPDATE = 1,
-    ACCELEROMETER_EVT_NONE = 0,
-    ACCELEROMETER_EVT_TILT_UP = 1,
-    ACCELEROMETER_EVT_TILT_DOWN = 2,
-    ACCELEROMETER_EVT_TILT_LEFT = 3,
-    ACCELEROMETER_EVT_TILT_RIGHT = 4,
-    ACCELEROMETER_EVT_FACE_UP = 5,
-    ACCELEROMETER_EVT_FACE_DOWN = 6,
-    ACCELEROMETER_EVT_FREEFALL = 7,
-    ACCELEROMETER_EVT_3G = 8,
-    ACCELEROMETER_EVT_6G = 9,
-    ACCELEROMETER_EVT_8G = 10,
-    ACCELEROMETER_EVT_SHAKE = 11,
-    ACCELEROMETER_REST_TOLERANCE = 200,
-    ACCELEROMETER_TILT_TOLERANCE = 200,
-    ACCELEROMETER_FREEFALL_TOLERANCE = 400,
-    ACCELEROMETER_SHAKE_TOLERANCE = 400,
-    ACCELEROMETER_3G_TOLERANCE = 3072,
-    ACCELEROMETER_6G_TOLERANCE = 6144,
-    ACCELEROMETER_8G_TOLERANCE = 8192,
-    ACCELEROMETER_GESTURE_DAMPING = 5,
-    ACCELEROMETER_SHAKE_DAMPING = 10,
-    ACCELEROMETER_SHAKE_RTX = 30,
-    ACCELEROMETER_SHAKE_COUNT_THRESHOLD = 4
-}
-
-
 namespace pxsim.input {
     export function onGesture(gesture: number, handler: RefAction) {
         let b = accelerometer();
         b.accelerometer.activate();
 
-        if (gesture == CODAL.ACCELEROMETER_EVT_SHAKE && !b.useShake) {
+        if (gesture == DAL.ACCELEROMETER_EVT_SHAKE && !b.useShake) {
             b.useShake = true;
             runtime.queueDisplayUpdate();
         }
-        pxtcore.registerWithDal(CODAL.ID_GESTURE, gesture, handler);
+        pxtcore.registerWithDal(DAL.DEVICE_ID_GESTURE, gesture, handler);
     }
 
     export function rotation(kind: number): number {
@@ -159,7 +112,7 @@ namespace pxsim {
         public sampleRange = 2;
 
         constructor(public runtime: Runtime) {
-            this.id = CODAL.ID_ACCELEROMETER;
+            this.id = DAL.DEVICE_ID_ACCELEROMETER;
         }
 
         public setSampleRange(range: number) {
@@ -188,7 +141,7 @@ namespace pxsim {
             this.updateGesture();
 
             // Indicate that a new sample is available
-            board().bus.queue(this.id, CODAL.ACCELEROMETER_EVT_DATA_UPDATE)
+            board().bus.queue(this.id, DAL.ACCELEROMETER_EVT_DATA_UPDATE)
         }
 
         public instantaneousAccelerationSquared() {
@@ -212,25 +165,25 @@ namespace pxsim {
             //
             // If we see enough zero crossings in succession (MICROBIT_ACCELEROMETER_SHAKE_COUNT_THRESHOLD), then we decide that the device
             // has been shaken.
-            if ((this.getX() < -CODAL.ACCELEROMETER_SHAKE_TOLERANCE && this.shake.x) || (this.getX() > CODAL.ACCELEROMETER_SHAKE_TOLERANCE && !this.shake.x)) {
+            if ((this.getX() < -DAL.ACCELEROMETER_SHAKE_TOLERANCE && this.shake.x) || (this.getX() > DAL.ACCELEROMETER_SHAKE_TOLERANCE && !this.shake.x)) {
                 shakeDetected = true;
                 this.shake.x = !this.shake.x;
             }
 
-            if ((this.getY() < -CODAL.ACCELEROMETER_SHAKE_TOLERANCE && this.shake.y) || (this.getY() > CODAL.ACCELEROMETER_SHAKE_TOLERANCE && !this.shake.y)) {
+            if ((this.getY() < -DAL.ACCELEROMETER_SHAKE_TOLERANCE && this.shake.y) || (this.getY() > DAL.ACCELEROMETER_SHAKE_TOLERANCE && !this.shake.y)) {
                 shakeDetected = true;
                 this.shake.y = !this.shake.y;
             }
 
-            if ((this.getZ() < -CODAL.ACCELEROMETER_SHAKE_TOLERANCE && this.shake.z) || (this.getZ() > CODAL.ACCELEROMETER_SHAKE_TOLERANCE && !this.shake.z)) {
+            if ((this.getZ() < -DAL.ACCELEROMETER_SHAKE_TOLERANCE && this.shake.z) || (this.getZ() > DAL.ACCELEROMETER_SHAKE_TOLERANCE && !this.shake.z)) {
                 shakeDetected = true;
                 this.shake.z = !this.shake.z;
             }
 
-            if (shakeDetected && this.shake.count < CODAL.ACCELEROMETER_SHAKE_COUNT_THRESHOLD && ++this.shake.count == CODAL.ACCELEROMETER_SHAKE_COUNT_THRESHOLD)
+            if (shakeDetected && this.shake.count < DAL.ACCELEROMETER_SHAKE_COUNT_THRESHOLD && ++this.shake.count == DAL.ACCELEROMETER_SHAKE_COUNT_THRESHOLD)
                 this.shake.shaken = 1;
 
-            if (++this.shake.timer >= CODAL.ACCELEROMETER_SHAKE_DAMPING) {
+            if (++this.shake.timer >= DAL.ACCELEROMETER_SHAKE_DAMPING) {
                 this.shake.timer = 0;
                 if (this.shake.count > 0) {
                     if (--this.shake.count == 0)
@@ -239,40 +192,40 @@ namespace pxsim {
             }
 
             if (this.shake.shaken)
-                return CODAL.ACCELEROMETER_EVT_SHAKE;
+                return DAL.ACCELEROMETER_EVT_SHAKE;
 
             let sq = (n: number) => n * n
 
-            if (force < sq(CODAL.ACCELEROMETER_FREEFALL_TOLERANCE))
-                return CODAL.ACCELEROMETER_EVT_FREEFALL;
+            if (force < sq(DAL.ACCELEROMETER_FREEFALL_TOLERANCE))
+                return DAL.ACCELEROMETER_EVT_FREEFALL;
 
-            if (force > sq(CODAL.ACCELEROMETER_3G_TOLERANCE))
-                return CODAL.ACCELEROMETER_EVT_3G;
+            if (force > sq(DAL.ACCELEROMETER_3G_TOLERANCE))
+                return DAL.ACCELEROMETER_EVT_3G;
 
-            if (force > sq(CODAL.ACCELEROMETER_6G_TOLERANCE))
-                return CODAL.ACCELEROMETER_EVT_6G;
+            if (force > sq(DAL.ACCELEROMETER_6G_TOLERANCE))
+                return DAL.ACCELEROMETER_EVT_6G;
 
-            if (force > sq(CODAL.ACCELEROMETER_8G_TOLERANCE))
-                return CODAL.ACCELEROMETER_EVT_8G;
+            if (force > sq(DAL.ACCELEROMETER_8G_TOLERANCE))
+                return DAL.ACCELEROMETER_EVT_8G;
 
             // Determine our posture.
-            if (this.getX() < (-1000 + CODAL.ACCELEROMETER_TILT_TOLERANCE))
-                return CODAL.ACCELEROMETER_EVT_TILT_LEFT;
+            if (this.getX() < (-1000 + DAL.ACCELEROMETER_TILT_TOLERANCE))
+                return DAL.ACCELEROMETER_EVT_TILT_LEFT;
 
-            if (this.getX() > (1000 - CODAL.ACCELEROMETER_TILT_TOLERANCE))
-                return CODAL.ACCELEROMETER_EVT_TILT_RIGHT;
+            if (this.getX() > (1000 - DAL.ACCELEROMETER_TILT_TOLERANCE))
+                return DAL.ACCELEROMETER_EVT_TILT_RIGHT;
 
-            if (this.getY() < (-1000 + CODAL.ACCELEROMETER_TILT_TOLERANCE))
-                return CODAL.ACCELEROMETER_EVT_TILT_DOWN;
+            if (this.getY() < (-1000 + DAL.ACCELEROMETER_TILT_TOLERANCE))
+                return DAL.ACCELEROMETER_EVT_TILT_DOWN;
 
-            if (this.getY() > (1000 - CODAL.ACCELEROMETER_TILT_TOLERANCE))
-                return CODAL.ACCELEROMETER_EVT_TILT_UP;
+            if (this.getY() > (1000 - DAL.ACCELEROMETER_TILT_TOLERANCE))
+                return DAL.ACCELEROMETER_EVT_TILT_UP;
 
-            if (this.getZ() < (-1000 + CODAL.ACCELEROMETER_TILT_TOLERANCE))
-                return CODAL.ACCELEROMETER_EVT_FACE_UP;
+            if (this.getZ() < (-1000 + DAL.ACCELEROMETER_TILT_TOLERANCE))
+                return DAL.ACCELEROMETER_EVT_FACE_UP;
 
-            if (this.getZ() > (1000 - CODAL.ACCELEROMETER_TILT_TOLERANCE))
-                return CODAL.ACCELEROMETER_EVT_FACE_DOWN;
+            if (this.getZ() > (1000 - DAL.ACCELEROMETER_TILT_TOLERANCE))
+                return DAL.ACCELEROMETER_EVT_FACE_DOWN;
 
             return 0;
         }
@@ -283,7 +236,7 @@ namespace pxsim {
 
             // Perform some low pass filtering to reduce jitter from any detected effects
             if (g == this.currentGesture) {
-                if (this.sigma < CODAL.ACCELEROMETER_GESTURE_DAMPING)
+                if (this.sigma < DAL.ACCELEROMETER_GESTURE_DAMPING)
                     this.sigma++;
             }
             else {
@@ -292,9 +245,9 @@ namespace pxsim {
             }
 
             // If we've reached threshold, update our record and raise the relevant event...
-            if (this.currentGesture != this.lastGesture && this.sigma >= CODAL.ACCELEROMETER_GESTURE_DAMPING) {
+            if (this.currentGesture != this.lastGesture && this.sigma >= DAL.ACCELEROMETER_GESTURE_DAMPING) {
                 this.lastGesture = this.currentGesture;
-                board().bus.queue(CODAL.ID_GESTURE, this.lastGesture);
+                board().bus.queue(DAL.DEVICE_ID_GESTURE, this.lastGesture);
             }
         }
 
