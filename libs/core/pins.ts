ce52476a343132afd7343982b2e47a3ad0f9777d
Michal Moskal <michal@moskal.me>
2017-06-18 20:22:34 +0100

    Implement msgpack for sequances of numbers

--- libs/core/pins.ts
+++ libs/core/pins.ts
@@ -128,6 +128,116 @@ namespace pins {
     }
 }
 
+// see http://msgpack.org/ for the spec
+// it currently only implements numbers and their sequances
+// once we handle any type and typeof expressions we can do more
+
+namespace msgpack {
+    function tagFormat(tag: number) {
+        switch (tag) {
+            case 0xCB: return NumberFormat.Float64BE
+            case 0xCC: return NumberFormat.UInt8BE
+            case 0xCD: return NumberFormat.UInt16BE
+            case 0xCE: return NumberFormat.UInt32BE
+            case 0xD0: return NumberFormat.Int8BE
+            case 0xD1: return NumberFormat.Int16BE
+            case 0xD2: return NumberFormat.Int32BE
+            default:
+                return null
+        }
+    }
+
+    function packNumberCore(buf: Buffer, offset: number, num: number) {
+        let tag = 0xCB
+        if (num == (num << 0) || num == (num >>> 0)) {
+            if (-31 <= num && num <= 127) {
+                if (buf) buf[offset] = num
+                return 1
+            } else if (0 <= num) {
+                if (num <= 0xff) {
+                    tag = 0xCC
+                } else if (num <= 0xffff) {
+                    tag = 0xCD
+                } else {
+                    tag = 0xCE
+                }
+            } else {
+                if (-0x7f <= num) {
+                    tag = 0xD0
+                } else if (-0x7fff <= num) {
+                    tag = 0xD1
+                } else {
+                    tag = 0xD2
+                }
+            }
+        }
+        let fmt = tagFormat(tag)
+        if (buf) {
+            buf[offset] = tag
+            buf.setNumber(fmt, offset + 1, num)
+        }
+        return pins.sizeOf(fmt) + 1
+    }
+
+    export function unpackNumberArray(buf: Buffer, offset = 0) {
+        let res: number[] = []
+
+        while (offset < buf.length) {
+            let fmt = tagFormat(buf[offset++])
+            if (fmt === null) {
+                let v = buf.getNumber(NumberFormat.Int8BE, offset - 1)
+                if (-31 <= v && v <= 127)
+                    res.push(v)
+                else
+                    return null
+            } else {
+                res.push(buf.getNumber(fmt, offset))
+                offset += pins.sizeOf(fmt)
+            }
+        }
+
+        return res
+    }
+
+    export function packNumberArray(nums: number[]) {
+        let off = 0
+        for (let n of nums) {
+            off += packNumberCore(null, off, n)
+        }
+        let buf = pins.createBuffer(off)
+        off = 0
+        for (let n of nums) {
+            off += packNumberCore(buf, off, n)
+        }
+        return buf
+    }
+
+    export function test() {
+        let t = (a: number[]) => {
+            serial.writeValue("test", a.length)
+            serial.writeLine("test2: " + a[0] + " " + a[1])
+            let pk = packNumberArray(a)
+            let b = unpackNumberArray(pk)
+            control.assert(a.length == b.length, 42)
+            for (let i = 0; i < a.length; ++i)
+                control.assert(a[i] == b[i], 43)
+        }
+        t([])
+        t([0, 1, 2, 3])
+        t([0, -1, -2, -3])
+        t([3.5])
+        t([0xffff])
+        t([0xffffffff])
+        for (let i = 0; i < 1e20; ++i) {
+            t([i])
+            t([-i])
+            if (i > 1000) {
+                i = Math.floor(i * 1.01)
+            }
+        }
+    }
+}
+
 
 //% noRefCounting fixedInstances
 interface DigitalPin {
