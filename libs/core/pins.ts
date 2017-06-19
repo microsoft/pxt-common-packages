86675214635fcf2577c8973b30ef64fd163cb4eb
Michał Moskal <michal@moskal.me>
2017-06-19 21:12:03 +0100

    Implement msgpack for sequances of numbers (#92)
    * Implement msgpack for sequances of numbers
    
    * Add docs, remove test

--- libs/core/pins.ts
+++ libs/core/pins.ts
@@ -128,6 +128,98 @@ namespace pins {
     }
 }
 
+// see http://msgpack.org/ for the spec
+// it currently only implements numbers and their sequances
+// once we handle any type and typeof expressions we can do more
+
+namespace msgpack {
+    function tagFormat(tag: number) {
+        switch (tag) {
+            case 0xCB: return NumberFormat.Float64BE
+            case 0xCC: return NumberFormat.UInt8BE
+            case 0xCD: return NumberFormat.UInt16BE
+            case 0xCE: return NumberFormat.UInt32BE
+            case 0xD0: return NumberFormat.Int8BE
+            case 0xD1: return NumberFormat.Int16BE
+            case 0xD2: return NumberFormat.Int32BE
+            default:
+                return null
+        }
+    }
+
+    function packNumberCore(buf: Buffer, offset: number, num: number) {
+        let tag = 0xCB
+        if (num == (num << 0) || num == (num >>> 0)) {
+            if (-31 <= num && num <= 127) {
+                if (buf) buf[offset] = num
+                return 1
+            } else if (0 <= num) {
+                if (num <= 0xff) {
+                    tag = 0xCC
+                } else if (num <= 0xffff) {
+                    tag = 0xCD
+                } else {
+                    tag = 0xCE
+                }
+            } else {
+                if (-0x7f <= num) {
+                    tag = 0xD0
+                } else if (-0x7fff <= num) {
+                    tag = 0xD1
+                } else {
+                    tag = 0xD2
+                }
+            }
+        }
+        let fmt = tagFormat(tag)
+        if (buf) {
+            buf[offset] = tag
+            buf.setNumber(fmt, offset + 1, num)
+        }
+        return pins.sizeOf(fmt) + 1
+    }
+
+    /**
+     * Unpacks a buffer into a number array.
+     */
+    export function unpackNumberArray(buf: Buffer, offset = 0) {
+        let res: number[] = []
+
+        while (offset < buf.length) {
+            let fmt = tagFormat(buf[offset++])
+            if (fmt === null) {
+                let v = buf.getNumber(NumberFormat.Int8BE, offset - 1)
+                if (-31 <= v && v <= 127)
+                    res.push(v)
+                else
+                    return null
+            } else {
+                res.push(buf.getNumber(fmt, offset))
+                offset += pins.sizeOf(fmt)
+            }
+        }
+
+        return res
+    }
+
+    /**
+     * Pack a number array into a buffer.
+     * @param nums the numbers to be packed
+     */
+    export function packNumberArray(nums: number[]) {
+        let off = 0
+        for (let n of nums) {
+            off += packNumberCore(null, off, n)
+        }
+        let buf = pins.createBuffer(off)
+        off = 0
+        for (let n of nums) {
+            off += packNumberCore(buf, off, n)
+        }
+        return buf
+    }
+}
+
 
 //% noRefCounting fixedInstances
 interface DigitalPin {
