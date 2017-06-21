cc22238535c54c14c5bbe1508215698536759e32
Majeed <majeed@umd.edu>
2017-06-21 15:32:22 -0700

    added "hidden" streaming of accelerometer data in the main loop.

--- libs/core/loops.cpp
+++ libs/core/loops.cpp
@@ -1,33 +1,80 @@
 #include "pxt.h"
+#include "DeviceSystemTimer.h"
+#include "LIS3DH.h"
+#include "string.h"
+// #include "helper.h"
 
-bool using_gesture = true;
-int prevTime1 = 0
-int prevTime2 = 0
-int time1 = 0
-int time2 = 0
-int sampleRate1 = 30; //33fps
-int sampleRate2 = 20; //50fps
+namespace pxt {
 
-namespace loops {    
+// Wrapper classes
+
+class PredictAccel {
+  public:
+    DeviceI2C i2c; // note that this is different pins than io->i2c
+    DevicePin int1;
+    LIS3DH acc;
+    PredictAccel()
+        : i2c((PinName)PIN_ACCELEROMETER_SDA, (PinName)PIN_ACCELEROMETER_SCL),
+          INIT_PIN(int1, PIN_ACCELEROMETER_INT), //
+          acc(i2c, int1, LIS3DH_DEFAULT_ADDR, DEVICE_ID_ACCELEROMETER, NORTH_EAST_UP) //
+    {
+        acc.init();        
+    }
+};
+SINGLETON(PredictAccel);
+}
+
+namespace loops {
+  bool using_gesture = true;
+  time_t prevTime1 = 0;
+  time_t prevTime2 = 0;
+  time_t curTime = 0;
+  time_t sampleRate1 = 30; // 33fps
+  time_t sampleRate2 = 20; // 50fps
+
     void forever_stub(void *a) {
       while (true) {
-        time = system_timer_current_time();
+        curTime = system_timer_current_time();
 
-        if (using_gesture && time1 - prevTime1 >= sampleRate1 || time1 < prevTime1) {
-          // int x = input.acceleration(Dimension::X);
-          // int y = input.acceleration(Dimension::Y);
-          // int z = input.acceleration(Dimension::Z);
+        if (using_gesture && (curTime - prevTime1 >= sampleRate1 || curTime < prevTime1)) {
+          int x = getPredictAccel()->acc.getX();
+          int y = getPredictAccel()->acc.getY();
+          int z = getPredictAccel()->acc.getZ();
 
           // call the predict function
           // stream the raw data
           // serial.writeString(numops.toString(x) + " " + numops.toString(y) + " " + numops.toString(z) + "\n");
 
-          prevTime1 = time1;
+          // test sending something in the background to ensure if it actually works
+          // the main goal here is to ensure that the predict function is being called with the correct values in the main loop
+          // with a precise frequency to ensure the sampling rate of the sensor data is the same for training and prediction.
+          // TODO: move this to another _thread_ similar to the events thread!
+          char packet[32];
+          char buf[8];
+
+          packet[0] = '\0';
+          strcat(packet, "ACC ");
+          
+          itoa(x, buf);
+          strcat(packet, buf);
+          strcat(packet, " ");
+
+          itoa(y, buf);
+          strcat(packet, buf);
+          strcat(packet, " ");
+
+          itoa(z, buf);
+          strcat(packet, buf);
+
+          hf2.sendSerial(packet, strlen(packet));
+
+          prevTime1 = curTime;
         }
-        else if (time2 - prevTime2 >= sampleRate2 || time2 < prevTime2) {
+        
+        if (curTime - prevTime2 >= sampleRate2 || curTime < prevTime2) {
           runAction0((Action)a);
 
-          prevTime2 = time2;
+          prevTime2 = curTime;
         }
 
         fiber_sleep(5);
