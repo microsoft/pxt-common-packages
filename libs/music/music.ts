92ef9acedeb5c2f3843b838d13af435b05640645
Sam El-Husseini <samelh@microsoft.com>
2017-10-05 10:49:25 -0700

    Add note field editor color

--- libs/music/music.ts
+++ libs/music/music.ts
@@ -162,7 +162,8 @@ namespace music {
     //% weight=1 help=music/note-frequency
     //% blockId=device_note block="%note"
     //% shim=TD_ID color="#FFFFFF" colorSecondary="#FFFFFF"
-    //% note.fieldEditor="note" note.defl="262" note.fieldOptions.decompileLiterals=true
+    //% note.fieldEditor="note" note.defl="262"
+    //% note.fieldOptions.editorColour="#D83B01" note.fieldOptions.decompileLiterals=true
     //% useEnumVal=1
     //% weight=10 blockGap=8
     export function noteFrequency(name: Note): number {
