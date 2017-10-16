9db102d66b5ad41d6db5cc5d0e8c188f2457304c
Sam El-Husseini <microsoftsam@users.noreply.github.com>
2017-10-15 19:11:07 -0700

    Refactor servo blocks into their own package (#123)

---
+++ libs/servo/pxt.h
@@ -0,0 +1,16 @@
+{
+    "name": "servo",
+    "description": "The servo library",
+    "files": [
+        "README.md",
+        "servo.cpp",
+        "shims.d.ts"
+    ],
+    "testFiles": [
+        "test.ts"
+    ],
+    "public": true,
+    "dependencies": {
+        "core": "file:../core"
+    }
+}
