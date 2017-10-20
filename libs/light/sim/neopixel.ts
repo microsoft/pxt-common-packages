012bd0a2eb4d961cf44e14f702216be739396a76
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-10-20 05:01:52 -0700

    more cleanup

--- libs/light/sim/neopixel.ts
+++ libs/light/sim/neopixel.ts
@@ -3,7 +3,6 @@ namespace pxsim {
     export class CommonNeoPixelState {
         public length = 10;
         private neopixels: [number, number, number][] = [];
-        private brightness: number = 20;
 
         public pixelColor(pixel: number): [number, number, number] {
             return this.neopixels[pixel];
@@ -15,27 +14,6 @@ namespace pxsim {
             a[0] = red;
             a[1] = green;
             a[2] = blue;
-        }
-
-        public setBrightness(brightness: number) {
-            this.brightness = brightness;
-        }
-
-        public getBrightness(): number {
-            return this.brightness;
-        }
-
-        public rotate(offset: number = 1, reverse?: boolean) {
-            for (let i = 0; i < offset; i++) {
-                if (reverse)
-                    this.neopixels.unshift(this.neopixels.pop());
-                else
-                    this.neopixels.push(this.neopixels.shift());
-            }
-        }
-
-        public clearPixels() {
-            this.neopixels = [];
         }
     }
 }
