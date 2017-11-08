4f3602576194ce73a38b0d530665773b0481e8d1
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-11-08 08:57:01 -0800

    updated icon

--- libs/base/console.ts
+++ libs/base/console.ts
@@ -3,9 +3,8 @@
 /**
  * Reading and writing data to the console output.
  */
-//% weight=2 color=#002050 icon="\uf287"
+//% weight=12 color=#002050 icon="\uf120"
 //% advanced=true
-
 namespace console {
     /**
      * Write a line of text to the console output.
