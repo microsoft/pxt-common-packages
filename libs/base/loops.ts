e2df0eee1df5963c5c54a974fd4a5416acbf9b36
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-11-17 09:05:49 -0800

    single fiber polling impl

--- libs/base/loops.ts
+++ libs/base/loops.ts
@@ -1,4 +1,58 @@
 namespace loops {
+    interface PollEvent {
+        eid: number;
+        vid: number;
+        start: number;
+        timeOut: number;
+        condition: () => boolean;
+        once: boolean;
+    }
+
+    let pollEvents: PollEvent[] = [];
+    let pollRunning = false;
+
+    function pollEvents() {
+        while (pollEvents.length > 0) {
+            const now = control.millis();
+            let needsCleanup = false;
+            for (let i = 0; i < pollEvents.length; ++i) {
+                const ev = pollEvents[i];
+                if (ev.condition() || (ev.timeOut > 0 && now - ev.start > ev.timeOut)) {
+                    control.raiseEvent(ev.eid, ev.vid);
+                    ev.condition = undefined;
+                    needsCleanup = true;
+                }
+            }
+            if (needsCleanup)
+                pollEvents = pollEvents.filter(ev => !!ev.condition);
+            loops.pause(50);
+        }
+        pollRunning = false;
+    }
+
+    function queuePollEvent(timeOut: number, condition: () => boolean, handler: () => void) {
+        const ev = {
+            eid: control.allocateNotifyEvent(),
+            vid: 1,
+            start: control.millis(),
+            timeOut: timeOut || 0,
+            condition: condition,
+            once: !handler
+        };
+        // register event
+        if (handler)
+            control.onEvent(ev.eid, ev.vid, handler);
+        pollEvents.push(ev)
+        // start polling fibier if needed
+        if (!pollRunning) {
+            pollRunning = true;
+            control.runInBackground(pollEvents);
+        }
+        // register wait
+        if (!handler)
+            control.waitForEvent(ev.eid, ev.vid);
+    }
+
     /**
      * Busy wait for a condition to be true
      * @param condition condition to test for
@@ -8,16 +62,7 @@ namespace loops {
     // add block when PXT supports this signature
     export function waitUntil(condition: () => boolean, timeOut?: number): void {
         if (!condition || condition()) return; // optimistic path
-
-        const eid = control.allocateNotifyEvent();
-        const start = control.millis();
-        control.runInBackground(() => {
-            while (!condition() || (timeOut > 0 && control.millis() - start > timeOut)) {
-                loops.pause(50);
-            }
-            control.raiseEvent(eid, 1);
-        })
-        control.waitForEvent(eid, 1);
+        queuePollEvent(timeOut, condition, undefined);
     }
 
     /**
@@ -27,20 +72,7 @@ namespace loops {
      */
     //%
     export function when(condition: () => boolean, handler: () => void) {
-        if (!condition) return;
-
-        const eid = control.allocateNotifyEvent();
-        control.onEvent(eid, 1, handler);
-        let old = false;
-        control.runInBackground(() => {
-            while (true) {
-                let current = condition();
-                if (current && current != old) {
-                    control.raiseEvent(eid, 1);
-                }
-                old = current;
-                loops.pause(50);
-            }
-        })
+        if (!condition || !handler) return;
+        queuePollEvent(0, condition, handler);
     }
 }
