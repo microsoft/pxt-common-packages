1dfe265dca314f833d8516756ea18b26a03964b6
Peli de Halleux <pelikhan@users.noreply.github.com>
2017-11-27 16:11:31 -0800

    don't preallocate poll event queue

--- libs/base/loops.ts
+++ libs/base/loops.ts
@@ -16,25 +16,25 @@ namespace loops {
         }
     }
 
-    let pollEventQueue: PollEvent[] = [];
-    let pollRunning = false;
+    let _pollEventQueue: PollEvent[] = undefined;
 
     function pollEvents() {
-        while (pollEventQueue.length > 0) {
+        while (_pollEventQueue.length > 0) {
             const now = control.millis();
-            for (let i = 0; i < pollEventQueue.length; ++i) {
-                const ev = pollEventQueue[i];
+            for (let i = 0; i < _pollEventQueue.length; ++i) {
+                const ev = _pollEventQueue[i];
                 if (ev.condition() || (ev.timeOut > 0 && now - ev.start > ev.timeOut)) {
                     control.raiseEvent(ev.eid, ev.vid);
                     if (ev.once) {
-                        pollEventQueue.splice(i, 1);
+                        _pollEventQueue.splice(i, 1);
                         --i;
                     }
                 }
             }
             loops.pause(50);
         }
-        pollRunning = false;
+        // release fiber
+        _pollEventQueue = undefined;
     }
 
     function queuePollEvent(timeOut: number, condition: () => boolean, handler: () => void) {
@@ -46,18 +46,20 @@ namespace loops {
             condition,
             !handler
         );
-        // register event
-        if (!!handler)
-            control.onEvent(ev.eid, ev.vid, handler);
-        // add to the queue
-        pollEventQueue.push(ev)
+
         // start polling fiber if needed
-        if (!pollRunning) {
-            pollRunning = true;
+        if (!_pollEventQueue) {
+            _pollEventQueue = [];
             control.runInBackground(pollEvents);
         }
-        // register wait
-        if (!handler)
+        
+        // add to the queue
+        _pollEventQueue.push(ev)
+
+        // register event
+        if (handler)
+            control.onEvent(ev.eid, ev.vid, handler);
+        else // or wait
             control.waitForEvent(ev.eid, ev.vid);
     }
 
