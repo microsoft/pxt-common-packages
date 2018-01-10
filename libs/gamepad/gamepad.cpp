050ac1ac73b90b519c4ff6a21a946cc09ceddf0e
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-01-10 09:57:51 -0800

    more apis

--- libs/gamepad/gamepad.cpp
+++ libs/gamepad/gamepad.cpp
@@ -6,7 +6,7 @@
 namespace pxt {
  
 class WJoystick {
-  private:
+  public:
     USBHIDJoystick joystick;
   public:
     WJoystick() 
@@ -26,6 +26,30 @@ namespace gamepad {
     //% index.min=0 index.max=127
     //% down.fieldeditor=toggleupdown
     void setButton(int index, bool down) {
+        const pJoystick = getWJoystick();
+        if (down)
+            pJoystick->joystick.buttonDown(index);
+        else
+            pJoystick->joystick.buttonUp(index);
+    }
 
+    /**
+    * Sets the current move on the gamepad
+    **/
+    //% block=joystickMove block="move %index|x %x|y %y"
+    //% index.min=0 index.max=1
+    void move(int index, int x, int y) {
+        const pJoystick = getWJoystick();
+        pJoystick->move(index, x, y);        
+    }
+
+    /** 
+    * Sets the throttle state
+    */
+    //% block=joystickSetThrottle block="set throttle %index|%value"
+    //% index.min=0 index.max=1
+    void setThrottle(int index, int value) {
+        const pJoystick = getWJoystick();
+        pJoystick->setThrottle(index, value);
     }
 }
--- libs/gamepad/shims.d.ts
+++ libs/gamepad/shims.d.ts
@@ -8,6 +8,20 @@ declare namespace gamepad {
     //% index.min=0 index.max=127
     //% down.fieldeditor=toggleupdown shim=gamepad::setButton
     function setButton(index: int32, down: boolean): void;
+
+    /**
+     * Sets the current move on the gamepad
+     **/
+    //% block=joystickMove block="move %index|x %x|y %y"
+    //% index.min=0 index.max=1 shim=gamepad::move
+    function move(index: int32, x: int32, y: int32): void;
+
+    /** 
+     * Sets the throttle state
+     */
+    //% block=joystickSetThrottle block="set throttle %index|%value"
+    //% index.min=0 index.max=1 shim=gamepad::setThrottle
+    function setThrottle(index: int32, value: int32): void;
 }
 
 // Auto-generated. Do not edit. Really.
