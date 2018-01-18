b494a99f38f07e4d7e9873666239f03adf054216
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-01-18 10:49:14 -0800

    datalog support

---
+++ libs/datalog/datalog.ts
@@ -0,0 +1,122 @@
+//% weight=100 color=#0fbc11 icon=""
+namespace datalog {
+    export interface DatalogStorage {
+        reset(filename: string): void;
+        appendHeaders(filename: string, headers: string[]): number;
+        appendRow(filename: string, values: number[]): void;
+        flush(): void;
+    }
+
+    let _headers: string[] = undefined;
+    let _headersLength: number;
+    let _values: number[];
+    let _start: number;
+    let _filename = "datalog.csv";
+    let _storage: DatalogStorage;
+    let _enabled = true;
+
+    function clear() {
+        _headers = undefined;
+        _values = undefined;
+    }
+
+    function init() {
+        if (!_headers) {
+            _headers = [];
+            _headersLength = 0;
+            _start = control.millis();
+            if (_storage) _storage.reset(_filename);
+        }
+        _values = [];
+    }
+
+    function commit() {
+        // write row if any data
+        if (_values && _values.length > 0) {
+            if (_storage) {
+                // write headers for the first row
+                if (!_headersLength) {
+                    _headersLength = _storage.appendHeaders(_filename, _headers);
+                }
+                // commit row data
+                _storage.appendRow(_filename, _values);
+            }
+        }
+
+        // clear values
+        _values = undefined;
+    }
+
+    /**
+     * Starts a row of data
+     */
+    //% weight=100
+    //% blockId=datalogAddRow block="datalog add row"
+    export function addRow(): void {
+        if (!_enabled) return;
+
+        commit();
+        init();
+        const s = (control.millis() - _start) / 1000;
+        addValue("time (s)", s);
+    }
+
+    /**
+     * Adds a cell to the row of data
+     * @param name name of the cell, eg: "x"
+     * @param value value of the cell, eg: 0
+     */
+    //% weight=99
+    //% blockId=datalogAddValue block="datalog add %name|=%value"
+    export function addValue(name: string, value: number) {
+        if (!_values) return;
+        let i = _headers.indexOf(name);
+        if (i < 0) {
+            _headers.push(name);
+            i = _headers.length - 1;
+        }
+        _values[i] = value;
+    }
+
+    /**
+     * Starts a new data logger for the given file
+     * @param filename the filename, eg: "datalog.csv"
+     */
+    //%
+    export function setFile(filename: string) {
+        flush();
+        _filename = filename;
+        clear();
+    }
+
+    /**
+     * 
+     * @param storage custom storage solution
+     */
+    //%
+    export function setStorage(storage: DatalogStorage) {
+        flush();
+        _storage = storage;
+        clear();
+    }
+
+    /**
+     * Commits any buffered row to disk
+     */
+    //%
+    export function flush() {
+        if (_storage)
+            _storage.flush();
+    }
+
+    /**
+     * Turns on or off datalogging
+     * @param enabled 
+     */
+    //% blockId=datalogEnabled block="datalog %enabled"
+    //% enabled.fieldEditor=fieldonoff
+    export function setEnabled(enabled: boolean) {
+        flush();
+        _enabled = enabled;
+    }
+}
