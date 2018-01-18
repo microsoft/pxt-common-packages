1509cda85260ca4f04120841465dd7d383c75450
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-01-18 10:10:34 -0800

    Timers (#205)
    * adding timers
    
    * updated description
    
    * moving to automation
    
    * adding namespace ordering
    
    * updated pid
    
    * updated pid
    
    * clamping variables
    
    * updated windup
    
    * updated ref

--- libs/automation/behaviors.ts
+++ libs/automation/behaviors.ts
@@ -1,8 +1,8 @@
 /**
- * Behavior drive blocks
+ * Process control, automation, robotics AI
  */
 //% color=0x000000 weight=80 icon="\uf277"
-namespace behaviors {
+namespace automation {
     /**
      * A behavior
      */
@@ -12,6 +12,14 @@ namespace behaviors {
 
         constructor() {
             this.active = false;
+        }
+
+        /**
+         * Called on each behavior iteration even for suppresed behaviors
+         * @param elapsed milli seconds since last call
+         */
+        update(elapsed: number) {
+            // override
         }
 
         shouldRun(): boolean {
@@ -37,17 +45,22 @@ namespace behaviors {
     export class BehaviorManager {
         private _behaviors: Behavior[];
         private _state: BehaviorManagerState;
+        private _timer: control.Timer;
+
+        public timestep: number;
 
         constructor() {
             this._behaviors = [];
             this._state = BehaviorManagerState.Stopped;
+            this._timer = new control.Timer();
+            this.timestep = 20;
         }
 
         /**
          * Adds a new behavior to the behavior manager
          * @param behavior the behavior to add
          */
-        //%
+        //% group="Behaviors"
         add(behavior: Behavior) {
             if (!behavior) return;
 
@@ -58,7 +71,7 @@ namespace behaviors {
         /**
          * Starts the behavior control loop
          */
-        //%
+        //%  group="Behaviors"
         start(): void {
             if (this._state == BehaviorManagerState.Running) return;
             this._state = BehaviorManagerState.Running;
@@ -74,10 +87,15 @@ namespace behaviors {
         }
         
         private run() {
+            let elapsed = 0;
             // this is the main control loop
             while (this._state == BehaviorManagerState.Running) {
                 const bvs = this._behaviors;
                 const n = bvs.length;
+
+                // update all behaviors, even supprsed
+                for (let i = 0; i < n; ++i)
+                    bvs[i].update(elapsed);                
 
                 // poll non-suppressed behaviors
                 for (let i = 0; i < n; ++i) {
@@ -92,7 +110,9 @@ namespace behaviors {
                 }
 
                 // give a breather to the events
-                loops.pause(20);
+                this._timer.pauseUntil(this.timestep);
+                elapsed = this._timer.millis();
+                this._timer.reset();
             }
 
             // tell manager that we are done
@@ -120,7 +140,7 @@ namespace behaviors {
      * @param behavior a behavior
      */
     //% blockId=behaviorsAddBehavior block="add behavior %behavior"
-    //% weight=100
+    //% weight=100 group="Behaviors"
     export function addBehavior(behavior: Behavior) {
         if (!_manager) {
             _manager = new BehaviorManager();
