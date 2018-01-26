fe9dd8188817c78d99842873334e1c4ad4c53405
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-01-26 14:42:35 -0800

    fixing gamepad field editor

--- libs/gamepad/gamepad.cpp
+++ libs/gamepad/gamepad.cpp
@@ -9,7 +9,7 @@ namespace gamepad {
     */
     //% help=gamepad/set-button
     //% blockId=joystickSetButton block="gamepad button %index=joystickStandardButton|%down"
-    //% down.fieldEditor=toggleupdown down.fieldOptions.inverted=true
+    //% down.fieldEditor=toggledownup
     //% weight=100
     void setButton(int index, bool down) {
         if (down)
