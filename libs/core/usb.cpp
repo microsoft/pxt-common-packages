38eff0150614bdc8a6f1a2e62205676468e37048
Michal Moskal <michal@moskal.me>
2018-02-06 11:52:57 -0800

    Use custom device descriptor so USB device id can be updated

--- libs/core/usb.cpp
+++ libs/core/usb.cpp
@@ -20,6 +20,24 @@ USBHIDKeyboard keyboard;
 USBHIDJoystick joystick;
 #endif
 
+static const DeviceDescriptor device_desc = {
+    0x12,            // bLength
+    0x01,            // bDescriptorType
+    0x0210,          // bcdUSBL
+
+    // Class etc specified per-interface
+    0x00, 0x00, 0x00,
+
+    0x40,            // bMaxPacketSize0
+    0x03EB, // Atmel
+    0x2403, // Example device
+    0x4202,          // bcdDevice - leave unchanged for the HF2 to work
+    0x01,            // iManufacturer
+    0x02,            // iProduct
+    0x03,            // SerialNumber
+    0x01             // bNumConfigs
+};
+
 // TODO extract these from uf2_info()?
 static const char *string_descriptors[] = {
     "Example Corp.",
@@ -36,6 +54,8 @@ static void start_usb() {
 
 void usb_init() {
     usb.stringDescriptors = string_descriptors;
+    usb.deviceDescriptor = &device_desc;
+
 #if CONFIG_ENABLED(DEVICE_MOUSE)
     usb.add(mouse);
 #endif
