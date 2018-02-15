7b63e01afbfe995674953dde2a2bc0b052824f80
Michal Moskal <michal@moskal.me>
2018-02-14 16:13:49 -0800

    Start on image sim

---
+++ libs/screen/sim/image.ts
@@ -0,0 +1,425 @@
+/// <reference path="../../../node_modules/pxt-core/built/pxtsim.d.ts"/>
+
+namespace pxsim {
+    export class RefImage {
+        _width: number;
+        _height: number;
+        data: Uint8Array;
+
+
+        constructor(w: number, h: number) {
+            this.data = new Uint8Array(w * h)
+            this._width = w
+            this._height = h
+        }
+
+        pix(x: number, y: number) {
+            return (x | 0) + (y | 0) * this._width
+        }
+
+        inRange(x: number, y: number) {
+            return 0 <= (x | 0) && (x | 0) < this._width &&
+                0 <= (y | 0) && (y | 0) < this._height;
+        }
+
+        color(c: number): number {
+            return c & 0xff
+        }
+
+        clamp(x: number, y: number) {
+            x |= 0
+            y |= 0
+
+            if (x < 0) x = 0
+            else if (x >= this._width)
+                x = this._width - 1
+
+            if (y < 0) y = 0
+            else if (y >= this._height)
+                y = this._height - 1
+
+            return [x, y]
+        }
+
+    }
+}
+
+namespace pxsim.ImageMethods {
+
+    export function width(img: RefImage) { return img._width }
+
+    export function height(img: RefImage) { return img._height }
+
+    export function set(img: RefImage, x: number, y: number, c: number) {
+        if (img.inRange(x, y))
+            img.data[img.pix(x, y)] = img.color(c)
+    }
+
+    export function get(img: RefImage, x: number, y: number) {
+        if (img.inRange(x, y))
+            return img.data[img.pix(x, y)]
+        return 0
+    }
+
+    export function fill(img: RefImage, c: number) {
+        img.data.fill(img.color(c))
+    }
+
+    export function fillRect(img: RefImage, x: number, y: number, w: number, h: number, c: color) {
+        let [x2, y2] = img.clamp(x + w - 1, y + h - 1);
+        [x, y] = img.clamp(x, y)
+        let p = img.pix(x, y)
+        w = x2 - x + 1
+        h = y2 - y + 1
+        let d = img._width - w
+        c = img.color(c)
+        while (h-- > 0) {
+            for (let i = 0; i < w; ++i)
+                img.data[p++] = c
+            p += d
+        }
+    }
+
+    export function clone(img: RefImage) {
+        let r = new RefImage(img._width, img._height)
+        r.data.set(img.data)
+        return r
+    }
+
+    export function flipX(img: RefImage) {
+        const w = img._width
+        const h = img._height
+        for (let i = 0; i < h; ++i) {
+            img.data.subarray(i * w, (i + 1) * w).reverse()
+        }
+    }
+
+
+    export function flipY(img: RefImage) {
+        const w = img._width
+        const h = img._height
+        const d = img.data
+        for (let i = 0; i < w; ++i) {
+            let top = i
+            let bot = i + (h - 1) * w
+            while (top < bot) {
+                let c = d[top]
+                d[top] = d[bot]
+                d[bot] = c
+                top += w
+                bot -= w
+            }
+        }
+    }
+
+    export function scroll(img: RefImage, dx: number, dy: number) {
+        dx |= 0
+        dy |= 0
+        if (dy < 0) {
+            dy = -dy
+            if (dy < img._height)
+                img.data.copyWithin(0, dy * img._width)
+            else
+                dy = img._height
+            img.data.fill(0, (img._height - dy) * img._width)
+        } else if (dy > 0) {
+            if (dy < img._height)
+                img.data.copyWithin(dy * img._width, 0)
+            else
+                dy = img._height
+            img.data.fill(0, 0, dy * img._width)
+        }
+        // TODO implement dx
+    }
+
+    export function doubleX(img: RefImage) {
+        const w = img._width
+        const h = img._height
+        const d = img.data
+        const r = new RefImage(w * 2, h)
+        const n = r.data
+        let dst = 0
+
+        for (let src = 0; src < d.length; ++src) {
+            let c = d[src]
+            n[dst++] = c
+            n[dst++] = c
+        }
+
+        return r
+    }
+
+    export function doubleY(img: RefImage) {
+        const w = img._width
+        const h = img._height
+        const d = img.data
+        const r = new RefImage(w, h * 2)
+        const n = r.data
+
+        let src = 0
+        let dst0 = 0
+        let dst1 = w
+        for (let i = 0; i < h; ++i) {
+            for (let j = 0; j < w; ++j) {
+                let c = d[src++]
+                n[dst0++] = c
+                n[dst1++] = c
+            }
+            dst0 += w
+            dst1 += w
+        }
+
+        return r
+    }
+
+
+    export function double(img: RefImage) {
+        return doubleX(doubleY(img))
+    }
+
+    export function drawImage(img: RefImage, from: RefImage, x: number, y: number) {
+        x |= 0
+        y |= 0
+
+        const w = from._width
+        let h = from._height
+        const sh = img._height
+        const sw = img._width
+
+        if (x + w <= 0) return
+        if (x >= sw) return
+        if (y + h <= 0) return
+        if (y >= sh) return
+
+        const len = x < 0 ? Math.min(sw, w + x) : Math.min(sw - x, w)
+        const fdata = from.data
+        const tdata = img.data
+
+        for (let p = 0; h--; y++ , p += w) {
+            if (0 <= y && y < sh) {
+                let dst = y * sw
+                let src = p
+                if (x < 0)
+                    src += -x
+                else
+                    dst += x
+                for (let i = 0; i < len; ++i) {
+                    const v = fdata[src++]
+                    if (v)
+                        tdata[dst] = v
+                    dst++
+                }
+            }
+        }
+    }
+
+    export function overlapsWith(img: RefImage, other: RefImage, x: number, y: number) {
+        x |= 0
+        y |= 0
+
+        const w = other._width
+        let h = other._height
+        const sh = img._height
+        const sw = img._width
+
+        if (x + w <= 0) return false
+        if (x >= sw) return false
+        if (y + h <= 0) return false
+        if (y >= sh) return false
+
+        const len = x < 0 ? Math.min(sw, w + x) : Math.min(sw - x, w)
+        const fdata = other.data
+        const tdata = img.data
+
+        for (let p = 0; h--; y++ , p += w) {
+            if (0 <= y && y < sh) {
+                let dst = y * sw
+                let src = p
+                if (x < 0)
+                    src += -x
+                else
+                    dst += x
+                for (let i = 0; i < len; ++i) {
+                    const v = fdata[src++]
+                    if (v && tdata[dst])
+                        return true
+                    dst++
+                }
+            }
+        }
+
+        return false
+    }
+
+    export function drawIcon(img: RefImage, icon: RefBuffer, x: number, y: number, color: number) {
+        const img2 = icon.data
+        if (!img2 || img2.length < 4 || img2[0] != 0xf1)
+            return
+        let w = img2[1]
+        let byteW = (w + 7) >> 3
+        let h = img2[2]
+
+        x |= 0
+        y |= 0
+        const sh = img._height
+        const sw = img._width
+
+        if (x + w <= 0) return
+        if (x >= sw) return
+        if (y + h <= 0) return
+        if (y >= sh) return
+
+        let p = 2
+        color = img.color(color)
+        const screen = img.data
+
+        for (let i = 0; i < h; ++i) {
+            let yy = y + i
+            if (0 <= yy && yy < sh) {
+                let dst = yy * sw
+                let src = p
+                let xx = x
+                let end = Math.min(sw, w + x)
+                if (x < 0) {
+                    src += ((-x) >> 3)
+                    xx += ((-x) >> 3) * 8
+                }
+                dst += xx
+                let mask = 0x01
+                let v = img2[src++]
+                while (xx < end) {
+                    if (xx >= 0 && (v & mask)) {
+                        screen[dst] = color
+                    }
+                    mask <<= 1
+                    if (mask & 0x100) {
+                        mask = 0x01
+                        v = img2[src++]
+                    }
+                    dst++
+                    xx++
+                }
+            }
+            p += byteW
+        }
+    }
+}
+
+
+namespace pxsim.image {
+    function isValidImage(buf: RefBuffer) {
+        if (!buf || buf.data.length < 4)
+            return false;
+
+        if (buf.data[0] != 0xf1 && buf.data[0] != 0xf4)
+            return false;
+
+        const bpp = buf.data[0] & 0xf;
+        const sz = buf.data[2] * ((buf.data[1] * bpp + 7) >> 3);
+        if (3 + sz != buf.data.length)
+            return false;
+
+        return true;
+    }
+
+
+    export function create(w: number, h: number) {
+        return new RefImage(w, h)
+    }
+
+    export function ofBuffer(buf: RefBuffer): RefImage {
+        if (!isValidImage(buf))
+            return null
+        const src = buf.data
+        const w = src[1]
+        const h = src[2]
+        if (w == 0 || h == 0)
+            return null
+        const r = new RefImage(w, h)
+        const dst = r.data
+
+        let dstP = 0
+        let srcP = 3
+
+        const bpp = src[0] & 0xf;
+
+        if (bpp == 1) {
+            const len = (w + 7) >> 3
+            for (let i = 0; i < h; ++i) {
+                for (let j = 0; j < len; ++j) {
+                    let v = src[srcP++]
+                    let mask = 0x80
+                    let n = 8
+                    if (j == len - 1 && w & 7)
+                        n = w & 7
+                    while (n-- && mask) {
+                        if (v & mask)
+                            dst[dstP++] = 1
+                        mask >>= 1
+                    }
+                }
+            }
+        } else if (bpp == 4) {
+            for (let i = 0; i < h; ++i) {
+                for (let j = 0; j < w >> 1; ++j) {
+                    const v = src[srcP++]
+                    dst[dstP++] = v >> 4
+                    dst[dstP++] = v & 0xf
+                }
+                if (w & 1)
+                    dst[dstP++] = src[srcP++] >> 4
+            }
+        }
+
+        return r
+    }
+
+
+    function bytes(x: number, isMono: boolean) {
+        if (isMono)
+            return ((x + 7) >> 3)
+        else
+            return ((x + 1) >> 1)
+    }
+
+    const bitdouble = [
+        0x00, 0x03, 0x0c, 0x0f, 0x30, 0x33, 0x3c, 0x3f, 0xc0, 0xc3, 0xcc, 0xcf, 0xf0, 0xf3, 0xfc, 0xff,
+    ]
+
+    const nibdouble = [
+        0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88, 0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff
+    ]
+
+
+    export function doubledIcon(buf: RefBuffer): RefBuffer {
+        if (!isValidImage(buf))
+            return null;
+        const w = buf.data[1];
+        if (w > 126)
+            return null;
+        const isMono = buf.data[0] == 0xf1
+        const bw = bytes(w, isMono);
+        const h = buf.data[2]
+        const bw2 = bytes(w * 2, isMono);
+        const out = pxsim.BufferMethods.createBuffer(2 + bw2 * h * 2)
+        out.data[0] = buf.data[0];
+        out.data[1] = w * 2;
+        out.data[2] = h * 2;
+        let src = 3
+        let dst = 3
+        const dbl = isMono ? bitdouble : nibdouble
+        for (let i = 0; i < h; ++i) {
+            for (let jj = 0; jj < 2; ++jj) {
+                let p = src;
+                for (let j = 0; j < bw; ++j) {
+                    const v = buf.data[p++]
+                    out.data[dst++] = dbl[v & 0xf];
+                    out.data[dst++] = dbl[v >> 4];
+                }
+                if (!isMono && (w & 1)) dst--
+            }
+            src += bw;
+        }
+        return out;
+    }
+}
