93801a9c1bce9736e41c6169b8d2a21eee82257a
Michal Moskal <michal@moskal.me>
2018-02-16 16:22:15 -0800

    Support for .comm symbols in assembly

--- libs/core/codal.cpp
+++ libs/core/codal.cpp
@@ -2,6 +2,15 @@
 
 void cpu_clock_init(void);
 
+PXT_ABI(__aeabi_dadd)
+PXT_ABI(__aeabi_dcmplt)
+PXT_ABI(__aeabi_dcmpgt)
+PXT_ABI(__aeabi_dsub)
+PXT_ABI(__aeabi_ddiv)
+PXT_ABI(__aeabi_dmul)
+
+#define PXT_COMM_BASE 0x20001000  // 4k in
+
 namespace pxt {
 
 void platform_init();
@@ -20,8 +29,45 @@ Event lastEvent;
 MessageBus devMessageBus;
 codal::CodalDevice device;
 
+struct FreeList {
+    FreeList *next;
+};
+
+static void commInit() {
+    int commSize = bytecode[20];
+    if (!commSize) return;
+
+    FreeList *head = NULL;        
+    void *commBase = (void*)PXT_COMM_BASE;
+    for (;;) {
+        // assume 4 byte alloc header; if we're not hitting 8 byte alignment, try allocing 8 bytes, not 4
+        void *p = malloc((unsigned)head & 4 ? 8 : 4);
+        if (p == commBase) {
+            free(p);
+            // allocate the comm section; this is never freed
+            p = malloc(commSize);
+            if (p != commBase)
+                target_panic(999);
+            break;
+        }
+        if (p > commBase) 
+            target_panic(999);
+        auto f = (FreeList*)p;
+        f->next = head;
+        head = f;
+    }
+    // free all the filler stuff
+    while (head) {
+        auto p = head;
+        head = head->next;
+        free(p);
+    }
+}
+
 static void initCodal() {
     cpu_clock_init();
+
+    commInit();
 
     // Bring up fiber scheduler.
     scheduler_init(devMessageBus);
