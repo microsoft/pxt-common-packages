b9fc2e16f2a9e3432b499d4047b8e49e488ca01d
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-03-13 13:02:09 -0700

    updated game.ts

--- libs/game/game.ts
+++ libs/game/game.ts
@@ -1,7 +1,16 @@
+/**
+ * Game transitions and dialog
+ **/
+//% color=#008272 weight=99 icon="\uf11b"
 namespace game {
+    /**
+     * Determins if diagnostics are shown
+     */
+    export let debug = false;
+
     let isOver = false
-    let _score: number = null
     let _waitAnyKey: () => void
+    let bgFunction = () => { }
 
     export function setWaitAnyKey(f: () => void) {
         _waitAnyKey = f
@@ -13,9 +22,48 @@ namespace game {
     }
 
     export function freeze() {
-        sprite.setBackgroundCallback(() => { })
-        loops.frame(() => { })
-        sprite.reset()
+        setBackgroundCallback(() => { })
+        frame(() => { })
+        sprites.allSprites = [];
+    }
+
+    export function init() {
+        if (!sprites.allSprites) {
+            sprites.allSprites = []
+            game.setBackground(0)
+            control.addFrameHandler(10, () => {
+                const dt = control.deltaTime;
+                physics.engine.update(dt);
+                for (let s of sprites.allSprites)
+                    s._update(dt);
+            })
+            control.addFrameHandler(60, () => { bgFunction() })
+            control.addFrameHandler(90, () => {
+                // stack overflow
+                // allSprites.sort(function (a, b) { return a.z - b.z || a.id - b.id; })
+                for (let s of sprites.allSprites)
+                    s._draw()
+                if (game.debug)
+                    physics.engine.draw();
+            })
+        }
+    }    
+
+    export function setBackgroundCallback(f: () => void) {
+        init();
+        bgFunction = f
+    }
+
+    /**
+     * Sets the game background color
+     * @param color 
+     */
+    //% blockId=gamesetbackgroundcolor block="set background to %color"
+    export function setBackground(color: number) {
+        init();
+        bgFunction = () => {
+            screen.fill(color)
+        }
     }
 
     function showBackground(h: number, c: number) {
@@ -30,25 +78,39 @@ namespace game {
         return top
     }
 
-    export function showDialog(name: string, content: string) {
-        let lines = 1
-        if (!content) lines = 0
-        else
-            for (let i = 0; i < content.length; ++i)
-                if (content[i] == '\n') lines++
-
-        let h = 28 + lines * (image.font5.charHeight + 2)
-        let top = showBackground(h, 9)
-        screen.print(name, 8, top + 8, 14, image.font8)
-        screen.print(content, 8, top + 23, 13, image.font5)
-    }
-
-    export function splash(name: string, help: string) {
-        showDialog(name, help)
+    /**
+     * Show a title, subtitle menu
+     * @param title 
+     * @param subtitle
+     */
+    //% weight=90
+    //% blockId=gameSplash block="splash %title %subtitle"
+    export function splash(title: string, subtitle: string) {
+        showDialog(title, subtitle)
         waitAnyKey()
     }
 
-    export function meltScreen() {
+    /**
+     * Shows a dialog on screen
+     * @param title 
+     * @param subtitle 
+     */
+    //% weight=89
+    //% blockId=gameDialog block="show dialog %title %subtitle"
+    export function showDialog(title: string, subtitle: string) {
+        let lines = 1
+        if (!subtitle) lines = 0
+        else
+            for (let i = 0; i < subtitle.length; ++i)
+                if (subtitle[i] == '\n') lines++
+
+        let h = 28 + lines * (image.font5.charHeight + 2)
+        let top = showBackground(h, 9)
+        screen.print(title, 8, top + 8, 14, image.font8)
+        screen.print(subtitle, 8, top + 23, 13, image.font5)
+    }
+
+    function meltScreen() {
         freeze()
         for (let i = 0; i < 10; ++i) {
             for (let j = 0; j < 1000; ++j) {
@@ -62,53 +124,27 @@ namespace game {
         }
     }
 
-    export function over(effect?: () => void) {
+    /**
+     * Finishes the game and displays score
+     */
+    //% blockId=gameOver block="game over"
+    //% weight=80
+    export function over() {
         if (isOver) return
         takeScreenshot();
         isOver = true
         control.clearHandlers()
         control.runInParallel(() => {
-            if (effect) effect()
+            music.playSound(music.sounds(Sounds.Wawawawaa))
+            meltScreen();
             let top = showBackground(44, 4)
             screen.printCenter("GAME OVER!", top + 8, 5, image.font8)
-            if (hasScore())
-                screen.printCenter("Score:" + game.score(), top + 23, 2, image.font5)
-            if (!effect)
-                pause(1000) // wait for users to stop pressing keys
+            if (hud.hasScore())
+                screen.printCenter("Score:" + hud.score(), top + 23, 2, image.font5)
+            pause(1000) // wait for users to stop pressing keys
             waitAnyKey()
             control.reset()
         })
-    }
-
-    export function score() {
-        initScore()
-        return _score
-    }
-
-    export function hasScore() {
-        return _score !== null
-    }
-
-    function initScore() {
-        if (_score !== null) return
-        _score = 0
-        let font = image.font8
-        let color = 15
-        let maxW = 8
-        control.addFrameHandler(95, () => {
-            let s = _score + ""
-            while (s.length < maxW) s = " " + s
-            screen.print(s, screen.width - font.charWidth * maxW - 10, font.charHeight, color, font)
-        })
-    }
-
-    export function setScore(score: number) {
-        initScore()
-        _score = score | 0
-    }
-
-    export function addToScore(points: number) {
-        setScore(_score + points)
     }
 
     /**
