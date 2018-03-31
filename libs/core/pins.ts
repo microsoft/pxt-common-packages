9dddcbe07b25db0038388c72c31c360ec2d5fb31
Michał Moskal <michal@moskal.me>
2018-03-30 18:07:33 -0700

    Introduce more pin classes; support DAC on samd (#268)

--- libs/core/pins.ts
+++ libs/core/pins.ts
@@ -230,12 +230,22 @@ interface DigitalPin {
 }
 
 //% noRefCounting fixedInstances
-interface AnalogPin extends DigitalPin {
+interface AnalogInPin extends DigitalPin {
     // methods filled from C++
 }
 
 //% noRefCounting fixedInstances
-interface PwmOnlyPin extends DigitalPin {
+interface AnalogOutPin extends DigitalPin {
+    // methods filled from C++
+}
+
+//% noRefCounting fixedInstances
+interface AnalogPin extends AnalogInPin, AnalogOutPin {
+    // methods filled from C++
+}
+
+//% noRefCounting fixedInstances
+interface PwmOnlyPin extends DigitalPin, AnalogOutPin {
     // methods filled from C++
 }
 
