20dfbab078615e3c79395d07a00e20b14086403e
Richard Knoll <riknoll@microsoft.com>
2018-04-03 17:18:26 -0700

    Fix palette

--- libs/game/game.ts
+++ libs/game/game.ts
@@ -191,7 +191,7 @@ namespace game {
     //% blockId=colorindexpicker block="%index" blockHidden=true shim=TD_ID
     //% index.fieldEditor="colornumber"
     //% index.fieldOptions.valueMode="index"
-    //% index.fieldOptions.colours='["#dedede","#33e2e4","#05b3e0","#3d30ad","#b09eff","#5df51f","#6a8927","#65471f","#98294a","#f80000","#e30ec0","#ff9da5","#ff9005","#efe204","#000000","#ffffff"]'
+    //% index.fieldOptions.colours='["#dedede","#ffffff","#33e2e4","#05b3e0","#3d30ad","#b09eff","#5df51f","#6a8927","#65471f","#98294a","#f80000","#e30ec0","#ff9da5","#ff9005","#efe204","#000000"]'
     export function _colorIndexPicker(index: number) {
         return index;
     }
