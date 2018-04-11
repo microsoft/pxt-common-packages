331fd831c8d7156b5ff4fbc3715cf6f976b05624
Michal Moskal <michal@moskal.me>
2018-04-11 13:36:41 -0700

    Start screen tests

---
+++ cpptests/screen/pxt.h
@@ -0,0 +1,13 @@
+#ifndef __PXT_H
+#define __PXT_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+#define ramint_t uint32_t
+#define IMAGE_BITS 4
+#define DMESG printf
+
+#include "pxtbase.h"
+
+#endif
