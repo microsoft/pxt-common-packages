89c37f3585bd2eda001c807db91be4b3e51adb90
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-04-12 09:50:11 -0700

    adding sim

--- libs/pixel/sim/pixel.ts
+++ libs/pixel/sim/pixel.ts
@@ -1,6 +1,17 @@
+namespace pxsim {
+    export interface PixelBoard extends CommonBoard {
+        pixelPin: Pin;
+    }
+
+    export function pixelPin(): Pin {
+        return (board() as PixelBoard).pixelPin;
+    }
+}
 namespace pxsim.pixel {
     export const defaultPin = light.defaultPin;
     export function sendBuffer(data: RefBuffer) {
-        light.sendBuffer((board() as DalBoard).neopixelPin as any, 1, data)
+        const p = pxsim.pixelPin() as any;
+        if (p)
+            light.sendBuffer(pxsim.pixelPin() as any, 1, data)
     }
 }
