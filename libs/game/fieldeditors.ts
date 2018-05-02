94cf2696983639096aa75d57f71edce6f8b7780f
Richard Knoll <riknoll@microsoft.com>
2018-05-02 13:31:28 -0700

    Left out file in last commit

--- libs/game/fieldeditors.ts
+++ libs/game/fieldeditors.ts
@@ -2,6 +2,7 @@ namespace fieldeditors {
     //% blockId=colorindexpicker block="%index" blockHidden=true shim=TD_ID
     //% index.fieldEditor="colornumber"
     //% index.fieldOptions.valueMode="index"
+    //% index.fieldOptions.decompileLiterals="true"
     export function __colorIndexPicker(index: number) {
         return index;
     }
