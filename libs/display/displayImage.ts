5e43d071d2d32d7bd4712dcdd2de88cf7f8f9c1e
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-05-09 08:59:25 -0700

    added delay to showImage

--- libs/display/displayImage.ts
+++ libs/display/displayImage.ts
@@ -6,8 +6,11 @@ namespace display {
     //% blockId=displayshowimage block="show image %image=image_picker"
     //% weight=100 group="Screen" blockGap=8
     //% help=display/show-image
-    export function showImage(image: Image) {
-        if (!image) return;
-        screen.drawImage(image, 0, 0)
+    export function showImage(image: Image, duration: number = 400) {
+        screen.fill(0);
+        if (image)
+            screen.drawImage(image, 0, 0)
+        if (duration > 0)
+            pause(duration);
     }
 }
