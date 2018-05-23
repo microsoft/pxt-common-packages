579ee2ef42a89536f3893c634ce5b5a8da31d5b0
Greg Norris <greg.norris@ghielectronics.com>
2018-05-23 11:06:20 -0400

    Fixed accelerometer ACC_SYSTEM

--- libs/accelerometer/axis.h
+++ libs/accelerometer/axis.h
@@ -1,5 +1,5 @@
 // Override in target to change inversion of axis
 
-#define ACC_SYSTEM NORTH_EAST_UP
+#define ACC_SYSTEM SIMPLE_CARTESIAN
 #define ACC_UPSIDEDOWN false
 #define ACC_ROTATION COORDINATE_SPACE_ROTATED_0
