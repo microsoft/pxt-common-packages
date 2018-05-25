0ccc85c01680001f2db789c0a63e239e1c17dcb3
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-05-24 17:09:56 -0700

    new field editors (#328)

--- libs/base/fieldeditors.ts
+++ libs/base/fieldeditors.ts
@@ -60,4 +60,26 @@ namespace fieldeditors {
     export function __colorWheelPicker(value: number) {
         return value;
     }
+
+    /**
+     * A speed picker
+     * @param speed the speed, eg: 50
+     */
+    //% blockId=speedPicker block="%speed" shim=TD_ID
+    //% speed.fieldEditor="speed" colorSecondary="#FFFFFF"
+    //% weight=0 blockHidden=1 speed.fieldOptions.decompileLiterals=1
+    export function __speedPicker(speed: number): number {
+        return speed;
+    }
+
+    /**
+     * A turn ratio picker
+     * @param turnratio the turn ratio, eg: 0
+     */
+    //% blockId=turnRatioPicker block="%turnratio" shim=TD_ID
+    //% turnratio.fieldEditor="turnratio" colorSecondary="#FFFFFF"
+    //% weight=0 blockHidden=1 turnRatio.fieldOptions.decompileLiterals=1
+    export function __turnRatioPicker(turnratio: number): number {
+        return turnratio;
+    }    
 }
