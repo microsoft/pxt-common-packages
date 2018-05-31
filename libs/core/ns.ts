34bae2a4612ca83a328138edd1ebba4c9276a195
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-05-31 14:46:59 -0700

    Servo blocks (#333)
    * friendly servo library
    
    * moving namespace
    
    * updated icons
    
    * updated namespace weight

--- libs/core/ns.ts
+++ libs/core/ns.ts
@@ -5,3 +5,12 @@
 //% groups='["other", "More"]'
 namespace input {
 }
+
+/**
+ * Motors and servos
+ */
+//% color="#03AA74" weight=88 icon="\uf021"
+//% groups='["Servos", "DC Motors"]'
+namespace motors {
+
+}
