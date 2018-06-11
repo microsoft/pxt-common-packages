b626c272a36f0f36d1e4de2ad05243c6b7f3161b
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-06-11 16:56:43 -0700

    adding repeat feature

--- libs/controller/controller.ts
+++ libs/controller/controller.ts
@@ -10,41 +10,55 @@ enum ControllerButtonEvent {
  */
 //% weight=97 color="#e15f41" icon="\uf11b"
 namespace controller {
+    const REPEAT_DELAY = 80;
+
     let _userEventsEnabled = true;
+    let activeButtons: Button[];
 
     //% fixedInstances
     export class Button {
         id: number;
         private _pressed: boolean;
-        private checked: boolean;
+        private _pressedElasped: number;
 
         constructor(id: number, buttonId?: number, upid?: number, downid?: number) {
             this.id = id;
             this._pressed = false;
-            this.checked = false;
             control.internalOnEvent(INTERNAL_KEY_UP, this.id, () => {
                 if (this._pressed) {
                     this._pressed = false
-                    if (_userEventsEnabled)
-                        control.raiseEvent(KEY_UP, this.id)
-                    else
-                        control.raiseEvent(SYSTEM_KEY_UP, this.id)
+                    this.raiseButtonUp();
                 }
             }, 16)
             control.internalOnEvent(INTERNAL_KEY_DOWN, this.id, () => {
                 if (!this._pressed) {
-                    this._pressed = true
-                    this.checked = false
-                    if (_userEventsEnabled)
-                        control.raiseEvent(KEY_DOWN, this.id)
-                    else
-                        control.raiseEvent(SYSTEM_KEY_DOWN, this.id)
+                    this._pressed = true;
+                    this._pressedElasped = 0;
+                    this.raiseButtonDown();
                 }
             }, 16)
             if (buttonId && upid && downid) {
                 control.internalOnEvent(buttonId, upid, () => control.raiseEvent(INTERNAL_KEY_UP, this.id), 16)
                 control.internalOnEvent(buttonId, downid, () => control.raiseEvent(INTERNAL_KEY_DOWN, this.id), 16)
             }
+
+            // register button in global list
+            if (!activeButtons) activeButtons = [];
+            activeButtons.push(this);
+        }
+
+        private raiseButtonUp() {
+            if (_userEventsEnabled)
+                control.raiseEvent(KEY_UP, this.id)
+            else
+                control.raiseEvent(SYSTEM_KEY_UP, this.id);
+        }
+
+        private raiseButtonDown() {
+            if (_userEventsEnabled)
+                control.raiseEvent(KEY_DOWN, this.id)
+            else
+                control.raiseEvent(SYSTEM_KEY_DOWN, this.id)
         }
 
         /**
@@ -71,20 +85,17 @@ namespace controller {
         //% weight=96 blockGap=8 help=controller/button/is-pressed
         //% blockId=keyispressed block="is %button **button** pressed"
         isPressed() {
-            return this._pressed
+            return this._pressed;
         }
 
-        /**
-         * Indicates if the button was pressed since the last call
-        */
-        //% weight=95 help=controller/button/was-pressed
-        //% blockId=keywaspressed block="was %button **button** pressed"
-        wasPressed() {
-            if (!this.checked) {
-                this.checked = true
-                return this._pressed
+        __update(dt: number) {
+            if (!this._pressed) return;
+            this._pressedElasped += dt;
+            // still holding?
+            if (this._pressedElasped > REPEAT_DELAY) {
+                this.raiseButtonDown();
+                this._pressedElasped = 0;
             }
-            return false
         }
     }
 
@@ -139,5 +150,13 @@ namespace controller {
 
     export function _setUserEventsEnabled(enabled: boolean) {
         _userEventsEnabled = enabled;
+    }
+
+    /**
+     * Called by the game engine to update and/or raise events
+     */
+    export function __update(dt: number) {
+        if (!activeButtons) return;
+        activeButtons.forEach(btn => btn.__update(dt));
     }
 }
