59e1a3b95142af99197b9a4cee38223a5fee10b5
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-06-15 08:38:06 -0700

    updated ordering

--- libs/core---stm32f401re/platform.cpp
+++ libs/core---stm32f401re/platform.cpp
@@ -15,6 +15,16 @@ static void initRandomSeed() {
     seedRandom(seed);
 }
 
+void platformSendSerial(const char *data, int len) {
+    /*
+    if (!serial) {
+        serial = new codal::_mbed::Serial(USBTX, NC);
+        serial->baud(9600);
+    }
+    serial->send((uint8_t*)data, len);
+    */
+}
+
 void platform_init() {
     initRandomSeed();
     setSendToUART(platformSendSerial);
@@ -27,17 +37,6 @@ void platform_init() {
     }
 */
 }
-
-void platformSendSerial(const char *data, int len) {
-    /*
-    if (!serial) {
-        serial = new codal::_mbed::Serial(USBTX, NC);
-        serial->baud(9600);
-    }
-    serial->send((uint8_t*)data, len);
-    */
-}
-
 
 }
 
