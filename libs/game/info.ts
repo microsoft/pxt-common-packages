a33bde6c0a9dd794586ecdb7cbdd88ed76abd419
Richard Knoll <riknoll@microsoft.com>
2018-06-25 12:10:26 -0700

    Improvements to life/countdown/score HUD

--- libs/game/info.ts
+++ libs/game/info.ts
@@ -1,7 +1,7 @@
 
-/** 
+/**
  * Head-up display
- * 
+ *
 */
 //% color=#AA5585 weight=80 icon="\uf2bb"
 namespace info {
@@ -10,6 +10,17 @@ namespace info {
     let _life: number = null;
     let _hud: boolean = false;
     let _gameEnd: number = undefined;
+    let _heartImage: Image;
+    let _multiplierImage: Image;
+    let _bgColor: number;
+    let _borderColor: number;
+    let _fontColor: number;
+    let _countdownExpired: boolean;
+
+
+    let _lifeOverHandler: () => void;
+    let _countdownEndHandler: () => void;
+
     /**
      * Color of the HUD display
      */
@@ -19,36 +30,61 @@ namespace info {
         if (_hud) return;
         _hud = true;
 
-        const font = image.font8;
-        const maxW = 8;
+        _heartImage = _heartImage || img`
+        . c 2 2 . 2 2 .
+        c 2 2 2 2 2 4 2
+        c 2 2 2 2 4 2 2
+        c 2 2 2 2 2 2 2
+        . c 2 2 2 2 2 .
+        . . c 2 2 2 . .
+        . . . c 2 . . .
+        `;
+
+        _multiplierImage = _multiplierImage || img`
+        1 . . . 1
+        . 1 . 1 .
+        . . 1 . .
+        . 1 . 1 .
+        1 . . . 1
+        `;
+
+        _bgColor = 1;
+        _borderColor = 3;
+        _fontColor = 3;
         game.eventContext().registerFrameHandler(95, () => {
             // show score
             if (_score !== null) {
-                let s = ""
-                if (_highScore)
-                    s += "HI" + _highScore + "  ";
-                s += _score + ""
-                while (s.length < maxW) s = " " + s
-                screen.print(s, screen.width - font.charWidth * maxW - 10, font.charHeight, color, font)    
+                drawScore();
             }
             // show life
             if (_life !== null) {
-                let s = _life + ""
-                screen.print(s, 10, font.charHeight, color, font)
-                if (_life == 0)
-                    game.over();
+                drawLives();
+                if (_life == 0) {
+                    if (_lifeOverHandler) {
+                        _lifeOverHandler();
+                    }
+                    else {
+                        game.over();
+                    }
+                    _life = null;
+                }
             }
             // show countdown
             if (_gameEnd !== undefined) {
+                drawTimer(_gameEnd - control.millis())
                 let t = Math.max(0, _gameEnd - control.millis()) / 1000;
-                // slow down timer
-                // turn to second
-                t = Math.ceil(t);
-                // print time
-                let s = t.toString();
-                screen.print(s, (screen.width - s.length * font.charWidth) / 2, font.charHeight, color, font);                
-                if (t <= 0)
-                    game.over();
+                if (t <= 0) {
+                    t = 0;
+                    if (!_countdownExpired) {
+                        _countdownExpired = true;
+                        if (_countdownEndHandler) {
+                            _countdownEndHandler();
+                        }
+                        else {
+                            game.over();
+                        }
+                    }
+                }
             }
         })
     }
@@ -85,7 +121,7 @@ namespace info {
     /**
      * Get the last recorded high score
      */
-    //% weight=60
+    //% weight=94
     //% blockId=highScore block="high score"
     //% help=info/high-score
     export function highScore(): number {
@@ -96,7 +132,7 @@ namespace info {
     /**
      * Set the score
      */
-    //% weight=94 blockGap=8
+    //% weight=93 blockGap=8
     //% blockId=hudsetScore block="set score to %value"
     //% help=info/set-score
     export function setScore(value: number) {
@@ -108,7 +144,7 @@ namespace info {
      * Change the score by the given amount
      * @param value the amount of change, eg: 1
      */
-    //% weight=93
+    //% weight=92
     //% blockId=hudChangeScoreBy block="change score by %value"
     //% help=info/change-score-by
     export function changeScoreBy(value: number) {
@@ -124,7 +160,7 @@ namespace info {
             updateHighScore(_score);
         }
     }
-    
+
     /**
      * Get the number of lives
      */
@@ -166,14 +202,173 @@ namespace info {
     }
 
     /**
+     * Register code to run when the player's life reaches 0. If this function
+     * is not called then game.over() will be called instead
+     */
+    //% weight=82
+    //% blockId=gamelifeevent block="on life zero"
+    export function onLifeZero(handler: () => void) {
+        _lifeOverHandler = handler;
+    }
+
+    /**
      * Start a countdown of the given duration in seconds
      * @param duration the duration of the countdown, eg: 10
      */
     //% blockId=gamecountdown block="start countdown %duration (s)"
-    //% help=info/start-countdown
+    //% help=info/start-countdown weight=79 blockGap=8
     export function startCountdown(duration: number) {
         initHUD();
         _gameEnd = control.millis() + duration * 1000;
+        _countdownExpired = false;
+    }
+
+    /**
+     * Stops the current countdown and hides the timer UI
+     */
+    //% blockId=gamestopcountdown block="stop countdown" weight=78
+    export function stopCountdown() {
+        _gameEnd = undefined;
+        _countdownExpired = true;
+    }
+
+    /**
+     * Register code to run when the countdown reaches 0. If this function
+     * is not called then game.over() will be called instead
+     */
+    //% blockId=gamecountdownevent block="on countdown end" weight=77
+    export function onCountdownEnd(handler: () => void) {
+        _countdownEndHandler = handler;
+    }
+
+    /**
+     * Replaces the image used to represent the player's lives. Images
+     * should be no larger than 8x8
+     */
+    //%
+    export function setLifeImage(image: Image) {
+        _heartImage = image;
+    }
+
+    /**
+     * Sets the color of the borders around the score, countdown, and life
+     * elements. Defaults to 3
+     * @param color The index of the color (0-15)
+     */
+    export function setBorderColor(color: number) {
+        _borderColor = Math.min(Math.max(color, 0), 15) | 0;
+    }
+
+    /**
+     * Sets the color of the background of the score, countdown, and life
+     * elements. Defaults to 1
+     * @param color The index of the color (0-15)
+     */
+    export function setBackgroundColor(color: number) {
+        _bgColor = Math.min(Math.max(color, 0), 15) | 0;
+    }
+
+    /**
+     * Sets the color of the text used in the score, countdown, and life
+     * elements. Defaults to 3
+     * @param color The index of the color (0-15)
+     */
+    export function setFontColor(color: number) {
+        _fontColor = Math.min(Math.max(color, 0), 15) | 0;
+    }
+
+    function drawTimer(millis: number) {
+        if (millis < 0) millis = 0;
+        millis |= 0;
+
+        const font = image.font8;
+        const smallFont = image.font5;
+        const seconds = Math.idiv(millis, 1000);
+        const width = font.charWidth * 5 - 2;
+        let left = (screen.width >> 1) - (width >> 1) + 1;
+        let color1 = _fontColor;
+        let color2 = _bgColor;
+
+        if (seconds < 10 && (seconds & 1)) {
+            const temp = color1;
+            color1 = color2;
+            color2 = temp;
+        }
+
+        screen.fillRect(left - 3, 0, width + 6, font.charHeight + 3, _borderColor)
+        screen.fillRect(left - 2, 0, width + 4, font.charHeight + 2, color2)
+
+
+        if (seconds < 60) {
+            left += 3
+            const top = 1;
+            const remainder = Math.idiv(millis % 1000, 10);
+
+            screen.print(formatDecimal(seconds) + ".", left, top, color1, font)
+            const decimalLeft = left + 3 * font.charWidth - 2;
+            screen.print(formatDecimal(remainder), decimalLeft, top + 2, color1, smallFont)
+        }
+        else {
+            const minutes = Math.idiv(seconds, 60);
+            const remainder = seconds % 60;
+            screen.print(formatDecimal(minutes) + ":" + formatDecimal(remainder), left, 1, color1, font);
+        }
+    }
+
+    function drawScore() {
+        const s = score() | 0;
+
+        let font: image.Font;
+        let offsetY: number;
+        if (s >= 1000000) {
+            offsetY = 2;
+            font = image.font5;
+        }
+        else {
+            offsetY = 1;
+            font = image.font8;
+        }
+
+        const num = s.toString();
+        const width = num.length * font.charWidth;
+
+        screen.fillRect(screen.width - width - 2, 0, screen.width, image.font8.charHeight + 3, _borderColor)
+        screen.fillRect(screen.width - width - 1, 0, screen.width, image.font8.charHeight + 2, _bgColor)
+        screen.print(num, screen.width - width, offsetY, _fontColor, font);
+    }
+
+    function drawLives() {
+        if (_life <= 0) return;
+
+        const font = image.font8;
+        if (_life <= 4) {
+            screen.fillRect(0, 0, _life * (_heartImage.width + 1) + 3, _heartImage.height + 4, _borderColor);
+            screen.fillRect(0, 0, _life * (_heartImage.width + 1) + 2,  _heartImage.height + 3, _bgColor);
+            for (let i = 0; i < _life; i++) {
+                screen.drawTransparentImage(_heartImage, 1 + i * (_heartImage.width + 1), 1);
+            }
+        }
+        else {
+            const num = _life.toString();
+            const textWidth = num.length * font.charWidth - 1;
+            screen.fillRect(0, 0, _heartImage.width + _multiplierImage.width + textWidth + 5, _heartImage.height + 4, _borderColor)
+            screen.fillRect(0, 0, _heartImage.width + _multiplierImage.width + textWidth + 4, _heartImage.height + 3, _bgColor)
+            screen.drawTransparentImage(_heartImage, 1, 1);
+
+            let mult = _multiplierImage.clone();
+            mult.replace(1, _fontColor);
+
+            screen.drawTransparentImage(mult, _heartImage.width + 2,  font.charHeight - _multiplierImage.height - 1);
+            screen.print(num, _heartImage.width + 3 + _multiplierImage.width, 1, _fontColor, font);
+        }
+    }
+
+    function formatDecimal(val: number) {
+        val |= 0;
+        if (val < 10) {
+            return "0" + val;
+        }
+        return val.toString();
     }
 }
 
