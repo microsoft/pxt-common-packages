c78a8e798bfdc584e1c74a6f7858bb398a079ac1
Michal Moskal <michal@moskal.me>
2018-11-09 10:11:42 -0800

    Add new C++ functions

--- libs/base/sim/control.ts
+++ libs/base/sim/control.ts
@@ -63,6 +63,10 @@ namespace pxsim.control {
     export function dmesgPtr(msg: string, ptr: any) {
         console.log(`DMESG: ${msg} ${ptr}`);
     }   
+    export function dmesgValue(ptr: any) {
+        console.log(`DMESG: ${ptr}`);
+    }
+    export function gc() {}
 
     export function __log(str: string) {
         console.log(str);
