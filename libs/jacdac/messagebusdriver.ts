92860ed02676280e0a59717db49c9793cbdf3c2c
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-14 08:54:21 -0800

    Jacdac broadcast (#468)
    * adding broadcast blocks
    
    * adding icon
    
    * surface service accessor
    
    * adding start stop
    
    * adding raise event

--- libs/jacdac/messagebusdriver.ts
+++ libs/jacdac/messagebusdriver.ts
@@ -1,4 +1,7 @@
 namespace jacdac {
+    // move to codal
+    const JD_MESSAGE_BUS_ID = 2500;
+
     /**
      * A driver that listens for message bus events
      */
@@ -10,6 +13,13 @@ namespace jacdac {
             this.suppressForwarding = false;
         }
 
+        raiseEvent(id: number, value: number) {
+            const event = control.createBuffer(4);
+            event.setNumber(NumberFormat.UInt16LE, 0, id);
+            event.setNumber(NumberFormat.UInt16LE, 2, value);
+            jacdac.sendPacket(event, this.device.driverAddress);
+        }
+
         /**
          * Pipes matching events to JacDac bus
          * @param id 
@@ -19,12 +29,7 @@ namespace jacdac {
             control.dmesg(`jd> msgbus> listen event ${id} ${value}`)        
             control.onEvent(id, value, () => {
                 if (this.suppressForwarding) return;
-
-                const event = control.createBuffer(4);
-                event.setNumber(NumberFormat.UInt16LE, 0, id);
-                event.setNumber(NumberFormat.UInt16LE, 2, value);
-                jacdac.sendPacket(event, this.device.driverAddress);
-
+                this.raiseEvent(id, value);
             }, DAL.MESSAGE_BUS_LISTENER_IMMEDIATE)
         }
 
@@ -43,5 +48,75 @@ namespace jacdac {
             control.dmesg(`jd> msgbus> control packet`)        
             return true;
         }
+    }
+
+    let _messageBus: MessageBusDriver;
+    /**
+     * Gets the message bus driver
+     */
+    export function messageBus(): MessageBusDriver {
+        if (!_messageBus) {
+            control.dmesg("jd> starting message bus")
+            _messageBus = new MessageBusDriver();
+            jacdac.addDriver(_messageBus);
+        }
+        return _messageBus;
+    }
+
+    /**
+     * Pipes specific events through JACDAC
+     */
+    //%
+    export function listenEvent(src: number, value: number) {
+        messageBus().listenEvent(src, value);
+    }
+
+    /**
+     * Gets the message code
+     */
+    //% blockHidden=1 shim=ENUM_GET
+    //% blockId=jacdacMessageCode block="$msg" enumInitialMembers="message1"
+    //% enumName=JacDacMessage enumMemberName=msg enumPromptHint="e.g. Start, Stop, Jump..."
+    export function __message(msg: number): number {
+        return msg;
+    }
+
+    /**
+     * Sends an event over JacDac
+     * @param id 
+     * @param value 
+     */
+    //% blockid=jacdacraisevent
+    //% block="raise event|from %src|with value %value" weight=5
+    export function raiseEvent(src: number, value: number) {
+        messageBus().raiseEvent(src, value);
+    }
+
+    /**
+     * Broadcasts a message over JacDac
+     * @param msg 
+     */
+    //% blockId=jacdacBroadcastMessage block="jacdac send $msg"
+    //% msg.shadow=jacdacMessageCode draggableParameters
+    //% weight=200
+    //% blockGap=8
+    //% help=jacdac/send-message
+    export function sendMessage(msg: number): void {
+        // 0 is MICROBIT_EVT_ANY, shifting by 1
+        messageBus().raiseEvent(JD_MESSAGE_BUS_ID, msg + 1);
+    }
+
+    /**
+     * Registers code to run for a particular message
+     * @param msg 
+     * @param handler 
+     */
+    //% blockId=jacdacOnMessageReceived block="on jacdac $msg received"
+    //% msg.shadow=jacdacMessageCode draggableParameters
+    //% weight=199
+    //% help=jacdac/on-received-message
+    export function onReceivedMessage(msg: number, handler: () => void) {
+        messageBus(); // start message bus
+        control.onEvent(JD_MESSAGE_BUS_ID, msg + 1, handler);
     }
 }
---
+++ libs/jacdac/ns.ts
@@ -0,0 +1,7 @@
+/**
+ * A Joint Asynchronous Communications, Device Agnostic Control.
+ */
+//% color="#E79251" advanced=true weight=10 icon="\uf185"
+namespace jacdac {
+
+}
