919073ca4c866615ce8b7fec5a0a5d4b9c523693
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-14 22:44:29 -0800

    basic console support

--- libs/game/console.ts
+++ libs/game/console.ts
@@ -1,161 +1,114 @@
-/**
- * Basic screen display functionalities
- */
-//% weight=98 icon="\uf108" color="#311557"
-//% groups='["Screen", "Charts"]'
-namespace display {
-    const textOffset = 4;
-    const lineOffset = 2;
-
-    /**
-     * Gets the text line height
-     */
-    export function lineHeight(): number {
-        return image.font8.charHeight + lineOffset;
+namespace game {
+    export function showConsole() {
+        const csl = new Console();
+        game.onUpdate(() => {
+            if (controller.down.isPressed())
+                csl.scroll(1);
+            else if (controller.up.isPressed())
+                csl.scroll(-1);
+            else if (controller.B.isPressed()) {
+                csl.hide();
+            }
+        })
+        pauseUntil(() => csl.hidden());
     }
 
-    /**
-     * Number of lines
-     */
-    export function lineCount(): number {
-        return ((screen.height - textOffset) / lineHeight()) >> 0
-    }
+    class Console {
+        private textOffset: number;
+        private lineOffset: number;
+        private maxLines: number;
+        private screenLines: number;
+        private lines: string[];
+        private scrollPosition: number;
+        private listener: (priority: ConsolePriority, text: string) => void;
 
-    /**
-     * Show text on the screen at a specific line.
-     * @param text the text to print on the screen, eg: "Hello world"
-     * @param line the line number to print the text at (starting at 1), eg: 1
-     */
-    //% blockId=displayshowstring block="show string %text|at line %line"
-    //% weight=98 inlineInputMode="inline" blockGap=8
-    //% help=display/show-string
-    export function showString(text: string, line: number) {
-        // line indexing starts at 1.
-        line = (line - 1) >> 0;
-        const nlines = lineCount();
-        if (line < 0 || line >= nlines) return; // out of screen
-
-        const h = lineHeight();
-        const y = textOffset + h * line;
-        const s = scene.backgroundImage();
-        s.fillRect(0, y, screen.width, h, 0); // clear background
-        s.print(text, textOffset, y, 1);
-    }
-
-    /**
-     * Shows a number on the screen
-     * @param value the numeric value
-     * @param line the line number to print the text at (starting at 1), eg: 1
-     */
-    //% blockId=displayshownumber block="show number %name|at line %line"
-    //% weight=96 inlineInputMode="inline" blockGap=8
-    //% help=display/show-number
-    //% line.min=1 line.max=10
-    export function showNumber(value: number, line: number) {
-        showString("" + value, line);
-    }
-
-    /**
-     * Shows a name, value pair on the screen
-     * @param value the numeric value
-     * @param line the line number to print the text at (starting at 1), eg: 1
-     */
-    //% blockId=displayshowvalue block="show value %name|: %text|at line %line"
-    //% weight=96 inlineInputMode="inline" blockGap=8
-    //% help=display/show-value
-    //% line.min=1 line.max=10
-    export function showValue(name: string, value: number, line: number) {
-        value = Math.round(value * 1000) / 1000;
-        showString((name ? name + ": " : "") + value, line);
-    }
-
-    /**
-     * Clear the screen
-     */
-    //% blockId=displayclear block="clear display"
-    //% weight=90
-    //% help=display/clear
-    export function clear() {
-        const s = scene.backgroundImage();
-        s.fill(0)
-    }
-
-    /**
-     * Sends the log messages to the brick screen and uses the brick up and down buttons to scroll.
-     */
-    //% blockId=displayshowconsole block="show console"
-    //% weight=1
-    //% help=display/show-console
-    export function showConsole(): void {
-        display.text.scroll(0);
-        console.log('log')
-    }
-}
-
-namespace display.text {
-    export let maxLines = 200;
-    let screenLines = 1;
-    let lines: string[];
-    let scrollPosition = 0;
-
-    function init() {
-        if (!lines) {
+        constructor() {
+            controller._setUserEventsEnabled(false);
             game.pushScene();
-            screenLines = display.lineCount();
-            scene.setBackgroundColor(0x00ff00)
+
+            this.textOffset = 4;
+            this.lineOffset = 2;
+            this.maxLines = 200;
+            this.screenLines = 1;
+            this.scrollPosition = 0;
+
+            this.screenLines = this.lineCount();
             const img = image.create(screen.width, screen.height);
-            scene.setBackgroundImage(img)
-            console.addListener(log);
-            lines = [];
-
-            controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
-                display.text.scroll(-1);
-            })
-            controller.up.onEvent(ControllerButtonEvent.Repeated, function () {
-                display.text.scroll(-1);
-            })
-            controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
-                display.text.scroll(1);
-            })
-            controller.down.onEvent(ControllerButtonEvent.Repeated, function () {
-                display.text.scroll(1);
-            })
+            scene.setBackgroundImage(img);
+            this.listener = (priority, text) => this.log(priority, text);
+            console.addListener(this.listener);
+            this.lines = [];
         }
-    }
 
-    function printLog() {
-        display.clear();
-        if (!lines) return;
-        screenLines = display.lineCount();
-        const h = display.lineHeight();
-        for (let i = 0; i < screenLines; ++i) {
-            const line = lines[i + scrollPosition];
-            if (line)
-                display.showString(line, i + 1);
+        hidden() {
+            return !this.lines;
         }
-    }
 
-    export function scroll(pos: number) {
-        init();
-        if (!pos) return;
-
-        scrollPosition += pos >> 0;
-        if (scrollPosition >= lines.length) scrollPosition = lines.length - 1;
-        if (scrollPosition < 0) scrollPosition = 0;
-        printLog();
-    }
-
-    function log(msg: string): void {
-        lines.push(msg);
-        if (lines.length + 5 > maxLines) {
-            lines.splice(0, lines.length >> 1);
-            scrollPosition = Math.min(scrollPosition, lines.length - 1)
+        public hide() {
+            console.removeListener(this.listener);
+            controller._setUserEventsEnabled(true);
+            game.popScene();
         }
-        // move down scroll once it gets large than the screen
-        if (lines.length > screenLines
-            && lines.length >= scrollPosition + screenLines) {
-            scrollPosition++;
+
+        lineHeight(): number {
+            return image.font8.charHeight + this.lineOffset;
         }
-        printLog();
+
+        lineCount(): number {
+            return ((screen.height - this.textOffset) / this.lineHeight()) >> 0
+        }
+
+        showString(text: string, line: number) {
+            // line indexing starts at 1.
+            line = (line - 1) >> 0;
+            const nlines = this.lineCount();
+            if (line < 0 || line >= nlines) return; // out of screen
+
+            const h = this.lineHeight();
+            const y = this.textOffset + h * line;
+            const s = scene.backgroundImage();
+            s.fillRect(0, y, screen.width, h, 0); // clear background
+            s.print(text, this.textOffset, y, 1);
+        }
+
+        clear() {
+            const s = scene.backgroundImage();
+            s.fill(0)
+        }
+
+        printLog() {
+            this.clear();
+            if (!this.lines) return;
+            this.screenLines = this.lineCount();
+            const h = this.lineHeight();
+            for (let i = 0; i < this.screenLines; ++i) {
+                const line = this.lines[i + this.scrollPosition];
+                if (line)
+                    this.showString(line, i + 1);
+            }
+        }
+
+        scroll(pos: number) {
+            if (!pos) return;
+
+            this.scrollPosition += pos >> 0;
+            if (this.scrollPosition >= this.lines.length) this.scrollPosition = this.lines.length - 1;
+            if (this.scrollPosition < 0) this.scrollPosition = 0;
+            this.printLog();
+        }
+
+        log(priority: ConsolePriority, msg: string): void {
+            this.lines.push(msg);
+            if (this.lines.length + 5 > this.maxLines) {
+                this.lines.splice(0, this.lines.length >> 1);
+                this.scrollPosition = Math.min(this.scrollPosition, this.lines.length - 1)
+            }
+            // move down scroll once it gets large than the screen
+            if (this.lines.length > this.screenLines
+                && this.lines.length >= this.scrollPosition + this.screenLines) {
+                this.scrollPosition++;
+            }
+            this.printLog();
+        }
     }
 }
