d303c8a461013ba080f2ec57a8d89de0ba3aefbd
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-15 23:40:07 -0800

    showing the menu

--- libs/game/systemmenu.ts
+++ libs/game/systemmenu.ts
@@ -1,40 +1,44 @@
 namespace scene {
-    let suppressMenu = false;
+    let systemMenuActive = false;
     export function registerSystemMenu() {
-        if (suppressMenu) {
+        if (systemMenuActive) {
             console.log('menu suppresed')
             return;
         }
 
         controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
             console.log('creating menu')
-            suppressMenu = true;
+            systemMenuActive = true;
             const initHeight = 12;
+            const finalHeight = screen.height - 10;
+            const finalWidth = screen.width - 10;
             const f = new menu.RoundedFrame(5, 1, 3);
             const b = new menu.Bounds(initHeight, initHeight);
             b.left = 30;
             b.top = 30;
             b.appendChild(f)
-            const root = new menu.JustifiedContent(b, Alignment.Center, Alignment.Center);
-            const list = new menu.VerticalList(root.width - 10, root.height - 10, root.width - 20, root.height - 20);
-            root.appendChild(list)
+            const list = new menu.VerticalList(finalWidth - 8, finalHeight - 8);
             list.addItem("console", 0);
             list.addItem("volume up", 1);
             list.addItem("volume down", 2);
+            const root = new menu.JustifiedContent(b, Alignment.Center, Alignment.Center);
+            root.appendChild(list)
             menu.setRoot(root);
 
             const vert = b.animate(menu.setHeight)
                 .from(initHeight)
-                .to(100)
+                .to(finalHeight)
                 .duration(200);
 
             const hori = b.animate(menu.setWidth)
                 .from(initHeight)
-                .to(150)
+                .to(finalWidth)
                 .duration(200);
 
             vert.chain(hori);
             vert.start();
+
+            list.show();
 
             // b handler
             controller.B.onEvent(ControllerButtonEvent.Pressed, () => {
@@ -51,7 +55,7 @@ namespace scene {
 
                 pauseUntil(() => !hori.running);
                 root.dispose();
-                suppressMenu = false;
+                systemMenuActive = false;
             })
         })
     }
