cebf1d0e22f4a4722ea2f435e60baf4097ee58c4
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-16 09:37:06 -0800

    fixing sim build

--- libs/jacdac/sim/jacdac.ts
+++ libs/jacdac/sim/jacdac.ts
@@ -7,7 +7,7 @@ namespace pxsim.jacdac {
         // TODO
     }
 
-    export function __internalSendPacket(packet: RefBuffer, address: number): number {
+    export function __internalSendPacket(packet: pxsim.RefBuffer, address: number): number {
         /* TODOs
         pxsim.Runtime.postMessage(<pxsim.SimulatorJacDacPacketMessage>{ 
             type: "jacdac",
@@ -22,17 +22,17 @@ namespace pxsim.jacdac {
     export function __internalAddDriver(
         driverType: number,
         deviceClass: number,
-        methods: ((p: RefBuffer) => void)[]):
+        methods: ((p: pxsim.RefBuffer) => void)[]):
         JacDacDriverStatus {
         // TODO keep track        
         return new JacDacDriverStatus(driverType, deviceClass, methods);
     }
 }
 
-export class JacDacDriverStatus {
-    constructor(private driverType: number, private deviceClass: number, private methods: ((p: Buffer) => void)[]) {
+class JacDacDriverStatus {
+    constructor(private driverType: number, private deviceClass: number, private methods: ((p: pxsim.RefBuffer) => void)[]) {
         this.serialNumber = Math.random();
-        this.id = pxsim.board().bus.allocateNotifyEvent();
+        this.id = pxsim.control.allocateNotifyEvent();
         this.isPaired = false;
         this.isPairable = false; // TODO
         this.isVirtualDriver = false; // TODO
@@ -69,15 +69,15 @@ export class JacDacDriverStatus {
 
     /** Get device class. */
     //% property shim=JacDacDriverStatusMethods::driverClass
-    driverClass: uint32;
+    driverClass: number;
 
     /** Get device class. */
     //% property shim=JacDacDriverStatusMethods::driverAddress
-    driverAddress: uint8;
+    driverAddress: number;
 
     /** Get device id for events. */
     //% property shim=JacDacDriverStatusMethods::id
-    id: boolean;
+    id: number;
 
     /** If paired, paired instance address */
     //% property shim=JacDacDriverStatusMethods::isPairedInstanceAddress
