779c5f6dfc1b382a08aace6d7301d131fba1c0ff
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-19 11:36:06 -0800

    Jacdac log (#479)
    * moving driver class
    
    * renaming
    
    * removing sniffer
    
    * fix off by 4 bug
    
    * remove serial
    
    * removing logging

--- libs/jacdac/loggerdriver.ts
+++ libs/jacdac/loggerdriver.ts
@@ -1,33 +1,35 @@
 namespace jacdac {
-    let _loggerDriver: LoggerVirtualDriver;
+    // TODO allocate ID in DAL
+    const LOGGER_DRIVER_CLASS = 4220;
+
+    let _logBroadcastDriver: LoggerBroadcastDriver;
     /**
      * Sends console messages over JacDac
      */
     //% blockId=jacdac_broadcast_console block="jacdac broadcast console"
     export function broadcastConsole() {
-        if (!_loggerDriver)
-            _loggerDriver = new LoggerVirtualDriver();
+        if (!_logBroadcastDriver)
+            _logBroadcastDriver = new LoggerBroadcastDriver();
     }
 
     export function suppressLogBroadcast(logf: () => void) {
         // pipe to console
-        if (_loggerDriver) // avoid cyclic repetition of messages
-            _loggerDriver.suppressForwading = true;
+        if (_logBroadcastDriver) // avoid cyclic repetition of messages
+            _logBroadcastDriver.suppressForwading = true;
         logf();
-        if (_loggerDriver)
-            _loggerDriver.suppressForwading = false;
+        if (_logBroadcastDriver)
+            _logBroadcastDriver.suppressForwading = false;
     }
 
 
-    class LoggerVirtualDriver extends JacDacDriver {
+    class LoggerBroadcastDriver extends JacDacDriver {
         public suppressForwading: boolean;
         constructor() {
-            super("log", DriverType.VirtualDriver, 20); // TODO pickup type from DAL
-            this.suppressForwading = false;
+            super("log", DriverType.VirtualDriver, LOGGER_DRIVER_CLASS, true); // TODO pickup type from DAL
             // send to other devices
-            console.addListener((priority, text) => this.broadcastLog(priority, text));
-
+            this.suppressForwading = false;
             jacdac.addDriver(this);
+            console.addListener((priority, text) => this.broadcastLog(priority, text));
         }
 
         /**
@@ -40,12 +42,11 @@ namespace jacdac {
 
             let cursor = 0;
             while (cursor < str.length) {
-                const txLength = Math.min(str.length - cursor, DAL.JD_SERIAL_DATA_SIZE - 1 - 4);
+                const txLength = Math.min(str.length - cursor, DAL.JD_SERIAL_DATA_SIZE - 1);
                 const buf = control.createBuffer(txLength + 1);
                 buf.setNumber(NumberFormat.UInt8LE, 0, priority);
-                buf.setNumber(NumberFormat.UInt32LE, 1, this.device.serialNumber);
                 for (let i = 0; i < txLength; i++) {
-                    buf.setNumber(NumberFormat.UInt8LE, i + 5, str.charCodeAt(i + cursor));
+                    buf.setNumber(NumberFormat.UInt8LE, i + 1, str.charCodeAt(i + cursor));
                 }
                 jacdac.sendPacket(buf, this.device.driverAddress);
                 cursor += txLength;
@@ -53,20 +54,19 @@ namespace jacdac {
         }
     }
 
-    let _logListenerDriver: LoggerHostDriver;
-
+    let _logListenerDriver: LoggerListenDriver;
     /**
      * Listens for console messages from other devices
      */
     //% blockId=jacdac_listen_console block="jacdac listen console"
     export function listenConsole() {
         if (!_logListenerDriver)
-            _logListenerDriver = new LoggerHostDriver();
+            _logListenerDriver = new LoggerListenDriver();
     }
 
-    class LoggerHostDriver extends JacDacDriver {
+    class LoggerListenDriver extends JacDacDriver {
         constructor() {
-            super("log", DriverType.HostDriver, 20); // TODO pickup type from DAL
+            super("log", DriverType.HostDriver, LOGGER_DRIVER_CLASS); // TODO pickup type from DAL
             jacdac.addDriver(this);
         }
 
@@ -79,61 +79,15 @@ namespace jacdac {
             // shortcut
             if (priority < console.minPriority) return true;
 
-            // who sent this?            
-            const serial = packet.data.getNumber(NumberFormat.UInt32LE, 1);
-
             // send message to console
             let str = "";
-            for (let i = 5; i < packetSize; i++)
+            for (let i = 1; i < packetSize; i++)
                 str += String.fromCharCode(packet.data.getNumber(NumberFormat.UInt8LE, i));
 
-            // pipe to console
-            suppressLogBroadcast(() => this.log(str));
+            // pipe to console TODO suppress forwarding
+            console.add(priority, str);
 
             return true;
-        }
-    }
-
-    let _snifferLoggerDriver: SnifferLoggerDriver = undefined;
-    /**
-     * Enables or disables all jacdac activity to the console
-     */
-    export function sniffAllPacketsToConsole(enabled = true) {
-        if (!_snifferLoggerDriver)
-            _snifferLoggerDriver = new SnifferLoggerDriver();
-        _snifferLoggerDriver.enabled = enabled;
-    }
-
-    class SnifferLoggerDriver extends JacDacDriver {
-        public enabled: boolean;
-        constructor() {
-            super("snif", DriverType.SnifferDriver, 0);
-            this.enabled = true;
-            jacdac.addDriver(this);
-        }
-
-        public handleControlPacket(pkt: Buffer): boolean {
-            if (this.enabled) {
-                const ctrl = new ControlPacket(pkt);
-                suppressLogBroadcast(() => this.log(`ctrl>from ${ctrl.serialNumber}:${ctrl.address}>${ctrl.data.toHex()}`))
-            }
-            return super.handleControlPacket(pkt);
-        }
-
-        public handlePacket(pkt: Buffer): boolean {
-            if (this.enabled) {
-                const jd = new JDPacket(pkt);
-                suppressLogBroadcast(() => this.log(`ctrl>${jd.address}>${jd.data.toHex()}`))
-            }
-            return super.handlePacket(pkt);
-        }
-
-        public deviceConnected(): void {
-            suppressLogBroadcast(() => this.log(`dev>con`));
-        }
-
-        public deviceRemoved(): void {
-            suppressLogBroadcast(() => this.log(`dev>dis`));
         }
     }
 }
