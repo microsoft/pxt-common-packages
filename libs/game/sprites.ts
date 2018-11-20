754b60684a35b2208b8f5abab097a45e88d1e890
Joey Wunderlich <jwunderl@users.noreply.github.com>
2018-11-19 20:54:37 -0800

    Reword controlSprite and reorder toolbox (#481)
    * reword control sprite with, swap game/sprites
    
    * rename ts functions to match block name
    
    * move game below controller

--- libs/game/sprites.ts
+++ libs/game/sprites.ts
@@ -12,7 +12,7 @@ Frame handlers:
 /**
  * Sprites on screen
  */
-//% weight=98 color="#4B7BEC" icon="\uf1d8"
+//% weight=99 color="#4B7BEC" icon="\uf1d8"
 //% groups='["Create", "Properties", "Overlaps", "Collisions", "Lifecycle"]'
 namespace sprites {
 
