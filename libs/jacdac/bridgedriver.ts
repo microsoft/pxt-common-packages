dc60bd456efd3a878b751c604d2afe83f2044f0c
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-21 16:42:41 -0800

    adding logger

--- libs/jacdac/bridgedriver.ts
+++ libs/jacdac/bridgedriver.ts
@@ -13,4 +13,32 @@ namespace jacdac {
             this._proxy.setBridge();
         }
     }
+
+    class LogAllDriver extends BridgeDriver {
+        constructor() {
+            super("log")
+        }
+
+        handleControlPacket(pkt: Buffer): boolean {
+            const packet = new ControlPacket(pkt);
+            console.log(`jd>cp ${packet.address}=${packet.driverClass} ${packet.flags}`)
+            return true;
+        }
+
+        handlePacket(pkt: Buffer): boolean {
+            const packet = new JDPacket(pkt);
+            console.log(`jd>p ${packet.address} ${packet.size}b`)
+            return true;
+        }
+    }
+
+    let _logAllDriver : LogAllDriver;
+    /**
+     * Show ALL jacdac packets on console
+     */
+    export function logAllPackets() {
+        if (!_logAllDriver)
+            _logAllDriver = new LogAllDriver();
+        _logAllDriver.enable();
+    }
 }
