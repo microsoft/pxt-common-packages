dcf5a07047174f3eae036618495f94a093c4dee1
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-21 22:29:04 -0800

    enough sim to avoid crashing

--- libs/jacdac/sim/jacdac.ts
+++ libs/jacdac/sim/jacdac.ts
@@ -25,23 +25,58 @@ namespace pxsim.jacdac {
         methods: ((p: pxsim.RefBuffer) => boolean)[],
         controlData: pxsim.RefBuffer
     ): JacDacDriverStatus {
-        // TODO keep track        
         return new JacDacDriverStatus(driverType, deviceClass, methods, controlData);
+    }
+
+    export class JDDevice {
+        buf: pxsim.RefBuffer;
+        constructor(buf: pxsim.RefBuffer) {
+            this.buf = buf;
+        }
+
+        static mk(address: number, flags: number, serialNumber: number, driverClass: number) {
+            const buf = BufferMethods.createBuffer(12);
+            BufferMethods.setNumber(buf, BufferMethods.NumberFormat.UInt8LE, 0, address);
+            BufferMethods.setNumber(buf, BufferMethods.NumberFormat.UInt8LE, 1, 0); // rolling counter
+            BufferMethods.setNumber(buf, BufferMethods.NumberFormat.UInt16LE, 2, flags);
+            BufferMethods.setNumber(buf, BufferMethods.NumberFormat.UInt16LE, 4, serialNumber);
+            BufferMethods.setNumber(buf, BufferMethods.NumberFormat.UInt16LE, 8, driverClass);
+            return new JDDevice(buf);
+        }
+
+        get driverAddress(): number {
+            return BufferMethods.getNumber(this.buf, BufferMethods.NumberFormat.UInt8LE, 0);
+        }
+        get rollingCounter(): number {
+            return BufferMethods.getNumber(this.buf, BufferMethods.NumberFormat.UInt8LE, 1);
+        }
+        get flags(): number {
+            return BufferMethods.getNumber(this.buf, BufferMethods.NumberFormat.UInt16LE, 2);
+        }
+        get serialNumber(): number {
+            return BufferMethods.getNumber(this.buf, BufferMethods.NumberFormat.UInt32LE, 4);
+        }
+        get driverClass(): number {
+            return BufferMethods.getNumber(this.buf, BufferMethods.NumberFormat.UInt32LE, 8);
+        }
     }
 }
 
 class JacDacDriverStatus {
-    constructor(private driverType: number, private deviceClass: number, private methods: ((p: pxsim.RefBuffer) => boolean)[], private controlData: pxsim.RefBuffer) {
-        this.id = pxsim.control.allocateNotifyEvent();
-    }
-
-    /** Get device id for events. */
-    //% property shim=JacDacDriverStatusMethods::id
+    device: pxsim.RefBuffer;
     id: number;
-
-    /** If paired, paired instance address */
-    //% property shim=JacDacDriverStatusMethods::isPairedInstanceAddress
+    constructor(
+        driverType: number,
+        deviceClass: number,
+        public methods: ((p: pxsim.RefBuffer) => boolean)[],
+        public controlData: pxsim.RefBuffer) {
+        this.id = pxsim.control.allocateNotifyEvent();
+        this.device = pxsim.jacdac.JDDevice.mk(0, driverType, 0, deviceClass).buf;
+    }
     isPairedInstanceAddress(address: number): number {
         return 0;
+    }
+    setBridge(): void {
+
     }
 }
