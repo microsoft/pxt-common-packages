44b371d1e4b398b71963cd241a6b9b7baf0b8c3f
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-26 08:21:14 -0800

    Basic support for JACDAC in sim (#497)
    * enough sim to avoid crashing
    
    * avoid naming issues
    
    * more plumbing
    
    * basic plubming
    
    * adding state
    
    * more wiring
    
    * updated streaming sensor interface
    
    * fixing signature

--- libs/jacdac/jacdac.ts
+++ libs/jacdac/jacdac.ts
@@ -11,6 +11,7 @@ enum JacDacDriverEvent {
  * JACDAC protocol support
  */
 namespace jacdac {
+    export type MethodCollection = ((p:Buffer) => boolean)[];
     // TODO allocate ID in DAL
     export const LOGGER_DRIVER_CLASS = 4220;
     // TODO allocate ID in DAL
@@ -167,11 +168,6 @@ namespace jacdac {
     //% shim=pxt::programHash
     export function programHash(): number { return 0 }
 
-    //% shim=jacdac::__internalAddDriver
-    function __internalAddDriver(driverType: number, deviceClass: number, methods: ((p: Buffer) => void)[], controlData: Buffer): JacDacDriverStatus {
-        return null
-    }
-
     /**
      * Adds a JacDac device driver
      * @param n driver
@@ -183,11 +179,12 @@ namespace jacdac {
         }
 
         n.log(`add t${n.driverType} c${n.deviceClass}`)
-        n.setProxy(__internalAddDriver(n.driverType, n.deviceClass, 
+        const proxy = __internalAddDriver(n.driverType, n.deviceClass, 
             [(p: Buffer) => n.handlePacket(p),
              (p: Buffer) => n.handleControlPacket(p)],
             n.controlData
-        ));
+        );
+        n.setProxy(proxy);
     }
 
     /**
--- libs/jacdac/sim/jacdac.ts
+++ libs/jacdac/sim/jacdac.ts
@@ -1,21 +1,21 @@
 namespace pxsim.jacdac {
+
     export function start() {
-        // TODO
+        const state = getJacDacState();
+        if (!state) return;
+        state.start();
     }
 
     export function stop() {
-        // TODO
+        const state = getJacDacState();
+        if (!state) return;
+        state.stop();
     }
 
     export function __internalSendPacket(packet: pxsim.RefBuffer, address: number): number {
-        /* TODOs
-        pxsim.Runtime.postMessage(<pxsim.SimulatorJacDacPacketMessage>{ 
-            type: "jacdac",
-            packetType: "pkt",
-            packet: packet.data,
-            address
-        });
-        */
+        const state = getJacDacState();
+        if (state)
+            state.sendPacket(packet, address);
         return 0;
     }
 
@@ -24,24 +24,82 @@ namespace pxsim.jacdac {
         deviceClass: number,
         methods: ((p: pxsim.RefBuffer) => boolean)[],
         controlData: pxsim.RefBuffer
-    ): JacDacDriverStatus {
-        // TODO keep track        
-        return new JacDacDriverStatus(driverType, deviceClass, methods, controlData);
+    ): pxsim.JacDacDriverStatus {
+        const state = getJacDacState();
+        const d = new pxsim.JacDacDriverStatus(driverType, deviceClass, methods, controlData);
+        if (state)
+            state.addDriver(d);
+        return d;
+    }
+
+    export class JDDevice {
+        buf: pxsim.RefBuffer;
+        constructor(buf: pxsim.RefBuffer) {
+            this.buf = buf;
+        }
+
+        static mk(address: number, flags: number, serialNumber: number, driverClass: number) {
+            const buf = BufferMethods.createBuffer(12);
+            BufferMethods.setNumber(buf, BufferMethods.NumberFormat.UInt8LE, 0, address);
+            BufferMethods.setNumber(buf, BufferMethods.NumberFormat.UInt8LE, 1, 0); // rolling counter
+            BufferMethods.setNumber(buf, BufferMethods.NumberFormat.UInt16LE, 2, flags);
+            BufferMethods.setNumber(buf, BufferMethods.NumberFormat.UInt16LE, 4, serialNumber);
+            BufferMethods.setNumber(buf, BufferMethods.NumberFormat.UInt16LE, 8, driverClass);
+            return new JDDevice(buf);
+        }
+
+        get driverAddress(): number {
+            return BufferMethods.getNumber(this.buf, BufferMethods.NumberFormat.UInt8LE, 0);
+        }
+        set driverAddress(value: number) {
+            BufferMethods.setNumber(this.buf, BufferMethods.NumberFormat.UInt8LE, 0, value);
+        }
+        get rollingCounter(): number {
+            return BufferMethods.getNumber(this.buf, BufferMethods.NumberFormat.UInt8LE, 1);
+        }
+        get flags(): number {
+            return BufferMethods.getNumber(this.buf, BufferMethods.NumberFormat.UInt16LE, 2);
+        }
+        get serialNumber(): number {
+            return BufferMethods.getNumber(this.buf, BufferMethods.NumberFormat.UInt32LE, 4);
+        }
+        get driverClass(): number {
+            return BufferMethods.getNumber(this.buf, BufferMethods.NumberFormat.UInt32LE, 8);
+        }
+    }
+
+
+}
+namespace pxsim {
+    export class JacDacDriverStatus {
+        dev: pxsim.jacdac.JDDevice;
+        device: pxsim.RefBuffer;
+        id: number;
+        constructor(
+            driverType: number,
+            deviceClass: number,
+            public methods: ((p: pxsim.RefBuffer) => boolean)[],
+            public controlData: pxsim.RefBuffer) {
+            this.id = pxsim.control.allocateNotifyEvent();
+            this.dev = pxsim.jacdac.JDDevice.mk(0, driverType, 0, deviceClass);
+            this.device = this.dev.buf;
+        }
     }
 }
-
-class JacDacDriverStatus {
-    constructor(private driverType: number, private deviceClass: number, private methods: ((p: pxsim.RefBuffer) => boolean)[], private controlData: pxsim.RefBuffer) {
-        this.id = pxsim.control.allocateNotifyEvent();
-    }
-
-    /** Get device id for events. */
-    //% property shim=JacDacDriverStatusMethods::id
-    id: number;
-
-    /** If paired, paired instance address */
-    //% property shim=JacDacDriverStatusMethods::isPairedInstanceAddress
-    isPairedInstanceAddress(address: number): number {
+namespace pxsim.JacDacDriverStatusMethods {
+    export function isPairedInstanceAddress(proxy: JacDacDriverStatus, address: number): number {
         return 0;
+    }
+    export function setBridge(proxy: JacDacDriverStatus): void {
+
+    }
+    export function id(proxy: JacDacDriverStatus): number {
+        return proxy.id;
+    }
+    export function device(proxy: JacDacDriverStatus): pxsim.RefBuffer {
+        return proxy.device;
+    }
+    export function isConnected(proxy: JacDacDriverStatus): boolean {
+        return false;
     }
 }
