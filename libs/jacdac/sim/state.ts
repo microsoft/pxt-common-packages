24aa0f7c69e527ec38f73c5ccee9e29521c7f2f7
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-26 12:02:23 -0800

    plumbing to receive jacadac message

--- libs/jacdac/sim/state.ts
+++ libs/jacdac/sim/state.ts
@@ -1,11 +1,19 @@
 namespace pxsim {
+    export interface SimulatorJacDacMessage extends pxsim.SimulatorBroadcastMessage {
+        type: "jacdac";
+        broadcast: true;
+        address: number;
+        packet: Uint8Array;
+    }
+
     export class JacDacState {
         drivers: JacDacDriverStatus[];
         running = false;
 
-        constructor() {
+        constructor(board: BaseBoard) {
             this.drivers = [new JacDacDriverStatus(0, 0, undefined, undefined)]
             this.drivers[0].dev.driverAddress = 0; // logic driver is always at address 0
+            board.addMessageListener(this.processMessage.bind(this));
         }
 
         start() {
@@ -22,8 +30,22 @@ namespace pxsim {
         }
 
         sendPacket(packet: pxsim.RefBuffer, address: number): number {
+            if (this.running)
+                Runtime.postMessage(<SimulatorJacDacMessage>{
+                    type: "jacdac",
+                    broadcast: true,
+                    address: address,
+                    packet: packet.data
+                })
             return 0;
         }
+
+        processMessage(msg: pxsim.SimulatorMessage) {
+            if (msg && msg.type == "jacdac") {
+                const jdmsg = msg as pxsim.SimulatorJacDacMessage;
+                // TODO
+            }
+        }   
     }
 
     export interface JacDacBoard extends CommonBoard {
