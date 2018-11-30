14fca2ea883d57c4fb9c04423d3744a707860800
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-11-29 21:57:27 -0800

    synching light create with cpx

--- libs/light/create.ts
+++ libs/light/create.ts
@@ -6,7 +6,7 @@ namespace light {
      * @param numleds number of leds in the strip, eg: 30
      * @param mode the light encoding mode for different LED strips, eg: NeoPixelMode.RGB_GRB
      */
-    //% blockId="neopixel_create_strip" block="create strip||on %pin with %numleds pixels"
+    //% blockId="neopixel_create_strip" block="create strip on %pin with %numleds pixels"
     //% help="light/create-strip"
     //% trackArgs=0,2
     //% parts="neopixel"
