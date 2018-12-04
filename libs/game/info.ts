d979f78783ef6935c75847e15c50336feee64d98
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-12-04 11:30:30 -0800

    lower animaton weight

--- libs/game/info.ts
+++ libs/game/info.ts
@@ -3,7 +3,7 @@
  * Head-up display
  *
 */
-//% color=#AA5585 weight=80 icon="\uf2bb"
+//% color=#AA5585 weight=80 icon="\uf2bb" blockGap=8
 //% groups='["Score", "Life", "Countdown"]'
 namespace info {
 
