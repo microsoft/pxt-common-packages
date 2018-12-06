7a77ca435d71fc0a80f8cbc983d20878d0155154
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-12-06 04:27:24 -0800

    Remove volume menu (#534)
    * moving out volume menu items
    
    * support for repeat entries

--- libs/game/systemmenu.ts
+++ libs/game/systemmenu.ts
@@ -4,13 +4,15 @@ namespace scene.systemMenu {
     interface MenuItem {
         name: () => string;
         handler: () => void;
+        repeat?: boolean;
     }
     let customItems: MenuItem[] = undefined;
-    export function addEntry(name: () => string, handler: () => void) {
+    export function addEntry(name: () => string, handler: () => void, repeat?: boolean) {
         if (!customItems) customItems = [];
         customItems.push({
             name: name,
-            handler: handler
+            handler: handler,
+            repeat: repeat
         });
     }
 
@@ -18,20 +20,9 @@ namespace scene.systemMenu {
         if (active) return; // don't show system menu, while in system menu
 
         controller.menu.onEvent(ControllerButtonEvent.Pressed, function () {
-            active = true;            
+            active = true;
             let itemHandler: () => void = undefined;
-            const m = new menu.Menu();            
-            m.addItem("volume up", () => {
-                const v = music.volume();
-                music.setVolume(v + 32);
-                music.playTone(440, 500);
-            });
-            m.addItem("volume down", () => {
-                const v = music.volume();
-                music.setVolume(v - 32);
-                music.playTone(440, 500);
-            });
-            m.addItem("brightness", () => {});
+            const m = new menu.Menu();
             m.addItem(game.stats ? "hide stats" : "show stats", () => {
                 game.stats = !game.stats;
                 m.hide();
@@ -48,10 +39,13 @@ namespace scene.systemMenu {
             if (customItems)
                 customItems.forEach(item => {
                     m.addItem(item.name(), () => {
-                        m.hide();
-                        itemHandler = item.handler;
+                        if (item.repeat) item.handler();
+                        else {
+                            itemHandler = item.handler;
+                            m.hide();
+                        }
                     })
-                });                
+                });
             m.onDidHide = () => {
                 active = false;
                 if (itemHandler)
