fc18db2c24065a7703e43c99cd43c28c2ec3894a
Michal Moskal <michal@moskal.me>
2018-12-06 17:37:28 -0800

    Remove unnecessary changes

--- libs/base/core.cpp
+++ libs/base/core.cpp
@@ -27,7 +27,7 @@ void setBinding(int source, int value, Action act) {
         curr->action = act;
         return;
     }
-    curr = new HandlerBinding;
+    curr = new HandlerBinding();
     curr->next = handlerBindings;
     curr->source = source;
     curr->value = value;
