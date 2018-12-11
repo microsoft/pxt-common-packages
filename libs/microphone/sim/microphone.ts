e94810c07053387d455d516a632bdef020850d8c
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-12-10 22:56:17 -0800

    don't crash when service not available

--- libs/microphone/sim/microphone.ts
+++ libs/microphone/sim/microphone.ts
@@ -1,18 +1,21 @@
 namespace pxsim.input {
     export function soundLevel(): number {
         let b = microphoneState();
+        if (!b) return 0;
         b.setUsed();
         return b.getLevel();
     }
 
     export function onLoudSound(body: RefAction) {
         let b = microphoneState();
+        if (!b) return;
         b.setUsed();
         pxtcore.registerWithDal(b.id, DAL.LEVEL_THRESHOLD_HIGH, body);
     }
 
     export function setLoudSoundThreshold(value: number) {
         let b = microphoneState();
+        if (!b) return;
         b.setUsed();
         b.setHighThreshold(value);
     }
