18ec8817b8875bfb9544a2fdfd69c2d2be995b73
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-12-20 15:33:38 -0800

    don't rely on controller on HW

--- libs/game/systemmenu.ts
+++ libs/game/systemmenu.ts
@@ -18,47 +18,47 @@ namespace scene.systemMenu {
     }
 
     export function register() {
-        if (!active) return; // don't show system menu, while in system menu
+        if (active) return; // don't show system menu, while in system menu
 
-        const ctrl = controller.controller1;
-        const mbtn = ctrl.menu;
-        mbtn.onEvent(ControllerButtonEvent.Pressed, function () {
-            active = true;
-            let itemHandler: () => void = undefined;
-            let onHidden: () => void = undefined;
-            const m = new menu.Menu();
-            m.addItem(game.stats ? "hide stats" : "show stats", () => {
-                game.stats = !game.stats;
-                m.hide();
-            })
-            m.addItem(game.consoleOverlay.isVisible() ? "hide console" : "show console", () => {
-                if (game.consoleOverlay.isVisible())
-                    game.consoleOverlay.setVisible(false);
-                else {
-                    game.consoleOverlay.setVisible(true);
-                    console.log("console");
-                }
-                m.hide();
-            });
-            if (customItems)
-                customItems.forEach(item => {
-                    m.addItem(item.name(), () => {
-                        if (item.repeat) item.handler();
-                        else {
-                            itemHandler = item.handler;
-                            onHidden = item.onHidden;
-                            m.hide();
-                        }
-                    })
-                });
-            m.onDidHide = () => {
-                active = false;
-                if (onHidden)
-                    onHidden;
-                if (itemHandler)
-                    itemHandler();
-            }
-            m.show();
+        control.onEvent(1 /* 1st controller */ + 6 /* 6th button */, INTERNAL_KEY_UP, showSystemMenu);
+    }
+
+    export function showSystemMenu() {
+        active = true;
+        let itemHandler: () => void = undefined;
+        let onHidden: () => void = undefined;
+        const m = new menu.Menu();
+        m.addItem(game.stats ? "hide stats" : "show stats", () => {
+            game.stats = !game.stats;
+            m.hide();
         })
+        m.addItem(game.consoleOverlay.isVisible() ? "hide console" : "show console", () => {
+            if (game.consoleOverlay.isVisible())
+                game.consoleOverlay.setVisible(false);
+            else {
+                game.consoleOverlay.setVisible(true);
+                console.log("console");
+            }
+            m.hide();
+        });
+        if (customItems)
+            customItems.forEach(item => {
+                m.addItem(item.name(), () => {
+                    if (item.repeat) item.handler();
+                    else {
+                        itemHandler = item.handler;
+                        onHidden = item.onHidden;
+                        m.hide();
+                    }
+                })
+            });
+        m.onDidHide = () => {
+            active = false;
+            if (onHidden)
+                onHidden;
+            if (itemHandler)
+                itemHandler();
+        }
+        m.show();
     }
 }
