82b86ee1ca4eda2f5b176b3adecbaacf2d495f14
Michal Moskal <michal@moskal.me>
2019-01-10 16:19:56 +0000

    Reorder USB interfaces to match STM bootloader

--- libs/core/usb.cpp
+++ libs/core/usb.cpp
@@ -85,6 +85,24 @@ void usb_init() {
 
     platform_usb_init();
 
+#ifdef STM32F4
+    // as long as we don't enable MSC here, put dummy in its place
+    dummyIface.interfaceIdx = 0x00;
+    hf2.interfaceIdx = 0x01;
+#endif
+
+    // the WINUSB descriptors don't seem to work if there's only one interface
+    // so we add a dummy interface first
+    usb.add(dummyIface);
+
+#ifdef STM32F4
+    // let's not waste EPs on the HF2 - it will run on CONTROL pipe instead
+    // this doesn't seem to currently work on SAMD, so only do it on STM, which
+    // has very few EPs
+    hf2.allocateEP = false;
+#endif
+    usb.add(hf2);
+
 #if CONFIG_ENABLED(DEVICE_MOUSE)
     usb.add(mouse);
 #endif
@@ -94,16 +112,7 @@ void usb_init() {
 #if CONFIG_ENABLED(DEVICE_JOYSTICK)
     usb.add(joystick);
 #endif
-    // the WINUSB descriptors don't seem to work if there's only one interface
-    // so we add a dummy interface first
-    usb.add(dummyIface);
-#ifdef STM32F4
-    // let's not waste EPs on the HF2 - it will run on CONTROL pipe instead
-    // this doesn't seem to currently work on SAMD, so only do it on STM, which
-    // has very few EPs
-    hf2.allocateEP = false;
-#endif
-    usb.add(hf2);
+
     create_fiber(start_usb);
 }
 
