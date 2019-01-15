c85d4166d5fc3a4b6493f436089d953d1f1e9c2c
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-01-15 09:13:03 -0800

    adding butotn tester in menu

--- libs/game/systemmenu.ts
+++ libs/game/systemmenu.ts
@@ -52,6 +52,9 @@ namespace scene.systemMenu {
             music.setVolume(v - 32);
             music.playTone(440, 500);
         });
+        m.addItem("button tester", () => {
+            buttonTester();
+        });
 
         if (customItems)
             customItems.forEach(item => {
@@ -72,5 +75,39 @@ namespace scene.systemMenu {
                 itemHandler();
         }
         m.show();
+    }
+
+    function buttonTester() {
+        game.popScene(); // kill current game
+        game.pushScene();
+        function createButton(name: string, b: controller.Button, x: number, y: number) {
+            const n = 24;
+            const img = image.create(n, n);
+            img.fill(6)
+            img.printCenter(name, n / 2 - image.font8.charHeight / 2, 0, image.font8);
+            const imgPressed = image.create(n, n);
+            imgPressed.fill(7)
+            imgPressed.printCenter(name, n / 2 - image.font8.charHeight / 2, 0, image.font8);
+        
+            const sprite = sprites.create(b.isPressed() ? imgPressed : img);
+            sprite.setPosition(x, y);
+            b.onEvent(ControllerButtonEvent.Pressed, function () {
+                sprite.setImage(imgPressed);        
+            })
+            b.onEvent(ControllerButtonEvent.Released, function () {
+                sprite.setImage(img);
+            })
+            b.onEvent(ControllerButtonEvent.Repeated, function () {
+                sprite.say("repeat", 200)
+            })
+        }
+        
+        scene.setBackgroundColor(8)
+        createButton("A", controller.A, 145, 30)
+        createButton("B", controller.B, 115, 60)
+        createButton("L", controller.left, 15, 60)
+        createButton("U", controller.up, 45, 30)
+        createButton("D", controller.down, 45, 90)
+        createButton("R", controller.right, 75, 60)
     }
 }
