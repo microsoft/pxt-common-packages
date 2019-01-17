5eee64ce8de6ba29b4d0307f96ef516caac9ab2d
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-01-17 04:23:46 -0800

    basic keypad (untested)

--- libs/matrix-keypad/keypad.ts
+++ libs/matrix-keypad/keypad.ts
@@ -1,19 +1,21 @@
-enum MatrixKeypadButtonEvent {
+enum MatrixKeypadEvent {
     Pressed = PinEvent.PulseHigh,
     Released = PinEvent.PulseLow
 }
 
 namespace input {
+    //% fixedInstances
     export class MatrixKeypad {
         rowPins: DigitalInOutPin[];
         columnPins: DigitalInOutPin[];
         messageBusId: number;
-        
+        timePressed: number[];
 
         constructor(rowPins: DigitalInOutPin[], columnPins: DigitalInOutPin[], messageBusId: number) {
             this.rowPins = rowPins;
             this.columnPins = columnPins;
             this.messageBusId = this.messageBusId;
+            this.timePressed = [];
         }
 
         static setInput(p: DigitalInOutPin) {
@@ -25,44 +27,61 @@ namespace input {
             return 1 + (x * this.rowPins.length + y) * 2;
         }
 
-        private clearPins(events: boolean) {
+        private pulseRows() {
             this.rowPins.forEach(p => MatrixKeypad.setInput(p));
             this.columnPins.forEach(p => MatrixKeypad.setInput(p));
 
-            this.rowPins.forEach((row, y) => {
-                row.onEvent(PinEvent.PulseHigh, () => this.pulseRow(y, PinEvent.PulseHigh));
-                row.onEvent(PinEvent.PulseLow, () => this.pulseRow(y, PinEvent.PulseLow));
-            })
+            this.rowPins.forEach((row, y) => this.pulseRow(y));
         }
 
-        private pulseRow(x: number, ev: number) {
-
+        private pulseRow(y: number) {
+            const row = this.rowPins[y];
+            const time = control.millis();
+            // set one row low
+            row.digitalWrite(false);
+            // check the column pins, which ones are pulled down
+            this.columnPins.forEach((col, x) => {
+                const pressed = col.digitalRead();
+                const idx = x * this.columnPins.length + y;
+                const wasPressed = !!this.timePressed[idx];
+                if (wasPressed != pressed) {
+                    control.raiseEvent(this.messageBusId, this.evId(x, y, pressed ? MatrixKeypadEvent.Pressed : MatrixKeypadEvent.Released));
+                    this.timePressed[idx] = pressed ? time : 0;
+                }
+            });
+            // reset the pin to be an input
+            MatrixKeypad.setInput(row);
+            // reset events
+            row.onEvent(PinEvent.PulseHigh, () => this.pulseRow(y));
+            row.onEvent(PinEvent.PulseLow, () => this.pulseRow(y));
         }
 
-        onButtonEvent(x: number, y: number, ev: MatrixKeypadButtonEvent, handler: () => void) {
+        /**
+         * Register an event handler
+         * @param x 
+         * @param y 
+         * @param ev 
+         * @param handler 
+         */
+        //% blockId=mkeypadonevent block="on keypad %keypad button at x %x y %y %ev"
+        onEvent(x: number, y: number, ev: MatrixKeypadEvent, handler: () => void) {
             x = x >> 0;
             y = y >> 0;
             if (x < 0 || y < 0 || x >= this.columnPins.length || y >= this.rowPins.length)
-                return;            
+                return;
+
             control.onEvent(this.messageBusId, this.evId(x, y, ev), handler);
+            this.pulseRow(y); // raise events as needed
         }
 
-        pressedButtons(): number[][] {
-            this.clearPins(false);
-
-            const pressed: number[][] = [];
-            this.rowPins.forEach((row, i) => {
-                // set one row low
-                row.digitalWrite(false);
-                // check the column pins, which ones are pulled down
-                this.columnPins.forEach((col, j) => {
-                    if (!col.digitalRead())
-                        pressed.push([i, j]);
-                });
-                // reset the pin to be an input
-                MatrixKeypad.setInput(row);
-            })
-            return pressed;
+        /**
+         * Indicates a button is pressed
+         * @param x column index starting from 0
+         * @param y row index starting from 0
+         */
+        //% blockId=mkeypadispressed block="is keypad %keypad button pressed at x %x y %y"
+        isPressed(x: number, y: number): boolean {
+            return !!this.timePressed[x * this.columnPins.length + y];
         }
     }
 }
