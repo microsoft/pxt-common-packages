af5ced62b7d38553a8a2d2391b95f68390fbed77
Joseph Wunderlich <jwunderl@uw.edu>
2019-01-23 11:33:26 -0800

    move pause so that responses don't get enqueued

--- libs/game/ask.ts
+++ libs/game/ask.ts
@@ -13,11 +13,11 @@ namespace game {
         control.pushEventContext();
         game.showDialog(title, subtitle, "A = OK, B = CANCEL");
         let answer: boolean = null;
-        controller.A.onEvent(ControllerButtonEvent.Pressed, () => answer = true);
-        controller.B.onEvent(ControllerButtonEvent.Pressed, () => answer = false);
-
         // short pause so that players don't skip through prompt
         pause(500);
+
+        controller.A.onEvent(ControllerButtonEvent.Pressed, () => answer = true);
+        controller.B.onEvent(ControllerButtonEvent.Pressed, () => answer = false);
         pauseUntil(() => answer !== null);
         control.popEventContext();
         return answer;
