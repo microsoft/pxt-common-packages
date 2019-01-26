03bc820e6499e99c9695ab3e5e5ab765b8e88778
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-01-25 19:32:35 -0800

    clenaup

--- libs/mqtt/Net.ts
+++ libs/mqtt/Net.ts
@@ -1,22 +1,12 @@
-declare interface MessageEvent {
-    data: any;
-}
 declare interface Socket {
-    //% shim=SocketMethods::onSend
     send(data: string): void;
-    //% shim=SocketMethods::onClose
     close(): void;
-    //% shim=SocketMethods::onOpen
     onOpen(handler: () => void): void;
-    //% shim=SocketMethods::onClose
     onClose(handler: () => void): void;
-    //% shim=SocketMethods::onError
     onError(handler: () => void): void;
-    //% shim=SocketMethods::onMessage
     onMessage(handler: (data: string) => void): void;
 }
 
 declare interface Net {
-    //% shim=NetMethods::connect
     connect(host: string, port: number): Socket;
 }
