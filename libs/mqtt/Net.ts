e38c60de652738ac44815cd6b8854f2930e40894
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-01-25 23:21:39 -0800

    support for AT based uart

--- libs/mqtt/Net.ts
+++ libs/mqtt/Net.ts
@@ -1,12 +1,14 @@
-declare interface Socket {
-    send(data: string): void;
-    close(): void;
-    onOpen(handler: () => void): void;
-    onClose(handler: () => void): void;
-    onError(handler: () => void): void;
-    onMessage(handler: (data: string) => void): void;
-}
+namespace net {
+    export interface Socket {
+        send(data: string): void;
+        close(): void;
+        onOpen(handler: () => void): void;
+        onClose(handler: () => void): void;
+        onError(handler: () => void): void;
+        onMessage(handler: (data: string) => void): void;
+    }
 
-declare interface Net {
-    connect(host: string, port: number): Socket;
+    export interface Net {
+        connect(host: string, port: number): Socket;
+    }
 }
