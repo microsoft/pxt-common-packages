4d3a3384672b2aa22d73be4916f51c8c4dc416aa
Michal Moskal <michal@moskal.me>
2019-02-02 11:07:22 -0800

    Add support for ILI9341 screens (2.2" and up; 320x240)

--- libs/screen---st7735/screen.cpp
+++ libs/screen---st7735/screen.cpp
@@ -1,12 +1,13 @@
 #include "pxt.h"
 #include "ST7735.h"
+#include "ILI9341.h"
 
 namespace pxt {
 
 class WDisplay {
   public:
     CODAL_SPI spi;
-    ST7735 lcd;
+    ST7735 *lcd;
 
     uint32_t currPalette[16];
     bool newPalette;
@@ -18,11 +19,22 @@ class WDisplay {
     uint16_t width, height;
     uint16_t displayHeight;
     uint8_t offX, offY;
+    bool doubleSize;
     uint32_t palXOR;
 
     WDisplay()
-        : spi(*LOOKUP_PIN(DISPLAY_MOSI), *LOOKUP_PIN(DISPLAY_MISO), *LOOKUP_PIN(DISPLAY_SCK)),
-          lcd(spi, *LOOKUP_PIN(DISPLAY_CS), *LOOKUP_PIN(DISPLAY_DC)) {
+        : spi(*LOOKUP_PIN(DISPLAY_MOSI), *LOOKUP_PIN(DISPLAY_MISO), *LOOKUP_PIN(DISPLAY_SCK)) {
+        int dispTp = getConfig(CFG_DISPLAY_TYPE, DISPLAY_TYPE_ST7735);
+
+        doubleSize = false;
+
+        if (dispTp == DISPLAY_TYPE_ST7735)
+            lcd = new ST7735(spi, *LOOKUP_PIN(DISPLAY_CS), *LOOKUP_PIN(DISPLAY_DC));
+        else if (dispTp == DISPLAY_TYPE_ILI9341) {
+            lcd = new ILI9341(spi, *LOOKUP_PIN(DISPLAY_CS), *LOOKUP_PIN(DISPLAY_DC));
+            doubleSize = true;
+        } else
+            target_panic(PANIC_SCREEN_ERROR);
 
         auto rst = LOOKUP_PIN(DISPLAY_RST);
         if (rst) {
@@ -52,14 +64,15 @@ class WDisplay {
 
         spi.setFrequency(freq * 1000000);
         spi.setMode(0);
-        lcd.init();
-        lcd.configure(madctl, frmctr1);
+        lcd->init();
+        lcd->configure(madctl, frmctr1);
         width = getConfig(CFG_DISPLAY_WIDTH, 160);
         height = getConfig(CFG_DISPLAY_HEIGHT, 128);
         displayHeight = height;
-        lcd.setAddrWindow(offX, offY, width, height);
+        lcd->setAddrWindow(offX, offY, width, height);
         DMESG("screen: %d x %d, off=%d,%d", width, height, offX, offY);
-        screenBuf = (uint8_t *)app_alloc(width * height / 2 + 20);
+        int sz = doubleSize ? (width >> 1) * (height >> 1) : width * height;
+        screenBuf = (uint8_t *)app_alloc(sz / 2 + 20);
 
         lastStatus = NULL;
         registerGC((TValue *)&lastStatus);
@@ -67,9 +80,9 @@ class WDisplay {
     }
 
     void setAddrStatus() {
-        lcd.setAddrWindow(offX, offY + displayHeight, width, height - displayHeight);
+        lcd->setAddrWindow(offX, offY + displayHeight, width, height - displayHeight);
     }
-    void setAddrMain() { lcd.setAddrWindow(offX, offY, width, displayHeight); }
+    void setAddrMain() { lcd->setAddrWindow(offX, offY, width, displayHeight); }
 };
 
 SINGLETON(WDisplay);
@@ -90,8 +103,10 @@ void setPalette(Buffer buf) {
 //%
 void setupScreenStatusBar(int barHeight) {
     auto display = getWDisplay();
-    display->displayHeight = display->height - barHeight;
-    display->setAddrMain();
+    if (!display->doubleSize) {
+        display->displayHeight = display->height - barHeight;
+        display->setAddrMain();
+    }
 }
 
 //%
@@ -111,14 +126,16 @@ void updateScreen(Image_ img) {
 
     display->inUpdate = true;
 
+    auto mult = display->doubleSize ? 2 : 1;
+
     if (img && img->isDirty()) {
-        if (img->bpp() != 4 || img->width() != display->width ||
-            img->height() != display->displayHeight)
+        if (img->bpp() != 4 || img->width() * mult != display->width ||
+            img->height() * mult != display->displayHeight)
             target_panic(PANIC_SCREEN_ERROR);
 
         img->clearDirty();
         // DMESG("wait for done");
-        display->lcd.waitForSendDone();
+        display->lcd->waitForSendDone();
 
         auto palette = display->currPalette;
 
@@ -131,19 +148,19 @@ void updateScreen(Image_ img) {
         memcpy(display->screenBuf, img->pix(), img->pixLength());
 
         // DMESG("send");
-        display->lcd.sendIndexedImage(display->screenBuf, img->width(), img->height(), palette);
+        display->lcd->sendIndexedImage(display->screenBuf, img->width(), img->height(), palette);
     }
 
-    if (display->lastStatus) {
-        display->lcd.waitForSendDone();
+    if (display->lastStatus && !display->doubleSize) {
+        display->lcd->waitForSendDone();
         img = display->lastStatus;
         auto barHeight = display->height - display->displayHeight;
         if (img->bpp() != 4 || barHeight != img->height() || img->width() != display->width)
             target_panic(PANIC_SCREEN_ERROR);
         memcpy(display->screenBuf, img->pix(), img->pixLength());
         display->setAddrStatus();
-        display->lcd.sendIndexedImage(display->screenBuf, img->width(), img->height(), NULL);
-        display->lcd.waitForSendDone();
+        display->lcd->sendIndexedImage(display->screenBuf, img->width(), img->height(), NULL);
+        display->lcd->waitForSendDone();
         display->setAddrMain();
         display->lastStatus = NULL;
     }
