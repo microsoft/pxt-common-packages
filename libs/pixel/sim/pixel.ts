de92bc3520eeef0d4f9cbfa94ce4748e373962e1
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-02-05 08:40:17 -0800

    removed unused sim feature

--- libs/pixel/sim/pixel.ts
+++ libs/pixel/sim/pixel.ts
@@ -8,8 +8,7 @@ namespace pxsim {
     }
 }
 namespace pxsim.pixel {
-    export const defaultPin = light.defaultPin;
-    export function sendBuffer(data: RefBuffer) {
+    export function sendBuffer(data: RefBuffer) {        
         const p = pxsim.pixelPin() as any;
         if (p)
             light.sendBuffer(pxsim.pixelPin() as any, 1, data)
