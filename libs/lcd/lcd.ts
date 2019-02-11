1598f5106583b3a8f741ca362b752a1779b2fe83
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-02-10 21:00:11 -0800

    adding configs

--- libs/lcd/lcd.ts
+++ libs/lcd/lcd.ts
@@ -10,7 +10,7 @@ namespace lcd {
         const db5 = pins.pinByCfg(DAL.CFG_PIN_LCD_DATALINE5);
         const db6 = pins.pinByCfg(DAL.CFG_PIN_LCD_DATALINE6);
         const db7 = pins.pinByCfg(DAL.CFG_PIN_LCD_DATALINE7);
-        const columns = pxt.cfg(DAL.CFG_NUM_LCD_COLUMNS, 123)
+        const columns = pxt.cfg(DAL.CFG_NUM_LCD_COLUMNS, 16);
         const lines = pxt.cfg(DAL.CFG_NUM_LCD_ROWS, 2);
 
         _mono = new CharacterLCDMono(rs, en, db4, db5, db6, db7, columns, lines);
