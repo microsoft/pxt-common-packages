48cd9b98a0844a52d999f3b509749222e53beb7e
Nickel <gnickel@aquent.com>
2019-03-13 12:34:34 -0700

    add in help paths

--- libs/keyboard/keyboard.cpp
+++ libs/keyboard/keyboard.cpp
@@ -110,6 +110,7 @@ namespace keyboard {
     //% blockId=keyboardType block="keyboard type %text"
     //% blockGap=8 weight=100
     //% text.shadowOptions.toString=true
+    //% help=keyboard/type
     void type(String text) {
         if (NULL != text)
             pxt::keyboard.type(text->getUTF8Data(), text->getUTF8Size());
@@ -120,6 +121,7 @@ namespace keyboard {
     */
     //% blockId=keyboardStandardKey block="keyboard key %key|%event"
     //% blockGap=8 weight=99
+    //% help=keyboard/key
     void key(String key, KeyboardKeyEvent event) {
         if (!key->getUTF8Size()) return;
         uint16_t ckey = key->getUTF8Data()[0];
@@ -141,6 +143,7 @@ namespace keyboard {
     */
     //% blockId=keyboardMediaKey block="keyboard media key %key|%event"
     //% blockGap=8
+    //% help=keyboard/media-key
     void mediaKey(KeyboardMediaKey key, KeyboardKeyEvent event) {
         codal::MediaKey ckey = (codal::MediaKey)((int)codal::MediaKey::Mute + (int)key);
         switch(event) {
@@ -161,6 +164,7 @@ namespace keyboard {
     */
     //% blockId=keyboardFunctionKey block="keyboard function key %key|%event"
     //% blockGap=8
+    //% help=keyboard/function-key
     void functionKey(KeyboardFunctionKey key, KeyboardKeyEvent event) {
         codal::FunctionKey ckey = (codal::FunctionKey)((int)codal::FunctionKey::F1Key + (int)key);
         switch(event) {
