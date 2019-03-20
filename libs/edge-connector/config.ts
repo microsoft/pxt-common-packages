c067f774a614ccd443964cd4baca9315f995e790
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-03-19 22:16:45 -0700

    adding pin definitions

---
+++ libs/edge-connector/config.ts
@@ -0,0 +1,12 @@
+namespace config {
+    // https://tech.microbit.org/hardware/edgeconnector/
+    export const PIN_P5 = PIN_BUTTON_A;
+    export const PIN_P11 = PIN_BUTTON_B;
+    // 12 is reserved
+    export const PIN_P13 = PIN_SCK;
+    export const PIN_P14 = PIN_MISO;
+    export const PIN_P15 = PIN_MOSI;
+    // 16,17,18 is 3v.
+    export const PIN_P19 = PIN_SCL;
+    export const PIN_P20 = PIN_SDA; 
+}
