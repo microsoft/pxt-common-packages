233135f723c642540e2bc41526dfd1418bbe0828
James Devine <j.devine@lancaster.ac.uk>
2019-04-03 15:12:08 -0700

    override jacdac options correctly

--- libs/jacdac-ts/makecodeoptions.ts
+++ libs/jacdac-ts/makecodeoptions.ts
@@ -1,24 +1,28 @@
 namespace jacdac{
-    export let options: JDOptions = {
+
+    class MakeCodeOptions implements JDOptions
+    {
         utf8Decode(buf: Buffer) : string{
             return buf.toString();
-        },
+        }
         utf8Encode(str: string) : Buffer{
             return control.createBufferFromUTF8(str);
-        },
+        }
         createBuffer(size:number) : Buffer {
             return control.createBuffer(size);
-        },
+        }
         error(message:string){
-            console.error(message);
-        },
+            console.add(ConsolePriority.Error, message);
+        }
         log(message:string){
-            console.log(message);
-        },
+            console.add(ConsolePriority.Log, message);
+        }
         getSerialNumber() : Buffer {
             const buf = control.createBuffer(8);
             buf.setNumber(NumberFormat.UInt32LE,0,control.deviceSerialNumber())
             return buf;
         }
     }
+
+    export let options: JDOptions = new MakeCodeOptions();
 }
