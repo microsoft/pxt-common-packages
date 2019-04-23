cd339c1bb4dec9b3bd22ba4a7dc7ad64b033e47c
Michal Moskal <michal@moskal.me>
2019-04-22 20:16:47 -0700

    Fix build

--- libs/mixer---none/melody.cpp
+++ libs/mixer---none/melody.cpp
@@ -1,5 +1,4 @@
 #include "pxt.h"
-#include "SoundOutput.h"
 #include "melody.h"
 
 namespace music {
@@ -7,12 +6,7 @@ namespace music {
 void forceOutput(int outp) {}
 
 //%
-void playInstructions(Buffer buf) {
-    auto instr = (SoundInstruction *)buf->data;
-    while (instr->soundWave) {
-        sleep_ms(instr->duration);
-        instr++;
-    }
+void queuePlayInstructions(int when, Buffer buf) {
 }
 
 //%
