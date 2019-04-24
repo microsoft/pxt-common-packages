e4e4d92e42b4f0eaf44aa4fd64754946b9c2b658
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-04-24 13:40:17 -0700

    add missing shims

---
+++ libs/core/sim/keyvaluestorage.ts
@@ -0,0 +1,15 @@
+namespace pxsim.configStorage {
+    export function setBuffer(key: string, value: Buffer): void {
+        // TODO
+    }
+    export function getBuffer(key: string): Buffer {
+        // TODO
+        return undefined;
+    }
+    export function removeItem(key: string): void {
+        // TODO
+    }
+    export function clear(): void {
+        // TODO
+    }
+}
