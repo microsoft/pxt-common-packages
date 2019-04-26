ed90474d818c0e42e3042835c08f860a5e91e6f4
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-04-25 20:53:35 -0700

    move pauseuntil to control.ts

--- libs/base/control.ts
+++ libs/base/control.ts
@@ -253,3 +253,15 @@ namespace control {
         }
     }
 }
+
+/**
+ * Busy wait for a condition to be true
+ * @param condition condition to test for
+ * @param timeOut if positive, maximum duration to wait for in milliseconds
+ */
+//% blockId="pxt_pause_until"
+function pauseUntil(condition: () => boolean, timeOut?: number): void {
+    if (!condition || condition()) return; // optimistic path
+    if (!timeOut) timeOut = 0;
+    control.__queuePollEvent(timeOut, condition, undefined);
+}
