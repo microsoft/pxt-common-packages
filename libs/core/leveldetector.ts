982cf742ee1bfd2e8553d4eef14f607f6b4b68b3
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-04-29 07:22:13 -0700

    adding leveldetector

---
+++ libs/core/leveldetector.ts
@@ -0,0 +1,77 @@
+namespace pins {
+    export class LevelDetector {
+        public id: number;
+        public min: number;
+        public max: number;
+        public lowThreshold: number;
+        public highThreshold: number;
+        private _level: number;
+        private _state: number;
+
+        constructor(id: number, 
+            min: number, max: number, 
+            lowThreshold: number, highThreshold: number) {
+            this.id = id;
+            this.min = min;
+            this.max = max;
+            this.lowThreshold = lowThreshold;
+            this.highThreshold = highThreshold;
+            this._level = Math.ceil((max - min) / 2);
+            this._state = 0;
+        }
+
+        get level(): number {
+            return this._level;
+        }
+
+        set level(level: number) {
+            this._level = this.clampValue(level);
+
+            if (this._level >= this.highThreshold) {
+                this.setState(DAL.LEVEL_THRESHOLD_HIGH);
+            }
+            else if (this._level <= this.lowThreshold) {
+                this.setState(DAL.LEVEL_THRESHOLD_LOW);
+            }
+            else {
+                this.setState(0);
+            }
+        }
+
+        public setLowThreshold(value: number) {
+            this.lowThreshold = this.clampValue(value);
+            this.highThreshold = Math.max(this.lowThreshold + 1, this.highThreshold);
+        }
+
+        public setHighThreshold(value: number) {
+            this.highThreshold = this.clampValue(value);
+            this.lowThreshold = Math.min(this.highThreshold - 1, this.lowThreshold);
+        }
+
+        private clampValue(value: number) {
+            if (value < this.min) {
+                return this.min;
+            }
+            else if (value > this.max) {
+                return this.max;
+            }
+            return value;
+        }
+
+        private setState(state: number) {
+            if (this._state === state) {
+                return;
+            }
+
+            this._state = state;
+            switch (state) {
+                case DAL.LEVEL_THRESHOLD_HIGH:
+                    control.raiseEvent(this.id, DAL.SENSOR_THRESHOLD_HIGH);
+                    break;
+                case DAL.LEVEL_THRESHOLD_LOW:
+                    control.raiseEvent(this.id, DAL.SENSOR_THRESHOLD_LOW);
+                    break;
+            }
+        }
+    }
+}
