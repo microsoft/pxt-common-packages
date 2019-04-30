9846fbf4a7ae911063bfddd2b1e41702f94ea4d3
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-04-30 12:49:03 -0700

    do HSV in a neopixel compatible way

--- libs/jacdac-drivers/colorsensorclient.ts
+++ libs/jacdac-drivers/colorsensorclient.ts
@@ -15,6 +15,56 @@ namespace jacdac {
             if (!s || s.length < 4) return 0;
             return s.getNumber(NumberFormat.UInt32LE, 0);
         }
+
+        /**
+         * Returns the hue of the color
+         */
+        lightHue(): number {
+            const c = this.lightColor();
+            const h = ColorSensorClient.mapRgbToColor(c);
+            console.log(`color ${c} -> ${h}`)
+            return h;
+        }
+
+        private static mapRgbToColor(col: number): number {
+            const r = ((col >> 16) & 0xff) / 255;
+            const g = ((col >> 8) & 0xff) / 255;
+            const b = ((col) & 0xff) / 255;
+
+            const cmax = Math.max(r, Math.max(g, b));
+            const cmin = Math.min(r, Math.min(g, b));
+            const c = cmax - cmin;
+            let hue: number;
+            if (c == 0) {
+                hue = 0;
+            } else {
+                switch (cmax) {
+                    case r: {
+                        const segment = (g - b) / c;
+                        let shift = 0 / 60;       // R° / (360° / hex sides)
+                        if (segment < 0) {          // hue > 180, full rotation
+                            shift = 360 / 60;         // R° / (360° / hex sides)
+                        }
+                        hue = segment + shift;
+                        break;
+                    }
+                    case g: {
+                        const segment = (b - r) / c;
+                        const shift = 120 / 60;     // G° / (360° / hex sides)
+                        hue = segment + shift;
+                        break;
+                    }
+                    case b: {
+                        const segment = (r - g) / c;
+                        const shift = 240 / 60;     // B° / (360° / hex sides)
+                        hue = segment + shift;
+                        break;
+                    }
+                }
+            }
+            hue = hue / 0.6 * 255; // hue is in [0,6], scale it up
+            return hue | 0;
+        }
     }
 
     //% fixedInstance whenUsed block="button client"
