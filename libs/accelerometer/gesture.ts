ea4e2a2b56486476750171ba2eaa5ead38dd1cad
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-05-01 06:07:41 -0700

    enable custom gestures

--- libs/accelerometer/gesture.ts
+++ libs/accelerometer/gesture.ts
@@ -15,13 +15,13 @@ namespace input {
         const evid = DAL.ACCELEROMETER_EVT_SHAKE + id;
         control.onEvent(DAL.DEVICE_ID_GESTURE, evid, handler);
         let sigma = 0;
-/*        control.onIdle(function() {
+        control.onIdle(function() {
             if (sigma > 0) {
                 sigma--;
             } else if(update()) {
                 sigma = 6;
                 control.raiseEvent(DAL.DEVICE_ID_GESTURE, evid);
             }
-        })*/
+        })
     }
 }
