3e5229fd1f13ae495772919f4c02da2e62ae6a1d
Michał Moskal <michal@moskal.me>
2019-05-07 08:00:11 -0700

    move mixer/synth from codal to PXT (#801)
    * New music APIs
    
    * Add stop playing; work on the TS side
    
    * Codal connection
    
    * Handle activation
    
    * Drop codal from linux build
    
    * Fix build
    
    * queuePlayInstructions works in sim
    
    * Codal fixes
    
    * Hardware fixes; works
    
    * Thread safety for mixer; add ringtone (clicking)
    
    * Default per-melody volume to 100%
    
    * Only use the play loop for sounds over 2s
    
    * Fix memory errors in melody
    
    * Configure DAC as 12 bit on D51

--- libs/mixer/melody.cpp
+++ libs/mixer/melody.cpp
@@ -1,67 +1,266 @@
 #include "pxt.h"
-#include "Synthesizer.h"
 #include "SoundOutput.h"
-
 #include "melody.h"
 
+//#define LOG DMESG
+#define LOG NOLOG
+
 namespace music {
-class SynthChannel {
-  public:
-    SynthChannel *next;
-    Synthesizer synth;
-    MixerChannel *mixch;
-    bool used;
-    virtual ~SynthChannel() {}
 
-    SynthChannel() : synth(SYNTHESIZER_SAMPLE_RATE, true) {}
-};
-
-class WSynthesizer {
-  public:
-    Mixer mixer;
-    SoundOutput out;
-    SynthChannel *channels;
-
-    WSynthesizer() : out(mixer) { channels = NULL; }
-    virtual ~WSynthesizer() {}
-};
 SINGLETON(WSynthesizer);
 
-SynthChannel *allocateChannel() {
-    auto snd = getWSynthesizer();
-    auto ch = snd->channels;
-    auto numCh = 0;
+static const int16_t sinQ[256] = {
+    0,     201,   403,   605,   807,   1009,  1210,  1412,  1614,  1815,  2017,  2218,  2419,
+    2621,  2822,  3023,  3224,  3425,  3625,  3826,  4026,  4226,  4426,  4626,  4826,  5026,
+    5225,  5424,  5623,  5822,  6020,  6219,  6417,  6615,  6812,  7009,  7206,  7403,  7600,
+    7796,  7992,  8187,  8383,  8578,  8772,  8967,  9161,  9354,  9547,  9740,  9933,  10125,
+    10317, 10508, 10699, 10890, 11080, 11270, 11459, 11648, 11836, 12024, 12212, 12399, 12585,
+    12772, 12957, 13142, 13327, 13511, 13695, 13878, 14060, 14243, 14424, 14605, 14785, 14965,
+    15145, 15323, 15501, 15679, 15856, 16032, 16208, 16383, 16557, 16731, 16905, 17077, 17249,
+    17420, 17591, 17761, 17930, 18099, 18267, 18434, 18600, 18766, 18931, 19096, 19259, 19422,
+    19585, 19746, 19907, 20067, 20226, 20384, 20542, 20699, 20855, 21010, 21165, 21318, 21471,
+    21623, 21774, 21925, 22074, 22223, 22371, 22518, 22664, 22810, 22954, 23098, 23241, 23382,
+    23523, 23663, 23803, 23941, 24078, 24215, 24350, 24485, 24618, 24751, 24883, 25014, 25144,
+    25273, 25401, 25528, 25654, 25779, 25903, 26026, 26148, 26269, 26389, 26509, 26627, 26744,
+    26860, 26975, 27089, 27202, 27314, 27425, 27535, 27644, 27752, 27859, 27964, 28069, 28173,
+    28275, 28377, 28477, 28576, 28674, 28772, 28868, 28963, 29056, 29149, 29241, 29331, 29421,
+    29509, 29596, 29682, 29767, 29851, 29934, 30015, 30096, 30175, 30253, 30330, 30406, 30480,
+    30554, 30626, 30697, 30767, 30836, 30904, 30970, 31036, 31100, 31163, 31225, 31285, 31345,
+    31403, 31460, 31516, 31570, 31624, 31676, 31727, 31777, 31825, 31873, 31919, 31964, 32008,
+    32050, 32092, 32132, 32171, 32209, 32245, 32280, 32314, 32347, 32379, 32409, 32438, 32466,
+    32493, 32518, 32542, 32565, 32587, 32607, 32627, 32645, 32661, 32677, 32691, 32704, 32716,
+    32727, 32736, 32744, 32751, 32757, 32761, 32764, 32766, 32767};
 
-    while (ch) {
-        if (!ch->used) {
-            ch->used = true;
-            return ch;
+typedef int (*gentone_t)(uintptr_t userData, uint32_t position);
+
+static int realNoiseTone(uintptr_t userData, uint32_t position) {
+    (void)userData;
+    (void)position;
+    // see https://en.wikipedia.org/wiki/Xorshift
+    static uint32_t x = 0xf01ba80;
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    return (x & 0xffff) - 0x7fff;
+}
+
+static int sineTone(uintptr_t userData, uint32_t position) {
+    (void)userData;
+    int p = position >= 512 ? position - 512 : position;
+    int r;
+    if (p < 256) {
+        r = sinQ[p];
+    } else {
+        r = sinQ[511 - p];
+    }
+    return position >= 512 ? -r : r;
+}
+
+static int sawtoothTone(uintptr_t userData, uint32_t position) {
+    (void)userData;
+    return (position << 6) - 0x7fff;
+}
+
+static int triangleTone(uintptr_t userData, uint32_t position) {
+    (void)userData;
+    return position < 512 ? (position << 7) - 0x7fff : ((1023 - position) << 7) - 0x7fff;
+}
+
+static int fakeNoiseTone(uintptr_t userData, uint32_t position) {
+    // deterministic, semi-random noise
+    return (((position * 7919) & 1023) << 6) - 0x7fff;
+}
+
+static int squareWaveTone(uintptr_t wave, uint32_t position) {
+    return position < (102 * (wave - SW_SQUARE_10 + 1)) ? -0x7fff : 0x7fff;
+}
+
+static int silenceTone(uintptr_t userData, uint32_t position) {
+    (void)userData;
+    (void)position;
+    return 0;
+}
+
+static gentone_t getWaveFn(uint8_t wave) {
+    switch (wave) {
+    case SW_TRIANGLE:
+        return triangleTone;
+    case SW_SAWTOOTH:
+        return sawtoothTone;
+    case SW_NOISE:
+        return fakeNoiseTone;
+    case SW_REAL_NOISE:
+        return realNoiseTone;
+    case SW_SINE:
+        return sineTone;
+    default:
+        if (SW_SQUARE_10 <= wave && wave <= SW_SQUARE_50)
+            return squareWaveTone;
+        else
+            return silenceTone;
+    }
+}
+
+#define CLAMP(lo, v, hi) ((v) = ((v) < (lo) ? (lo) : (v) > (hi) ? (hi) : (v)))
+
+int WSynthesizer::updateQueues() {
+    const int maxTime = 0xffffff;
+    while (1) {
+        WaitingSound *p;
+        int minLeft = maxTime;
+        for (p = waiting; p; p = p->next) {
+            int timeLeft =
+                p->state == SoundState::Waiting ? p->startSampleNo - currSample : maxTime;
+            if (timeLeft <= 0) {
+                break;
+            }
+            if (timeLeft < minLeft)
+                minLeft = timeLeft;
         }
-        ch = ch->next;
-        numCh++;
+        if (p) {
+            PlayingSound *snd;
+            int minIdx = -1;
+            for (unsigned i = 0; i < MAX_SOUNDS; ++i) {
+                snd = &playingSounds[i];
+                if (snd->sound == NULL)
+                    break;
+                if (minIdx == -1 ||
+                    playingSounds[minIdx].startSampleNo < playingSounds[i].startSampleNo)
+                    minIdx = i;
+                snd = NULL;
+            }
+            // if we didn't find a free slot, expel the oldest sound
+            if (!snd)
+                snd = &playingSounds[minIdx];
+            if (snd->sound)
+                snd->sound->state = SoundState::Done;
+            snd->sound = p;
+            p->state = SoundState::Playing;
+            snd->startSampleNo = currSample;
+            snd->currInstr = (SoundInstruction *)p->instructions->data;
+            snd->instrEnd = snd->currInstr + p->instructions->length / sizeof(SoundInstruction);
+            for (auto p = snd->currInstr; p < snd->instrEnd; p++) {
+                CLAMP(20, p->frequency, 20000);
+                CLAMP(0, p->startVolume, 1023);
+                CLAMP(0, p->endVolume, 1023);
+                CLAMP(1, p->duration, 60000);
+            }
+            snd->prevVolume = -1;
+        } else {
+            // no more sounds to move
+            return minLeft;
+        }
+    }
+}
+
+int WSynthesizer::fillSamples(int16_t *dst, int numsamples) {
+    if (numsamples <= 0)
+        return 1;
+
+    int timeLeft = updateQueues();
+    int res = waiting != NULL;
+
+    // if there's a pending sound to be started somewhere during numsamples,
+    // split the call into two
+    if (timeLeft < numsamples) {
+        fillSamples(dst, timeLeft);
+        LOG("M split %d", timeLeft);
+        fillSamples(dst + timeLeft, numsamples - timeLeft);
+        return 1;
     }
 
-    if (numCh >= 3)
-        return NULL;
+    memset(dst, 0, numsamples * 2);
 
-    ch = new SynthChannel();
-    ch->synth.setVolume(1024);
-    ch->synth.setSampleRate(snd->out.dac.getSampleRate());
-    ch->mixch = snd->mixer.addChannel(ch->synth.output);
-    ch->used = true;
-    ch->next = snd->channels;
-    snd->channels = ch;
+    uint32_t samplesPerMS = (sampleRate << 8) / 1000;
+    float toneStepMult = (1024.0 * (1 << 16)) / sampleRate;
+    const int MAXVAL = (1 << (OUTPUT_BITS - 1)) - 1;
 
-    return ch;
+    for (unsigned i = 0; i < MAX_SOUNDS; ++i) {
+        PlayingSound *snd = &playingSounds[i];
+        if (snd->sound == NULL)
+            continue;
+
+        res = 1;
+
+        SoundInstruction *instr = NULL;
+        gentone_t fn = NULL;
+        snd->currInstr--;
+        uint32_t toneStep = 0;
+        int32_t volumeStep = 0;
+        uint32_t tonePosition = snd->tonePosition;
+        uint32_t samplesLeft = 0;
+        uint8_t wave = 0;
+        int32_t volume = 0;
+        uint32_t prevFreq = 0;
+
+        for (int j = 0; j < numsamples; ++j) {
+            if (samplesLeft == 0) {
+                instr = ++snd->currInstr;
+                if (instr >= snd->instrEnd) {
+                    break;
+                }
+                wave = instr->soundWave;
+                fn = getWaveFn(wave);
+                samplesLeft = (uint32_t)(instr->duration * samplesPerMS >> 8);
+                if (prevFreq != instr->frequency) {
+                    toneStep = (uint32_t)(toneStepMult * instr->frequency);
+                    prevFreq = instr->frequency;
+                }
+                volumeStep = ((int)(instr->endVolume - instr->startVolume) << 16) / samplesLeft;
+                if (j == 0 && snd->prevVolume != -1) {
+                    // restore previous state
+                    samplesLeft = snd->samplesLeftInCurr;
+                    volume = snd->prevVolume;
+                } else {
+                    LOG("#sampl %d %p", samplesLeft, instr);
+                    volume = instr->startVolume << 16;
+                }
+            }
+
+            int v = fn(wave, (tonePosition >> 16) & 1023);
+            v = (v * (volume >> 16)) >> (10 + (16 - OUTPUT_BITS));
+
+            // if (v > MAXVAL)
+            //    target_panic(123);
+
+            dst[j] += v;
+
+            tonePosition += toneStep;
+            volume += volumeStep;
+            samplesLeft--;
+        }
+
+        if (instr >= snd->instrEnd) {
+            snd->sound->state = SoundState::Done;
+            snd->sound = NULL;
+        } else {
+            snd->tonePosition = tonePosition;
+            if (samplesLeft == 0)
+                samplesLeft++; // avoid infinite loop in next iteration
+            snd->samplesLeftInCurr = samplesLeft;
+            snd->prevVolume = volume;
+        }
+    }
+
+    currSample += numsamples;
+
+    for (int j = 0; j < numsamples; ++j) {
+        if (dst[j] > MAXVAL)
+            dst[j] = MAXVAL;
+        else if (dst[j] < -MAXVAL)
+            dst[j] = -MAXVAL;
+    }
+
+    return res;
 }
 
 //%
-void enableAmp(int enabled)
-{
+void enableAmp(int enabled) {
     // this is also compiled on linux
 #ifdef LOOKUP_PIN
     auto pin = LOOKUP_PIN(SPEAKER_AMP);
-    if (pin) pin->setDigitalValue(enabled);
+    if (pin)
+        pin->setDigitalValue(enabled);
 #endif
 }
 
@@ -71,62 +270,58 @@ void forceOutput(int outp) {
     snd->out.setOutput(outp);
 }
 
-static uint16_t realNoiseTone(void *arg, int position) {
-    (void)arg;
-    (void)position;
-    // see https://en.wikipedia.org/wiki/Xorshift
-    static uint32_t x = 0xf01ba80;
-    x ^= x << 13;
-    x ^= x >> 17;
-    x ^= x << 5;
-    return x & 1023;
+//%
+void queuePlayInstructions(int when, Buffer buf) {
+    auto snd = getWSynthesizer();
+
+    registerGCPtr((TValue)buf);
+
+    auto p = new WaitingSound;
+    p->state = SoundState::Waiting;
+    p->instructions = buf;
+    p->startSampleNo = snd->currSample + when * snd->sampleRate / 1000;
+
+    LOG("Queue %dms now=%d off=%d %p", when, snd->currSample, p->startSampleNo - snd->currSample,
+        buf->data);
+
+    target_disable_irq();
+    // add new sound to queue
+    p->next = snd->waiting;
+    snd->waiting = p;
+    // remove sounds that have already been fully played
+    while (p) {
+        while (p->next && p->next->state == SoundState::Done) {
+            auto todel = p->next;
+            p->next = todel->next;
+            unregisterGCPtr((TValue)todel->instructions);
+            delete todel;
+        }
+        p = p->next;
+    }
+    target_enable_irq();
+
+    snd->poke();
 }
 
 //%
-void playInstructions(Buffer buf) {
-    auto ch = allocateChannel();
-    auto instr = (SoundInstruction *)buf->data;
-    auto wave = 0;
-    while (instr->soundWave) {
-        if (ch) {
-            if (wave != instr->soundWave) {
-                wave = instr->soundWave;
-                switch (wave) {
-                case SW_TRIANGLE:
-                    ch->synth.setTone(Synthesizer::TriangleTone);
-                    break;
-                case SW_SAWTOOTH:
-                    ch->synth.setTone(Synthesizer::SawtoothTone);
-                    break;
-                case SW_NOISE:
-                    ch->synth.setTone(Synthesizer::NoiseTone);
-                    break;
-                case SW_REAL_NOISE:
-                    ch->synth.setTone(realNoiseTone);
-                    break;
-                case SW_SINE:
-                    ch->synth.setTone(Synthesizer::SineTone);
-                    break;
-                default:
-                    if (SW_SQUARE_10 <= wave && wave <= SW_SQUARE_50) {
-                        ch->synth.setTone(Synthesizer::SquareWaveToneExt,
-                                          (void *)(102 * (wave - SW_SQUARE_10 + 1)));
-                    } else {
-                        // silence
-                        ch->synth.setTone(Synthesizer::SquareWaveToneExt, (void *)(0));
-                    }
-                    break;
-                }
-            }
-            ch->synth.setFrequency(instr->frequency, instr->duration, instr->startVolume,
-                                   instr->endVolume);
-        } else {
-            fiber_sleep(instr->duration);
-        }
-        instr++;
+void stopPlaying() {
+    LOG("stop playing!");
+
+    auto snd = getWSynthesizer();
+
+    target_disable_irq();
+    auto p = snd->waiting;
+    snd->waiting = NULL;
+    for (unsigned i = 0; i < MAX_SOUNDS; ++i) {
+        snd->playingSounds[i].sound = NULL;
     }
-    if (ch)
-        ch->used = false;
+    while (p) {
+        auto n = p->next;
+        unregisterGCPtr((TValue)p->instructions);
+        delete p;
+        p = n;
+    }
+    target_enable_irq();
 }
 
 } // namespace music
--- libs/mixer/melody.h
+++ libs/mixer/melody.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#ifndef OUTPUT_BITS
+#define OUTPUT_BITS 10
+#endif
+
 #define SW_TRIANGLE 1
 #define SW_SAWTOOTH 2
 #define SW_SINE 3 // TODO remove it? it takes space
@@ -11,8 +15,109 @@
 struct SoundInstruction {
     uint8_t soundWave;
     uint8_t flags;
-    uint16_t frequency;
-    uint16_t duration;
-    uint16_t startVolume;
-    uint16_t endVolume;
+    uint16_t frequency;  // Hz
+    uint16_t duration;   // ms
+    int16_t startVolume; // 0-1023
+    int16_t endVolume;   // 0-1023
 };
+
+#ifdef DATASTREAM_MAXIMUM_BUFFERS
+#define CODAL 1
+#endif
+
+namespace music {
+
+#define MAX_SOUNDS 5
+
+STATIC_ASSERT((1 << (16 - OUTPUT_BITS)) > MAX_SOUNDS);
+
+enum class SoundState : uint8_t {
+    Waiting,
+    Playing,
+    Done
+};
+
+struct WaitingSound {
+    uint32_t startSampleNo;
+    SoundState state;
+    WaitingSound *next;
+    Buffer instructions;
+};
+
+struct PlayingSound {
+    uint32_t startSampleNo;
+    uint32_t samplesLeftInCurr;
+    uint32_t tonePosition;
+    int32_t prevVolume;
+    WaitingSound *sound;
+    SoundInstruction *currInstr, *instrEnd;
+};
+
+class WSynthesizer
+#ifdef CODAL
+    : public DataSource
+#endif
+{
+  public:
+#ifdef CODAL
+    DataSink *upstream;
+#else
+    void *upstream;
+#endif
+    uint32_t currSample; // after 25h of playing we might get a glitch
+    int32_t sampleRate; // eg 44100
+    PlayingSound playingSounds[MAX_SOUNDS];
+    WaitingSound *waiting;
+    bool active;
+
+    SoundOutput out;
+
+    int fillSamples(int16_t *dst, int numsamples);
+    int updateQueues();
+
+    WSynthesizer() : upstream(NULL), out(*this) {
+        currSample = 0;
+        active = false;
+        sampleRate = out.dac.getSampleRate();
+        memset(&playingSounds, 0, sizeof(playingSounds));
+        waiting = NULL;
+    }
+    virtual ~WSynthesizer() {}
+
+    void pokeUpstream() {
+#ifdef CODAL
+        if (upstream) {
+            upstream->pullRequest();
+        }
+#endif
+    }
+
+    void poke() {
+        if (!active) {
+            active = true;
+            pokeUpstream();
+        }
+    }
+
+#ifdef CODAL
+    virtual ManagedBuffer pull() {
+        ManagedBuffer data(512);
+        auto dp = (int16_t *)data.getBytes();
+        auto sz = 512/2;
+        int r = fillSamples(dp, sz);
+        while (sz--) {
+            *dp++ += 512;
+        }
+        if (!r) {
+            active = false;
+            // return empty - nothing left to play
+            return ManagedBuffer();
+        }
+        pokeUpstream();
+        return data;
+    }
+    virtual void connect(DataSink &sink) { upstream = &sink; }
+#endif
+};
+
+} // namespace music
