07a45b89ca57e8d32b86d79298304150f07632ee
Michal Moskal <michal@moskal.me>
2019-05-23 16:37:44 -0700

    Make sure playUntilDone() waits until the melody is done Also, change playTone() to use sine wave

--- libs/mixer/melody.ts
+++ libs/mixer/melody.ts
@@ -64,7 +64,7 @@ namespace music {
 
     function playNoteCore(when: number, frequency: number, ms: number) {
         let buf = control.createBuffer(10)
-        addNote(buf, 0, ms, 255, 255, 1, frequency, volume())
+        addNote(buf, 0, ms, 255, 255, 3, frequency, volume())
         queuePlayInstructions(when, buf)
     }
 
@@ -384,6 +384,9 @@ namespace music {
                 let currNote = scanNextWord();
                 let prevNote: boolean = false;
                 if (!currNote) {
+                    let timeLeft = timePos - now
+                    if (timeLeft > 0)
+                        pause(timeLeft)
                     if (this.onPlayFinished)
                         this.onPlayFinished();
                     return;
