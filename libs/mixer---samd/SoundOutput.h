05cbd3d0e8c09e36db61fa0416526b8d51dcc4b6
Michal Moskal <michal@moskal.me>
2019-05-30 13:45:02 -0700

    Pass flags to SAMDDAC ctor

--- libs/mixer---samd/SoundOutput.h
+++ libs/mixer---samd/SoundOutput.h
@@ -10,7 +10,9 @@ class SoundOutput {
   public:
     SAMDDAC dac;
 
-    SoundOutput(DataSource &data) : dac(*pxt::lookupPin(PIN_PA02), data) {
+    SoundOutput(DataSource &data)
+        : dac(*pxt::lookupPin(PIN_PA02), data, SAMDDAC_DEFAULT_FREQUENCY, DEVICE_ID_SYSTEM_DAC,
+              getConfig(CFG_PIN_JACK_SND, 0) >> 16) {
         jacdac::setJackRouterOutput(-1);
     }
 
