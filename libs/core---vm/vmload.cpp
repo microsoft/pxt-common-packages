630073ad24e599268440193e930797cb0d2ca452
Michal Moskal <michal@moskal.me>
2019-06-02 21:55:29 -0700

    Add pxt_vm_start_buffer

--- libs/core---vm/vmload.cpp
+++ libs/core---vm/vmload.cpp
@@ -4,20 +4,7 @@ namespace pxt {
 
 VMImage *vmImg;
 
-static void vmStartCore(const char *fn) {
-    auto f = fopen(fn, "rb");
-    if (!f) {
-        dmesg("cannot open %s", fn);
-        return;
-    }
-
-    fseek(f, 0, SEEK_END);
-    auto len = (unsigned)ftell(f);
-    fseek(f, 0, SEEK_SET);
-    auto data = new uint8_t[len + 16];
-    fread(data, len, 1, f);
-    fclose(f);
-
+static void vmStartCore(uint8_t *data, unsigned len) {
     unloadVMImage(vmImg);
     vmImg = NULL;
 
@@ -40,17 +27,57 @@ static void vmStartCore(const char *fn) {
     initRuntime();
 }
 
-void vmStart() {
-    auto fn = pxt::initialArgv[1];
-    vmStartCore(fn);
+static void vmStartFile(const char *fn) {
+    auto f = fopen(fn, "rb");
+    if (!f) {
+        dmesg("cannot open %s", fn);
+        return;
+    }
+
+    fseek(f, 0, SEEK_END);
+    auto len = (unsigned)ftell(f);
+    fseek(f, 0, SEEK_SET);
+    auto data = new uint8_t[len + 16];
+    fread(data, len, 1, f);
+    fclose(f);
+
+    vmStartCore(data, len);
 }
 
-DLLEXPORT void pxt_vm_start(const char *fn)
-{
+static uint8_t *vm_data;
+static unsigned vm_len;
+static const char *vm_filename;
+
+static void *multiStart(void *) {
+    if (vm_filename)
+        vmStartFile(vm_filename);
+    else
+        vmStartCore(vm_data, vm_len);
+    return NULL;
+}
+
+void vmStart() {
+    auto fn = pxt::initialArgv[1];
+    vmStartFile(fn);
+}
+
+static void spinThread() {
     panicCode = 0;
     pthread_t disp;
-    pthread_create(&disp, NULL, (void*(*)(void*))vmStartCore, (void*)fn);
+    pthread_create(&disp, NULL, multiStart, NULL);
     pthread_detach(disp);
+}
+
+DLLEXPORT void pxt_vm_start(const char *fn) {
+    vm_filename = fn;
+    spinThread();
+}
+
+DLLEXPORT void pxt_vm_start_buffer(uint8_t *data, unsigned len) {
+    vm_filename = NULL;
+    vm_data = data;
+    vm_len = len;
+    spinThread();
 }
 
 } // namespace pxt
