c0b5aa3a07fba4e84ca87269a6b274ab8e5ad1fe
Chase Mortensen <C_Mortensen@live.com>
2019-06-04 15:23:49 -0700

    Add check for dot star leds before making APA102 strip

--- libs/light/onboardstrip.ts
+++ libs/light/onboardstrip.ts
@@ -12,10 +12,10 @@ namespace light {
 
         const data = pins.pinByCfg(DAL.CFG_PIN_DOTSTAR_DATA);
         const clk = pins.pinByCfg(DAL.CFG_PIN_DOTSTAR_CLOCK);
-        const dsnum = control.getConfigValue(DAL.CFG_NUM_DOTSTARS, 1);
+        const dsnum = control.getConfigValue(DAL.CFG_NUM_DOTSTARS, 0);
         const neo = pins.pinByCfg(DAL.CFG_PIN_NEOPIXEL);
-        const neonum = control.getConfigValue(DAL.CFG_NUM_NEOPIXELS, 1);
-        if (data && clk) {
+        const neonum = control.getConfigValue(DAL.CFG_NUM_NEOPIXELS, 0);
+        if (data && clk && dsnum > 0) {
             _onboardStrip = light.createAPA102Strip(data, clk, dsnum);
             _onboardStrip.setBrightness(96);
         } else if (neo) {
