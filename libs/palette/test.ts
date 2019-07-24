097a082b6f3e122abd76b1e7ff1196ca50492d8b
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-07-24 10:33:31 -0700

    adding palette test

---
+++ libs/palette/test.ts
@@ -0,0 +1,25 @@
+
+let mySprite = sprites.create(img`
+0 1 2 3
+4 5 6 7
+8 9 a b
+c d e f
+`.doubled().doubled().doubled().doubled(), SpriteKind.Player)
+
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    const p = palette.defaultPalette();
+    for (let i = 0; i < p.length; ++i) {
+        p.setColor(i, colors.rgb(i * 16, 0, 255 - i * 16));
+    }
+    p.setColor(0, 0)
+    palette.setColors(p)
+})
+
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    const p = palette.defaultPalette();
+    for (let i = 0; i < p.length; ++i) {
+        p.setColor(i, colors.rgb(i * 16, i * 16, 255 - i * 16));
+    }
+    p.setColor(0, 0)
+    palette.setColors(p)
+})
