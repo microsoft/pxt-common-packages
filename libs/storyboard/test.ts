6a66317ab64c6b0a27b1436ef510412f493194eb
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-07-25 16:14:13 -0700

    remove microsoft animation

--- libs/storyboard/test.ts
+++ libs/storyboard/test.ts
@@ -1,6 +1,26 @@
-
+let mySprite = sprites.create(img`
+    . . . . . . b b b b a a . . . .
+    . . . . b b d d d 3 3 3 a a . .
+    . . . b d d d 3 3 3 3 3 3 a a .
+    . . b d d 3 3 3 3 3 3 3 3 3 a .
+    . b 3 d 3 3 3 3 3 b 3 3 3 3 a b
+    . b 3 3 3 3 3 a a 3 3 3 3 3 a b
+    b 3 3 3 3 3 a a 3 3 3 3 d a 4 b
+    b 3 3 3 3 b a 3 3 3 3 3 d a 4 b
+    b 3 3 3 3 3 3 3 3 3 3 d a 4 4 e
+    a 3 3 3 3 3 3 3 3 3 d a 4 4 4 e
+    a 3 3 3 3 3 3 3 d d a 4 4 4 e .
+    a a 3 3 3 d d d a a 4 4 4 e e .
+    . e a a a a a a 4 4 4 4 e e . .
+    . . e e b b 4 4 4 4 b e e . . .
+    . . . e e e e e e e e . . . . .
+    . . . . . . . . . . . . . . . .
+`, SpriteKind.Player)
+mySprite.x = 10
+controller.moveSprite(mySprite)
+storyboard.loaderBootSequence.register()
 storyboard.registerScene("lemon", function () {
-    let mySprite = sprites.create(img`
+    let mySprite2 = sprites.create(img`
         4 4 4 . . 4 4 4 4 4 . . . . . .
         4 5 5 4 4 5 5 5 5 5 4 4 . . . .
         b 4 5 5 1 5 1 1 1 5 5 5 4 . . .
@@ -18,12 +38,17 @@ storyboard.registerScene("lemon", function () {
         . . . . c c b 4 4 4 b b 4 5 4 4
         . . . . . . c c c c c c b b 4 .
     `, SpriteKind.Player)
+    mySprite2.y = 20
+    controller.moveSprite(mySprite2)
     controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-        storyboard.transitionToScene("burger")
+        storyboard.push("burger");
+    })
+    controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+        storyboard.pop();
     })
 })
 storyboard.registerScene("burger", function () {
-    let mySprite = sprites.create(img`
+    let mySprite3 = sprites.create(img`
         . . . . c c c b b b b b . . . .
         . . c c b 4 4 4 4 4 4 b b b . .
         . c c 4 4 4 4 4 5 4 4 4 4 b c .
@@ -41,8 +66,19 @@ storyboard.registerScene("burger", function () {
         . e e b b 4 4 4 4 4 4 4 4 e e .
         . . . c c c c c e e e e e . . .
     `, SpriteKind.Player)
+    mySprite3.y = 80
+    controller.moveSprite(mySprite3)
     controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-        storyboard.transitionToScene("lemon")
+        storyboard.replace("lemon");
+    })
+    controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+        storyboard.pop()
     })
 })
-storyboard.transitionToScene("lemon")
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    storyboard.start("lemon")
+})
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    storyboard.start("burger")
+})
+
