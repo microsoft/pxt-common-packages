552716a8905b570b0db4a2fe29fc2133a30cb005
Michal Moskal <michal@moskal.me>
2019-08-12 02:33:44 +0200

    Use new pxt-core sim storage apis

--- libs/settings/sim/settings.ts
+++ libs/settings/sim/settings.ts
@@ -1,33 +1,36 @@
 namespace pxsim.settings {
-    let scope: string
     let currSize = 0
-    const storage = window.localStorage
     const MAX_SIZE = 32 * 1024
 
     export function _setScope(newScope: string): void {
-        scope = newScope
         if (!newScope || newScope.length > 100)
             U.userError("invalid scope: " + newScope)
-        computeSize()
+        const st = board().storedState
+        const key = encodeKey("#scope")
+        if (newScope != st[key]) {
+            _userClean()
+            board().setStoredState(key, newScope)
+            computeSize()
+        }
     }
 
     function encodeKey(key: string) {
-        return "S/" + scope + "///" + key
+        return "S/" + key
     }
 
     function allKeys() {
-        const mykeys: string[] = []
         const pref = encodeKey("")
-        for (let i = 0; i < storage.length; ++i) {
-            const k = storage.key(i)
-            if (k && k.slice(0, pref.length) == pref)
-                mykeys.push(k)
-        }
-        return mykeys
+        const st = board().storedState
+        return Object.keys(st).filter(k => k.slice(0, pref.length) == pref)
+    }
+
+    function userKeys() {
+        return allKeys().filter(s => s[2] != "#")
     }
 
     function computeSize() {
         let sz = 0
+        const storage = board().storedState
         for (let k of allKeys()) {
             sz += k.length + storage[k].length
         }
@@ -36,32 +39,35 @@ namespace pxsim.settings {
 
     export function _set(key: string, buf: RefBuffer) {
         key = encodeKey(key)
+        const storage = board().storedState
         const prev = storage[key]
         const newSize = prev == null
             ? currSize + key.length + buf.data.length
             : currSize + buf.data.length - prev.length
         if (newSize > MAX_SIZE)
             return -1
-        storage[key] = U.uint8ArrayToString(buf.data)
+        board().setStoredState(key, U.uint8ArrayToString(buf.data))
         currSize = newSize
         return 0;
     }
 
     export function _remove(key: string) {
         key = encodeKey(key)
+        const storage = board().storedState
         if (storage[key] == null)
             return -1
         currSize -= key.length + storage[key].length
-        storage.removeItem(key)
+        board().setStoredState(key, null)
         return 0
     }
 
     export function _exists(key: string): boolean {
-        return _get(key) != null
+        return _get(key) != undefined
     }
 
     export function _get(key: string): RefBuffer {
         key = encodeKey(key)
+        const storage = board().storedState
         const val = storage[key] as string
         if (val == null)
             return undefined
@@ -69,8 +75,8 @@ namespace pxsim.settings {
     }
 
     export function _userClean(): void {
-        for (let k of allKeys())
-            storage.removeItem(k)
+        for (let k of userKeys())
+            board().setStoredState(k, null)
         computeSize()
     }
 
@@ -78,10 +84,8 @@ namespace pxsim.settings {
         const r = new RefCollection()
 
         const emptyPref = encodeKey("")
-        for (let k of allKeys()) {
+        for (let k of prefix[0] == "#" ? allKeys() : userKeys()) {
             const n = k.slice(emptyPref.length)
-            if (prefix == "" && n[0] == "#")
-                continue
             if (n.slice(0, prefix.length) != prefix)
                 continue
             r.push(n)
