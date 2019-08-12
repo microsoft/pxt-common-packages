93b35809bd2d91606ae9d0d2263b440e423f21ba
Richard Knoll <riknoll@users.noreply.github.com>
2019-08-12 16:05:13 -0700

    Move localstorage reference into try/catch (#924)

--- libs/game/sim/api.ts
+++ libs/game/sim/api.ts
@@ -2,9 +2,9 @@ namespace pxsim.info {
     export function updateHighScore(score: number): number {
         const b = board();
         const id = b.runOptions.version || "local";
-        if (!id || !window.localStorage) return 0;
 
         try {
+            if (!id || !window.localStorage) return 0;
             const key = "highscore-" + id;
             let hs = parseFloat(window.localStorage[key]) || 0;
             if (score > hs) {
