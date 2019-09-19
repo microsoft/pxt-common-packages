13290bfe116be64717a96e3fa0eec05052289bcb
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-09-19 13:25:42 -0700

    fixing tests

--- libs/azureiot/test.ts
+++ libs/azureiot/test.ts
@@ -1,15 +1,7 @@
 function test() {
 
     const log = console.log;
-    const esp = new esp32.NinaController(pins.spi(),
-        pins.D13, pins.D11, pins.D12, pins.D10, 1);
-
-    if (esp.isIdle)
-        return
-
-    log(`Firmware vers. ${esp.firmwareVersion}`)
-    log(`MAC addr: ${esp.MACaddress.toHex()}`)
-    log("Temp: " + esp.getTemperature())
+    const esp = net.Net.instance.controller();
 
     if (!esp.connect()) {
         log("can't connect")
