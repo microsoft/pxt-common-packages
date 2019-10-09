2faac58a2ac57fef0ffc05d96ec0f7420ac2c271
Richard Knoll <riknoll@microsoft.com>
2019-10-09 10:57:06 -0700

    Adding block for animation editor

--- libs/animation/animation.ts
+++ libs/animation/animation.ts
@@ -13,7 +13,7 @@ namespace animation {
     export class Point {
         public x: number;
         public y: number;
-        
+
         constructor(x: number, y: number) {
             this.x = x;
             this.y = y;
@@ -206,7 +206,7 @@ namespace animation {
             for (let i = 0; i < pathString.length; i++) {
                 const char = pathString.charAt(i);
                 const lastNode = path.nodes[path.nodes.length - 1];
-                
+
                 // This is an SVG path parser. It's kinda complicated. For each character, evaluate the following conditions:
                 // - if it's a digit, add it to the current argument
                 // - else if it's whitespace or newline, finish the current argument and prepare for the next one
@@ -223,7 +223,7 @@ namespace animation {
                 } else if (this.commandToArgCount(char) > -1) { // Parses command arguments
                     if (command && currentArg) {
                         args.push(parseInt(currentArg));
-                        
+
                         // Try to finish up this node, otherwise just toss it out
                         if (command && args.length >= this.commandToArgCount(command)) {
                             let node: PathNode = this.generateNode(p0, command, args, [
@@ -234,7 +234,7 @@ namespace animation {
                             p0 = node.getEndPoint(); // Set the start for the next node to the end of this node
                             if (node.setStart) pathStart = p0; // If this is a move command, then this sets the new start of the path (for the Z/z command)
                         }
-                        
+
                         // Clean up before continuing
                         command = "";
                         args = [];
@@ -255,7 +255,7 @@ namespace animation {
                         args.push(parseInt(currentArg));
                     }
                 }
-                
+
                 // If the command has a sufficient amount of arguments, then create a node for it
                 if (command && args.length >= this.commandToArgCount(command)) {
                     // Generate the node
@@ -266,7 +266,7 @@ namespace animation {
                     path.add(node);
                     p0 = node.getEndPoint();
                     if (node.setStart) pathStart = p0;
-                    
+
                     // Reset and prepare for the next command
                     command = "";
                     args = [];
@@ -289,7 +289,7 @@ namespace animation {
             const runningTime = control.millis() - startedAt; // The time since the start of the path
             const nodeIndex = Math.floor(runningTime / interval); // The current node
             const nodeTime = runningTime % interval; // The time the current node has been animating
-            
+
             if (this.lastNode > -1 && this.lastNode < nodeIndex && this.nodes.length) { // If the last node hasn't been completed yet
                 this.nodes[this.lastNode].apply(target, interval, interval); // Applies the last state of the previous node in case it was missed (this makes sure all moveTos fire)
 
@@ -309,7 +309,7 @@ namespace animation {
         }
 
         apply(target: Sprite, nodeTime: number, interval: number) {};
-        
+
         getLastControlPoint(): Point {
             return null;
         };
@@ -483,12 +483,12 @@ namespace animation {
 
             this.loop = loop;
         }
-        
+
         public update(): boolean {
             if (this.sprite.flags & sprites.Flag.Destroyed) return true;
-            
+
             if (this.startedAt == null) this.startedAt = control.millis();
-            
+
             let result = this.path.run(this.nodeInterval, this.sprite, this.startedAt);
             if (result) {
                 if (!this.loop) return true;
@@ -505,8 +505,7 @@ namespace animation {
      * @param frameInterval the time between changes, eg: 500
      */
     //% blockId=run_image_animation
-    //% block="animate $sprite=variables_get(mySprite) frames $frames=lists_create_with interval (ms) $frameInterval=timePicker loop $loop=toggleOnOff"
-    //% frames.defl=screen_image_picker
+    //% block="animate $sprite=variables_get(mySprite) frames $frames=animation_editor interval (ms) $frameInterval=timePicker loop $loop=toggleOnOff"
     //% group="Animate"
     export function runImageAnimation(sprite: Sprite, frames: Image[], frameInterval?: number, loop?: boolean) {
         const anim = new ImageAnimation(sprite, frames, frameInterval || 500, !!loop);
@@ -528,7 +527,7 @@ namespace animation {
         const anim = new MovementAnimation(sprite, path, duration / path.length, !!loop);
         anim.init();
     }
-    
+
     export enum AnimationTypes {
         //% block="all"
         All,
@@ -537,7 +536,7 @@ namespace animation {
         //% block="path"
         MovementAnimation
     }
-    
+
     /**
      * Stops all animations (simple and looping) of the specified type on a sprite
      * @param type the animation type to stop
@@ -569,7 +568,7 @@ namespace animation {
 
     //% fixedInstance whenUsed block="fly to center"
     export const flyToCenter = new PathPreset("L 80 60");
-    
+
     //% fixedInstance whenUsed block="shake"
     export const shake = new PathPreset("m 4 -1 m 1 2 m -6 2 m -4 -8 m 8 8 m 2 -4 m -8 0 m 6 3 m -3 -2");
 
@@ -622,5 +621,16 @@ namespace animation {
     //% blockHidden=1
     export function animationPresets(animationPath: PathPreset) {
         return animationPath.pathString;
+    }
+
+
+    //% blockId=animation_editor block="%frames"
+    //% shim=TD_ID
+    //% frames.fieldEditor="animation"
+    //% frames.fieldOptions.decompileLiterals="true"
+    //% frames.fieldOptions.filter="!tile !dialog"
+    //% weight=100 duplicateShadowOnDrag
+    export function _animationFrames(frames: Image[]) {
+        return frames
     }
 }
