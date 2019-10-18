0d88602fc0f554e0ea1451e56a6ccfdd11b9b88a
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-10-18 10:14:19 -0700

    move checks

--- libs/keyboard/keyboard.ts
+++ libs/keyboard/keyboard.ts
@@ -11,32 +11,37 @@ namespace keyboard {
         constructor() { }
 
         type(text: string) {
-            if (!this._keysDown) return;
-            for(let i = 0; i < text.length; ++i) {
-                const c = text.charCodeAt(i);
-                const ic = this._keysDown.indexOf(c);
-                if (ic > -1)
-                    this._keysDown.splice(ic, 1);
+            if (!text) return;
+
+            if (this._keysDown) {
+                for (let i = 0; i < text.length; ++i) {
+                    const c = text.charCodeAt(i);
+                    const ic = this._keysDown.indexOf(c);
+                    if (ic > -1)
+                        this._keysDown.splice(ic, 1);
+                }
             }
             __type(text);
         }
 
         key(key: string, event: KeyboardKeyEvent) {
+            if (!key) return;
+
             if (!this._keysDown) this._keysDown = [];
             const c = key.charCodeAt(0);
-            if(State.updateState(this._keysDown, c, event))
+            if (State.updateState(this._keysDown, c, event))
                 __key(c, event);
         }
 
         mediaKey(key: KeyboardMediaKey, event: KeyboardKeyEvent) {
             if (!this._mediasDown) this._mediasDown = [];
-            if(State.updateState(this._mediasDown, key, event))
+            if (State.updateState(this._mediasDown, key, event))
                 __mediaKey(key, event);
         }
 
         functionKey(key: KeyboardFunctionKey, event: KeyboardKeyEvent) {
             if (!this._functionsDown) this._functionsDown = [];
-            if(State.updateState(this._functionsDown, key, event))
+            if (State.updateState(this._functionsDown, key, event))
                 __functionKey(key, event);
         }
 
@@ -73,15 +78,15 @@ namespace keyboard {
             // send up command for all down keys and clear
             if (this._keysDown) {
                 this._keysDown.forEach(c => __key(c, KeyboardKeyEvent.Up));
-                this._keysDown = [];
+                this._keysDown = undefined;
             }
             if (this._mediasDown) {
                 this._mediasDown.forEach(c => __key(c, KeyboardKeyEvent.Up));
-                this._mediasDown = [];
+                this._mediasDown = undefined;
             }
             if (this._functionsDown) {
                 this._functionsDown.forEach(c => __key(c, KeyboardKeyEvent.Up));
-                this._functionsDown = [];
+                this._functionsDown = undefined;
             }
         }
     }
@@ -101,9 +106,7 @@ namespace keyboard {
     //% text.shadowOptions.toString=true
     //% help=keyboard/type
     export function type(text: string) {
-        if (!text) return;
-
-        const st =  state();
+        const st = state();
         st.type(text);
     }
 
@@ -114,7 +117,6 @@ namespace keyboard {
     //% blockGap=8 weight=99
     //% help=keyboard/key
     export function key(key: string, event: KeyboardKeyEvent) {
-        if (!key) return;
         const st = state();
         st.key(key, event);
     }
