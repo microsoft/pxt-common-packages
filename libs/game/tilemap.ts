8761e280668cdb8f33b251c409293a870000b246
Joey Wunderlich <jwunderl@users.noreply.github.com>
2019-12-17 16:48:04 -0800

    restrict tiles to only show up to the current tile size (#1041)
    * restrict tiles to only show up to the current scale
    
    * forgot compiler bug->getter needs to be before setter
    
    * clear tileview cache on scale change (don't dup images unnecessarily)
    
    * remove some now unneeded code (dup array and checks handled by cache invalidation)

--- libs/game/tilemap.ts
+++ libs/game/tilemap.ts
@@ -107,7 +107,9 @@ namespace tiles {
         protected layers: Image;
 
         protected tileset: Image[];
+        protected cachedTileView: Image[];
 
+        protected _scale: TileScale;
         protected _width: number;
         protected _height: number;
 
@@ -118,6 +120,7 @@ namespace tiles {
             this.data = data;
             this.layers = layers;
             this.tileset = tileset;
+            this.scale = scale;
 
             this._width = data.getNumber(NumberFormat.UInt16LE, 0);
             this._height = data.getNumber(NumberFormat.UInt16LE, 2);
@@ -132,6 +135,15 @@ namespace tiles {
 
         get height(): number {
             return this._height;
+        }
+
+        get scale(): TileScale {
+            return this._scale;
+        }
+
+        set scale(s: TileScale) {
+            this._scale = s;
+            this.cachedTileView = [];
         }
 
         getTile(col: number, row: number) {
@@ -151,7 +163,19 @@ namespace tiles {
         }
 
         getTileImage(index: number) {
-            return this.tileset[index];
+            const size = 1 << this.scale;
+            let cachedImage = this.cachedTileView[index];
+            if (!cachedImage) {
+                const originalImage = this.tileset[index];
+                if (originalImage.width <= size && originalImage.height <= size) {
+                    cachedImage = originalImage;
+                } else {
+                    cachedImage = image.create(size, size);
+                    cachedImage.drawImage(originalImage, 0, 0);
+                }
+                this.cachedTileView[index] = cachedImage;
+            }
+            return cachedImage;
         }
 
         setWall(col: number, row: number, on: boolean) {
@@ -175,6 +199,7 @@ namespace tiles {
          */
         _setTileImage(index: number, img: Image, collisions: boolean) {
             this.tileset[index] = img;
+            this.cachedTileView[index] = undefined;
             this._walls[index] = collisions;
             for (let col = 0; col < this.width; ++col) {
                 for (let row = 0; row < this.height; ++row) {
@@ -204,7 +229,7 @@ namespace tiles {
     }
 
     export class TileMap {
-        scale: number
+        protected _scale: TileScale;
 
         protected _layer: number;
         protected _map: TileMapData;
@@ -217,6 +242,17 @@ namespace tiles {
                 scene.TILE_MAP_Z,
                 (t, c) => this.draw(t, c)
             );
+        }
+
+        get scale() {
+            return this._scale;
+        }
+
+        set scale(s: TileScale) {
+            this._scale = s;
+            if (this._map) {
+                this._map.scale = s;
+            }
         }
 
         // ## LEGACY: DO NOT USE ##
@@ -567,7 +603,7 @@ namespace tiles {
      */
     //% blockId=mapgettilestype block="array of all $tile locations"
     //% tile.shadow=tileset_tile_picker
-    //% tile.decompileIndirectFixedInstances=true  
+    //% tile.decompileIndirectFixedInstances=true
     //% blockNamespace="scene" group="Tiles" blockGap=8
     //% help=tiles/get-tiles-by-type
     export function getTilesByType(tile: Image): Location[] {
