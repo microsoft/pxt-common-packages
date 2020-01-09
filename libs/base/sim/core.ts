f5a7c3b22e7b19022070ff7c726939d5b00016ee
Peli <jhalleux@microsoft.com>
2020-01-09 08:46:11 -0800

    missed interface

--- libs/base/sim/core.ts
+++ libs/base/sim/core.ts
@@ -2,7 +2,7 @@
 
 namespace pxsim {
     export interface CommonBoard extends CoreBoard
-        , EdgeConnectorBoard {
+        , EdgeConnectorBoard, EventBusBoard {
         bus: EventBus;
         buttonState: CommonButtonState;
         edgeConnectorState: EdgeConnectorState;
