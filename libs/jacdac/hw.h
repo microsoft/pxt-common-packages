6e267ed0b60c312ba6bc1ae8e63ff238abf20ae6
Michal Moskal <michal@moskal.me>
2020-03-06 08:00:00 -0600

    Measure overheads

--- libs/jacdac/hw.h
+++ libs/jacdac/hw.h
@@ -2,19 +2,21 @@
 
 #define JD_TX_QUEUE_SIZE 8
 
-#if defined(NRF52_SERIES)
-#define JD_WR_OVERHEAD 26 // TODO
+#if defined(NRF52840) || defined(NRF52832) || defined(NRF52833)
+// NRF52_SERIES not defined here for some reason
+#define JD_TIM_OVERHEAD 20
+#define JD_WR_OVERHEAD 7
 #elif defined(SAMD21)
 #define JD_TIM_OVERHEAD 29
 #define JD_WR_OVERHEAD 34
 #elif defined(SAMD51)
-#define JD_TIM_OVERHEAD 13
-#define JD_WR_OVERHEAD 0 // TODO
+#define JD_TIM_OVERHEAD 11
+#define JD_WR_OVERHEAD 10
 #elif defined(STM32F4)
-#define JD_TIM_OVERHEAD 3
-#define JD_WR_OVERHEAD -2
+#define JD_TIM_OVERHEAD 13
+#define JD_WR_OVERHEAD 7
 #endif
 
 #ifndef JD_TIM_OVERHEAD
-#define JD_TIM_OVERHEAD 5
+#define JD_TIM_OVERHEAD 12
 #endif
