54bb31d37474b75c444e0b38a4f472bdd9ab556d
Michal Moskal <michal@moskal.me>
2020-03-19 20:06:11 -0700

    Make jacdac-game compile again

--- libs/jacdac-game/controllerservice.ts
+++ libs/jacdac-game/controllerservice.ts
@@ -1,35 +1,36 @@
 namespace jacdac {
     //% fixedInstances
     export class ControllerService extends Broadcast {
-        promptedServers: number[];
+        promptedServers: string[];
         prompting: boolean;
+        players: string[];
 
         constructor() {
-            super("ctrl", jacdac.CONTROLLER_DEVICE_CLASS, 5);
+            super("ctrl", jacdac.CONTROLLER_DEVICE_CLASS);
+            this.players = [];
             this.promptedServers = [];
             this.prompting = false;
-            this.controlData[0] = JDControllerCommand.ControlServer;
         }
 
-        private connectClient(address: number, serverAddress: number, receivedPlayerIndex: number): number {
+        private connectClient(address: string, serverAddress: string, receivedPlayerIndex: number): number {
             // fast path: check if player is current
             if (!!serverAddress
                 && receivedPlayerIndex > 0
-                && receivedPlayerIndex < this.controlData.length
-                && address == this.controlData[receivedPlayerIndex]) {
+                && receivedPlayerIndex < this.players.length
+                && address == this.players[receivedPlayerIndex]) {
                 // player index and server address match
                 return receivedPlayerIndex;
             }
 
             // search existing player index
-            for (let i = 1; i < this.controlData.length; ++i)
-                if (address == this.controlData[i]) {
+            for (let i = 1; i < this.players.length; ++i)
+                if (address == this.players[i]) {
                     if (!serverAddress)
-                        this.sendPacket(this.controlData);
+                        this.sendState()
                     return i;
                 }
 
-            this.log(`new player ${toHex8(address)}`);
+            this.log(`new player ${(address)}`);
             const devices = jacdac.devices();
             const players = controller.players();
             const ids: number[] = [0, 0, 0, 0, 0]; // player 0 is not used
@@ -37,84 +38,77 @@ namespace jacdac {
 
             // did it move?
             // clean dead players
-            for (let i = 1; i < this.controlData.length; ++i) {
-                const ci = this.controlData[i];
+            for (let i = 1; i < this.players.length; ++i) {
+                const ci = this.players[i];
+                /*
                 if (ci && !devices.some(d => d.device_address == ci)) {
                     this.log(`del ${toHex8(this.controlData[i])} from ${i}`);
                     this.controlData[i] = 0;
                     const p = players.find(p => p.playerIndex == i);
                     if (p) p.connected = false;
                 }
+                */
             }
 
             // add new player
             // try receivedPlayerIndex first
             if (receivedPlayerIndex
-                && this.controlData[receivedPlayerIndex] == 0
+                && !this.players[receivedPlayerIndex]
                 && ids[receivedPlayerIndex]) {
-                this.log(`client ${toHex8(address)} -> p${receivedPlayerIndex}`);
-                this.controlData[receivedPlayerIndex] = address;
+                this.log(`client ${(address)} -> p${receivedPlayerIndex}`);
+                this.players[receivedPlayerIndex] = address;
                 return receivedPlayerIndex;
             }
 
             // try other positions 2,3,4 first
-            for (let i = 2; i < this.controlData.length; ++i) {
+            for (let i = 2; i < this.players.length; ++i) {
                 // if slot is free and there is such a player
-                if (this.controlData[i] == 0 && ids[i]) {
-                    this.log(`client ${toHex8(address)} -> p${i}`);
-                    this.controlData[i] = address;
+                if (!this.players[i] && ids[i]) {
+                    this.log(`client ${(address)} -> p${i}`);
+                    this.players[i] = address;
                     return i;
                 }
             }
             // try player 1
-            if (this.controlData[1] == 0 && ids[1]) {
-                this.log(`client ${toHex8(address)} -> ${1}`);
-                this.controlData[1] = address;
+            if (!this.players[1] && ids[1]) {
+                this.log(`client ${(address)} -> ${1}`);
+                this.players[1] = address;
                 return 1;
             }
 
             // no slots available
-            this.log(`no player for ${toHex8(address)}`);
+            this.log(`no player for ${(address)}`);
             return -1;
         }
 
-        handleServiceInformation(device: JDDevice, serviceInfo: JDServiceInformation): number {
-            const data = serviceInfo.data;
-            return this.processPacket(device.device_address, data);
-        }
-
-        handlePacket(packet: JDPacket): number {
-            const data = packet.data;
-            return this.processPacket(packet.device_address, data);
-        }
-
-        private processPacket(address: number, data: Buffer): number {
-            const cmd: JDControllerCommand = data[0];
-            switch (cmd) {
+        handlePacket(packet: JDPacket): void {
+            const address = packet.device_identifier
+            const data = packet.data
+            switch (packet.service_command) {
                 case JDControllerCommand.ControlClient:
-                    this.connectClient(address, data[1], data[2]);
-                    return DAL.DEVICE_OK;
+                    this.connectClient(address, data.slice(0, 8).toHex(), data[8]);
+                    return
                 case JDControllerCommand.ClientButtons:
                     return this.processClientButtons(address, data);
                 case JDControllerCommand.ControlServer:
                     return this.processControlServer(address, data);
                 default:
-                    return DAL.DEVICE_OK;
+                    return
             }
         }
 
-        private processControlServer(address: number, data: Buffer): number {
+        private processControlServer(address: string, data: Buffer): void {
             // already prompting for another server
-            if (this.prompting) return DAL.DEVICE_OK;
+            if (this.prompting) return;
             // so there's another server on the bus,
             // if we haven't done so yet, prompt the user if he wants to join the game
-            const device = jacdac.devices().find(d => d.device_address == address);
+            const device = jacdac.devices().find(d => d.deviceId == address);
             if (!device) // can't find any device at that address
-                return DAL.DEVICE_OK;
+                return;
 
             // check if prompted already
-            if (this.promptedServers.indexOf(device.udidl) >= 0)
-                return DAL.DEVICE_OK;
+            if (this.promptedServers.indexOf(device.deviceId) >= 0)
+                return;
 
             this.prompting = true;
             control.runInParallel(() => {
@@ -123,17 +117,15 @@ namespace jacdac {
                     joinGame();
                 this.prompting = false;
             });
-
-            return DAL.DEVICE_OK;
         }
 
         private hasPlayers(): boolean {
-            for (let i = 1; i < this.controlData.length; ++i)
-                if (this.controlData[i]) return true;
+            for (let i = 1; i < this.players.length; ++i)
+                if (this.players[i]) return true;
             return false;
         }
 
-        private askJoin(device: JDDevice): boolean {
+        private askJoin(device: Device): boolean {
             game.eventContext(); // initialize the game
             control.pushEventContext();
             game.showDialog("Arcade Detected", "Join?", "A = OK, B = CANCEL");
@@ -146,41 +138,48 @@ namespace jacdac {
                 // server got joined
                 || this.hasPlayers()
                 // other driver dissapeared
-                || !jacdac.devices().find(d => d.device_address == device.device_address)
+                || !jacdac.devices().find(d => d.deviceId == device.deviceId)
             );
             // wait until we have an answer or the service
             control.popEventContext();
 
             // cache user answer
             if (answer !== null)
-                this.promptedServers.push(device.udidl);
+                this.promptedServers.push(device.deviceId);
 
             // check that we haven't been join by then
             return !!answer
                 && !this.hasPlayers()
-                && !!jacdac.devices().find(d => d.device_address == device.device_address);
+                && !!jacdac.devices().find(d => d.deviceId == device.deviceId);
         }
 
-        private processClientButtons(address: number, data: Buffer): number {
-            const playerIndex = this.connectClient(address, -1, 0);
+        private processClientButtons(address: string, data: Buffer): void {
+            const playerIndex = this.connectClient(address, null, 0);
             if (playerIndex < 0) {
-                this.log(`no player for ${toHex8(address)}`);
-                return DAL.DEVICE_BUSY;
+                this.log(`no player for ${address}`);
+                return;
             }
             const player = controller.players().find(p => p.playerIndex == playerIndex);
             if (!player) {
                 this.log(`no player ${player.playerIndex}`);
-                return DAL.DEVICE_OK;
+                return;
             }
             const state = data[1];
             const btns = player.buttons;
             for (let i = 0; i < btns.length; ++i)
                 btns[i].setPressed(!!(state & (1 << (i + 1))));
-            return DAL.DEVICE_OK;
+            return;
         }
 
         sendState() {
-            this.sendPacket(this.controlData);
+            const d = Buffer.create(this.players.length * 8)
+            let i = 0
+            for (let p of this.players) {
+                if (p)
+                    d.write(i, Buffer.fromHex(p))
+                i += 8
+            }
+            this.sendReport(JDPacket.from(CMD_ADVERTISEMENT_DATA, 0, d))
         }
     }
 
