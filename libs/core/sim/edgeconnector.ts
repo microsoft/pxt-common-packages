10397d39cb23a9194e4a2b2d7fd1bc9324986015
Peli <jhalleux@microsoft.com>
2020-03-23 22:47:31 -0700

    missing constants

--- libs/core/sim/edgeconnector.ts
+++ libs/core/sim/edgeconnector.ts
@@ -46,8 +46,8 @@ namespace pxsim {
         setPull(pull: number) {
             this.pull = pull;
             switch(pull) {
-                case PinPullMode.PullDown: this.value = 0; break;
-                case PinPullMode.PullUp: this.value = 1023; break;
+                case 2 /*PinPullMode.PullDown*/: this.value = 0; break;
+                case 1 /*PinPullMode.PullUp*/: this.value = 1023; break;
                 default: this.value = Math.randomRange(0, 1023); break;
             }
         }
