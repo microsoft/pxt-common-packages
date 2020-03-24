172707f3984df940d2db06ceaa7f5433c58144f0
Peli <jhalleux@microsoft.com>
2020-03-23 22:42:34 -0700

    random value when no pull

--- libs/core/sim/edgeconnector.ts
+++ libs/core/sim/edgeconnector.ts
@@ -45,7 +45,11 @@ namespace pxsim {
 
         setPull(pull: number) {
             this.pull = pull;
-            this.value = pull == PinPullMode.PullDown ? 0 : 1023;
+            switch(pull) {
+                case PinPullMode.PullDown: this.value = 0; break;
+                case PinPullMode.PullUp: this.value = 1023; break;
+                default: this.value = Math.randomRange(0, 1023); break;
+            }
         }
 
         analogReadPin(): number {
