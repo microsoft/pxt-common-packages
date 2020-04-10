edc3dc96d94ab62d4ad836fddf6d3f9458a850bb
Michal Moskal <michal@moskal.me>
2020-04-09 19:50:31 -0700

    Add RotaryEncoderClient.setRange()

--- libs/jacdac-services/rotaryencoderclient.ts
+++ libs/jacdac-services/rotaryencoderclient.ts
@@ -5,6 +5,20 @@ namespace jacdac {
             super("crank", jd_class.ROTARY_ENCODER, requiredDevice);
         }
 
+        scale = 1
+        private _min: number
+        private _max: number
+        private _offset: number
+
+        /**
+         * Always clamp the encoder position to given range.
+         */
+        setRange(min: number, max: number) {
+            this._min = min
+            this._max = max
+            this._offset = 0
+        }
+
         /**
          * Gets the position of the rotary encoder
          */
@@ -13,7 +27,18 @@ namespace jacdac {
         get position(): number {
             const st = this.state;
             if (!st || st.length < 4) return 0;
-            return st.getNumber(NumberFormat.Int32LE, 0);
+
+            const curr = st.getNumber(NumberFormat.Int32LE, 0) * this.scale
+            if (this._offset != null) {
+                const p = curr + this._offset
+                if (p < this._min)
+                    this._offset = this._min - curr
+                else if (p > this._max)
+                    this._offset = this._max - curr
+                return curr + this._offset
+            } else {
+                return curr
+            }
         }
 
         /**
