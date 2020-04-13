a3529dc5512f2f75d23602305699165f0dbf55fb
Peli <jhalleux@microsoft.com>
2020-04-13 08:02:56 -0700

    moving enums to ts

--- libs/keyboard/keyboard.cpp
+++ libs/keyboard/keyboard.cpp
@@ -3,123 +3,6 @@
 #include "pxt.h"
 #include "USB_HID_Keys.h"
 
-enum class KeyboardMediaKey
-{
-    //% block="mute"
-    Mute,
-    //% block="volume up"
-    VolumeUp,
-    //% block="volume down"
-    VolumeDown,
-    //% block="play pause"
-    PlayPause,
-    //% block="stop"
-    Stop,
-    //% block="previous track"
-    PreviousTrack,
-    //% block="next track"
-    NextTrack,
-    //% block="mail"
-    Mail,
-    //% block="calculator"
-    Calculator,
-    //% block="web search"
-    WebSearch,
-    //% block="web home"
-    WebHome,
-    //% block="web favourites"
-    WebFavourites,
-    //% block="web refresh"
-    WebRefresh,
-    //% block="web stop"
-    WebStop,
-    //% block="web forward"
-    WebForward,
-    //% block="web back"
-    WebBack
-};
-
-enum class KeyboardFunctionKey
-{
-    //% block="F1"
-    F1Key = KEY_F1,  
-    //% block="F2"
-    F2Key = KEY_F2,
-    //% block="F3"
-    F3Key = KEY_F3,
-    //% block="F4"
-    F4Key = KEY_F4, 
-    //% block="F5"
-    F5Key = KEY_F5,
-    //% block="F6"
-    F6Key = KEY_F6,
-    //% block="F7"
-    F7Key = KEY_F7,
-    //% block="F8"
-    F8Key = KEY_F8,
-    //% block="F9"
-    F9Key = KEY_F9,
-    //% block="F0"
-    F10Key = KEY_F10,
-    //% block="F11"
-    F11Key = KEY_F11,
-    //% block="F12"
-    F12Key = KEY_F12,
-    //% block="F13"
-    F13Key = KEY_F13,
-    //% block="F14"
-    F14Key = KEY_F14,
-    //% block="F15"
-    F15Key = KEY_F15,
-    //% block="F16"
-    F16Key = KEY_F16,
-    //% block="F17"
-    F17Key = KEY_F17,
-    //% block="F18"
-    F18Key = KEY_F18,
-    //% block="F19"
-    F19Key = KEY_F19,
-    //% block="F20"
-    F20Key = KEY_F20,
-    //% block="F21"
-    F21Key = KEY_F21,
-    //% block="F22"
-    F22Key = KEY_F22,
-    //% block="F23"
-    F23Key = KEY_F23,
-    //% block="F24"
-    F24Key = KEY_F24,
-
-
-    //% block="print screen"
-    PrintScreen = KEY_SYSRQ,
-    //% block="scroll lock"
-    ScrollLock = KEY_SCROLLLOCK,
-    //% block="pause"
-    Pause = KEY_PAUSE,
-    //% block="insert"
-    Insert = KEY_INSERT,
-    //% block="home"
-    Home = KEY_HOME,
-    //% block="page up"
-    PageUp = KEY_PAGEUP,
-    //% block="delete"
-    DeleteForward = KEY_DELETE,
-    //% block="end"
-    End = KEY_END,
-    //% block="page down"
-    PageDown = KEY_PAGEDOWN,
-
-    //% block="right arrow"
-    RightArrow = KEY_RIGHT,
-    //% block="left arrow"
-    LeftArrow = KEY_LEFT,
-    //% block="down arrow"
-    DownArrow = KEY_DOWN,
-    //% block="up arrow"
-    UpArrow = KEY_UP
-};
-
 enum class KeyboardKeyEvent {
     //% block="press"
     Press,
@@ -152,7 +35,7 @@ namespace keyboard {
     }
 
     //%
-    void __mediaKey(KeyboardMediaKey key, KeyboardKeyEvent event) {
+    void __mediaKey(uint16 key, KeyboardKeyEvent event) {
         codal::MediaKey ckey = (codal::MediaKey)((int)codal::MediaKey::Mute + (int)key);
         switch(event) {
             case KeyboardKeyEvent::Down:
@@ -168,7 +51,7 @@ namespace keyboard {
     }
 
     //%
-    void __functionKey(KeyboardFunctionKey key, KeyboardKeyEvent event) {
+    void __functionKey(uint16 key, KeyboardKeyEvent event) {
         codal::FunctionKey ckey = (codal::FunctionKey)key;
         switch(event) {
             case KeyboardKeyEvent::Down:
--- libs/keyboard/keyboard.ts
+++ libs/keyboard/keyboard.ts
@@ -1,3 +1,121 @@
+
+const enum KeyboardMediaKey
+{
+    //% block="mute"
+    Mute,
+    //% block="volume up"
+    VolumeUp,
+    //% block="volume down"
+    VolumeDown,
+    //% block="play pause"
+    PlayPause,
+    //% block="stop"
+    Stop,
+    //% block="previous track"
+    PreviousTrack,
+    //% block="next track"
+    NextTrack,
+    //% block="mail"
+    Mail,
+    //% block="calculator"
+    Calculator,
+    //% block="web search"
+    WebSearch,
+    //% block="web home"
+    WebHome,
+    //% block="web favourites"
+    WebFavourites,
+    //% block="web refresh"
+    WebRefresh,
+    //% block="web stop"
+    WebStop,
+    //% block="web forward"
+    WebForward,
+    //% block="web back"
+    WebBack
+}
+
+const enum KeyboardFunctionKey
+{
+    //% block="F1"
+    F1Key = DAL.KEY_F1,  
+    //% block="F2"
+    F2Key = DAL.KEY_F2,
+    //% block="F3"
+    F3Key = DAL.KEY_F3,
+    //% block="F4"
+    F4Key = DAL.KEY_F4, 
+    //% block="F5"
+    F5Key = DAL.KEY_F5,
+    //% block="F6"
+    F6Key = DAL.KEY_F6,
+    //% block="F7"
+    F7Key = DAL.KEY_F7,
+    //% block="F8"
+    F8Key = DAL.KEY_F8,
+    //% block="F9"
+    F9Key = DAL.KEY_F9,
+    //% block="F0"
+    F10Key = DAL.KEY_F10,
+    //% block="F11"
+    F11Key = DAL.KEY_F11,
+    //% block="F12"
+    F12Key = DAL.KEY_F12,
+    //% block="F13"
+    F13Key = DAL.KEY_F13,
+    //% block="F14"
+    F14Key = DAL.KEY_F14,
+    //% block="F15"
+    F15Key = DAL.KEY_F15,
+    //% block="F16"
+    F16Key = DAL.KEY_F16,
+    //% block="F17"
+    F17Key = DAL.KEY_F17,
+    //% block="F18"
+    F18Key = DAL.KEY_F18,
+    //% block="F19"
+    F19Key = DAL.KEY_F19,
+    //% block="F20"
+    F20Key = DAL.KEY_F20,
+    //% block="F21"
+    F21Key = DAL.KEY_F21,
+    //% block="F22"
+    F22Key = DAL.KEY_F22,
+    //% block="F23"
+    F23Key = DAL.KEY_F23,
+    //% block="F24"
+    F24Key = DAL.KEY_F24,
+
+
+    //% block="print screen"
+    PrintScreen = DAL.KEY_SYSRQ,
+    //% block="scroll lock"
+    ScrollLock = DAL.KEY_SCROLLLOCK,
+    //% block="pause"
+    Pause = DAL.KEY_PAUSE,
+    //% block="insert"
+    Insert = DAL.KEY_INSERT,
+    //% block="home"
+    Home = DAL.KEY_HOME,
+    //% block="page up"
+    PageUp = DAL.KEY_PAGEUP,
+    //% block="delete"
+    DeleteForward = DAL.KEY_DELETE,
+    //% block="end"
+    End = DAL.KEY_END,
+    //% block="page down"
+    PageDown = DAL.KEY_PAGEDOWN,
+
+    //% block="right arrow"
+    RightArrow = DAL.KEY_RIGHT,
+    //% block="left arrow"
+    LeftArrow = DAL.KEY_LEFT,
+    //% block="down arrow"
+    DownArrow = DAL.KEY_DOWN,
+    //% block="up arrow"
+    UpArrow = DAL.KEY_UP
+}
+
 const enum KeyboardModifierKey {
     //% block="CTRL"
     Control = DAL.KEY_MOD_LCTRL,
