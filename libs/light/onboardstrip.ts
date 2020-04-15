594e4f5b0c6430bc5e47fafb83cfa4619743efa3
Peli <jhalleux@microsoft.com>
2020-04-14 22:54:36 -0700

    add keys for onboard strips

--- libs/light/onboardstrip.ts
+++ libs/light/onboardstrip.ts
@@ -10,11 +10,11 @@ namespace light {
     export function onboardStrip(): NeoPixelStrip {
         if (_onboardStrip) return _onboardStrip;
 
-        const data = pins.pinByCfg(DAL.CFG_PIN_DOTSTAR_DATA);
-        const clk = pins.pinByCfg(DAL.CFG_PIN_DOTSTAR_CLOCK);
-        const dsnum = control.getConfigValue(DAL.CFG_NUM_DOTSTARS, 0);
-        const neo = pins.pinByCfg(DAL.CFG_PIN_NEOPIXEL);
-        const neonum = control.getConfigValue(DAL.CFG_NUM_NEOPIXELS, 0);
+        const data = pins.pinByCfg(DAL.CFG_PIN_ONBOARD_DOTSTAR_DATA);
+        const clk = pins.pinByCfg(DAL.CFG_PIN_ONBOARD_DOTSTAR_CLOCK);
+        const dsnum = control.getConfigValue(DAL.CFG_NUM_ONBOARD_DOTSTARS, 0);
+        const neo = pins.pinByCfg(DAL.CFG_PIN_ONBOARD_NEOPIXEL);
+        const neonum = control.getConfigValue(DAL.CFG_NUM_ONBOARD_NEOPIXELS, 0);
         if (data && clk && dsnum > 0) {
             _onboardStrip = light.createAPA102Strip(data, clk, dsnum);
             _onboardStrip.setBrightness(96);
