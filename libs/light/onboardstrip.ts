2cfb344aae41d491b7e86806f8ab237bff398a77
Peli de Halleux <pelikhan@users.noreply.github.com>
2020-04-15 11:40:52 -0700

    Onboard pins cfg keys + keypad (#1097)
    * working on keypad api
    
    * basic keypad (untested)
    
    * config keys for keypad
    
    * load keypad config from settings
    
    * finish config
    
    * add keys for onboard strips
    
    * updated onboard logic
    
    * logic
    
    * fix typo in keys
    
    * updated dal
    
    * typo
    
    * delete old scripts
    
    * refresh dal.d.ts
    
    * fixing keys
    
    * fix column pins
    
    * add default id
    
    * updated
    
    * handle from cfg
    
    * update namespace name
    
    * more cleanup
    
    * fix event id generation
    
    * indexing issue
    
    * fix pull
    
    * button events
    
    * updated enum
    
    * adding comments

--- libs/light/onboardstrip.ts
+++ libs/light/onboardstrip.ts
@@ -1,7 +1,7 @@
 namespace light {
     let _onboardStrip: light.LightStrip;
     /**
-     * Get the default light strip.
+     * Get the onboard light strip.
      */
     //% help=light/onboard-strip
     //% blockId="neopixel_onboard_strip" block="onboard strip"
@@ -10,19 +10,40 @@ namespace light {
     export function onboardStrip(): NeoPixelStrip {
         if (_onboardStrip) return _onboardStrip;
 
-        const data = pins.pinByCfg(DAL.CFG_PIN_DOTSTAR_DATA);
-        const clk = pins.pinByCfg(DAL.CFG_PIN_DOTSTAR_CLOCK);
-        const dsnum = control.getConfigValue(DAL.CFG_NUM_DOTSTARS, 0);
-        const neo = pins.pinByCfg(DAL.CFG_PIN_NEOPIXEL);
-        const neonum = control.getConfigValue(DAL.CFG_NUM_NEOPIXELS, 0);
-        if (data && clk && dsnum > 0) {
-            _onboardStrip = light.createAPA102Strip(data, clk, dsnum);
-            _onboardStrip.setBrightness(96);
-        } else if (neo && neonum > 0) {
-            _onboardStrip = light.createNeoPixelStrip(neo, neonum, NeoPixelMode.RGB);
-        } else {
-            _onboardStrip = light.createNeoPixelStrip(undefined, 0);
+        { // try onboard cfg
+            const data = pins.pinByCfg(DAL.CFG_PIN_ONBOARD_DOTSTAR_DATA);
+            const clk = pins.pinByCfg(DAL.CFG_PIN_ONBOARD_DOTSTAR_CLOCK);
+            const dsnum = control.getConfigValue(DAL.CFG_NUM_ONBOARD_DOTSTARS, 0);
+            const neo = pins.pinByCfg(DAL.CFG_PIN_ONBOARD_NEOPIXEL);
+            const neonum = control.getConfigValue(DAL.CFG_NUM_ONBOARD_NEOPIXELS, 0);
+            if (data && clk && dsnum > 0) {
+                _onboardStrip = light.createAPA102Strip(data, clk, dsnum);
+                _onboardStrip.setBrightness(96);
+                return _onboardStrip;
+            } else if (neo && neonum > 0) {
+                _onboardStrip = light.createNeoPixelStrip(neo, neonum, NeoPixelMode.RGB);
+                return _onboardStrip;
+            }
         }
+
+        { // resolve from pins
+            const data = pins.pinByCfg(DAL.CFG_PIN_DOTSTAR_DATA);
+            const clk = pins.pinByCfg(DAL.CFG_PIN_DOTSTAR_CLOCK);
+            const dsnum = control.getConfigValue(DAL.CFG_NUM_DOTSTARS, 0);
+            const neo = pins.pinByCfg(DAL.CFG_PIN_NEOPIXEL);
+            const neonum = control.getConfigValue(DAL.CFG_NUM_NEOPIXELS, 0);
+            if (data && clk && dsnum > 0) {
+                _onboardStrip = light.createAPA102Strip(data, clk, dsnum);
+                _onboardStrip.setBrightness(96);
+                return _onboardStrip;
+            } else if (neo && neonum > 0) {
+                _onboardStrip = light.createNeoPixelStrip(neo, neonum, NeoPixelMode.RGB);
+                return _onboardStrip;
+            } else {
+                _onboardStrip = light.createNeoPixelStrip(undefined, 0);
+            }
+        }
+
         return _onboardStrip;
     }
 }
