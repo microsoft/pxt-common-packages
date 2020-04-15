a194392e4b8f31dbb2f932f9d47b0e0b0de2d85f
Peli <jhalleux@microsoft.com>
2020-04-15 09:20:05 -0700

    fix pull

--- libs/matrix-keypad/keypad.ts
+++ libs/matrix-keypad/keypad.ts
@@ -17,11 +17,13 @@ namespace matrixKeypad {
             const rowPins: DigitalInOutPin[] = [];
             for(let i = 0; i < rows; ++i) {
                 const p = pins.pinByCfg(DAL.CFG_PIN_MATRIX_KEYPAD_ROW0 + i);
+                control.assert(!!p, control.PXT_PANIC.CODAL_HARDWARE_CONFIGURATION_ERROR)
                 rowPins.push(p);
             }
             const columnPins: DigitalInOutPin[] = [];
             for(let i = 0; i < columns; ++i) {
                 const p = pins.pinByCfg(DAL.CFG_PIN_MATRIX_KEYPAD_COL0 + i);
+                control.assert(!!p, control.PXT_PANIC.CODAL_HARDWARE_CONFIGURATION_ERROR)
                 columnPins.push(p);
             }
             return new MatrixKeypad(messageBusId, rowPins, columnPins);
@@ -34,7 +36,7 @@ namespace matrixKeypad {
 
         static setInput(p: DigitalInOutPin) {
             p.digitalRead();
-            p.setPull(PinPullMode.PullUp)
+            p.setPull(PinPullMode.PullDown)
         }
 
         private evId(x: number, y: number, ev: number) {
@@ -44,36 +46,36 @@ namespace matrixKeypad {
         private pulseRows() {
             this.rowPins.forEach(p => MatrixKeypad.setInput(p));
             this.columnPins.forEach(p => MatrixKeypad.setInput(p));
-
             this.rowPins.forEach((row, y) => this.pulseRow(y));
+            this.rowPins.forEach(p => MatrixKeypad.setInput(p));
         }
 
         private pulseRow(y: number) {
             const row = this.rowPins[y];
             const time = control.millis();
             // set one row low
-            row.digitalWrite(false);
+            row.digitalWrite(true);
             // check the column pins, which ones are pulled down
             this.columnPins.forEach((col, x) => {
                 const pressed = col.digitalRead();
                 const idx = x + y * this.columns;
                 const wasPressed = !!this.timePressed[idx];
                 if (wasPressed != pressed) {
-                    control.raiseEvent(this.messageBusId, this.evId(x, y, pressed ? MatrixKeypadEvent.Pressed : MatrixKeypadEvent.Released));
                     this.timePressed[idx] = pressed ? time : 0;
+                    control.raiseEvent(this.messageBusId, this.evId(x, y, pressed ? MatrixKeypadEvent.Pressed : MatrixKeypadEvent.Released));
                 }
             });
-            // reset the pin to be an input
             MatrixKeypad.setInput(row);
             // reset events
-            row.onEvent(PinEvent.PulseHigh, () => this.pulseRow(y));
-            row.onEvent(PinEvent.PulseLow, () => this.pulseRow(y));
+           // row.onEvent(PinEvent.Fall, () => this.pulseRow(y));
+           // row.onEvent(PinEvent.Rise, () => this.pulseRow(y));
         }
 
         /**
          * Gets the number of rows
          */
-        //% blockId=mkeypadrows block="keypad %keypad rows"
+        //% blockId=mkeypadrows
+        //% blockSetVariable="rows" blockCombine block="rows"
         get rows(): number {
             return this.rowPins.length;
         }
@@ -81,7 +83,8 @@ namespace matrixKeypad {
         /**
          * Gets the number of columns
          */
-        //% blockId=mkeypadcolumns block="keypad %keypad columns"
+        //% blockId=mkeypadcolumns
+        //% blockSetVariable="columns" blockCombine block="columns"
         get columns(): number {
             return this.columnPins.length;
         }
@@ -101,7 +104,7 @@ namespace matrixKeypad {
                 return;
 
             control.onEvent(this.messageBusId, this.evId(x, y, ev), handler);
-            this.pulseRow(y); // raise events as needed
+            this.pulseRows(); // raise events as needed
         }
 
         /**
@@ -116,6 +119,7 @@ namespace matrixKeypad {
             if (x < 0 || y < 0 || x >= this.columns || y >= this.rows)
                 return false;
 
+            this.pulseRows();
             return !!this.timePressed[x + y * this.columns];
         }
     }
