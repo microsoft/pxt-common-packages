b68ba235e9f61af61a2a03d581024c71e20a0b67
Peli <jhalleux@microsoft.com>
2020-05-13 22:54:14 -0700

    updated enums

--- libs/serial/enums.d.ts
+++ libs/serial/enums.d.ts
@@ -1,7 +1,7 @@
 // Auto-generated. Do not edit.
 
 
-    declare const enum BaudRate {
+declare const enum BaudRate {
     //% block=115200
     BaudRate115200 = 115200,
     //% block=57600
@@ -26,14 +26,40 @@
     BaudRate1200 = 1200,
     //% block=300
     BaudRate300 = 300,
-    }
+}
 
 
-    declare const enum SerialEvent {
+declare const enum SerialEvent {
     //% block="data received"
     DataReceived = 4,  // CODAL_SERIAL_EVT_DATA_RECEIVED
     //% block="rx buffer full"
     RxBufferFull = 3,  // CODAL_SERIAL_EVT_RX_FULL
-    }
+}
+
+
+declare const enum Delimiters {
+    //% block="new line (\n)"
+    NewLine = 10,
+    //% block=","
+    Comma = 44,
+    //% block="$"
+    Dollar = 36,
+    //% block=":"
+    Colon = 58,
+    //% block="."
+    Fullstop = 46,
+    //% block="#"
+    Hash = 35,
+    //% block="carriage return (\r)"
+    CarriageReturn = 13,
+    //% block="space"
+    Space = 32,
+    //% block="tab (\t)"
+    Tab = 9,
+    //% block="|"
+    Pipe = 124,
+    //% block=";"
+    SemiColon = 59,
+}
 
 // Auto-generated. Do not edit. Really.
