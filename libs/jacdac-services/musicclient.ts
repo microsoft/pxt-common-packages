7dfa464b7e547bf60ec6e82cd96a75556a152e49
Michał Moskal <michal@moskal.me>
2020-06-05 07:51:58 -0700

    Allow melody playing through jacdac (#1115)

--- libs/jacdac-services/musicclient.ts
+++ libs/jacdac-services/musicclient.ts
@@ -1,8 +1,75 @@
 namespace jacdac {
+    class ToneToPlay {
+        constructor(public payload: Buffer, public timestamp: number) { }
+    }
+
+    function tonePayload(frequency: number, ms: number, volume: number) {
+        const period = Math.idiv(1000000, frequency)
+        const duty = (period * volume) >> 11
+        return Buffer.pack("HHH", [period, duty, ms])
+    }
+
+    class JDMelodyPlayer extends music.MelodyPlayer {
+        private queue: ToneToPlay[]
+
+        constructor(m: music.Melody, private musicClient: MusicClient) {
+            super(m)
+        }
+
+        stop() {
+            this.queue = null
+            super.stop()
+        }
+
+        private playLoop() {
+            while (true) {
+                if (this.queue == null)
+                    break
+                const e = this.queue.shift()
+                if (!e) {
+                    this.queue = null
+                    return
+                }
+                const now = control.millis()
+                const delta = e.timestamp - now
+                if (delta > 0)
+                    pause(delta)
+                this.musicClient.sendCommand(JDPacket.from(JDMusicCommand.PlayTone, e.payload))
+            }
+        }
+
+        protected queuePlayInstructions(timeDelta: number, buf: Buffer) {
+            const isize = 12
+            let timestamp = control.millis() + timeDelta
+            let startPlay = false
+            if (this.queue == null) {
+                this.queue = []
+                startPlay = true
+            }
+            for (let off = 0; off < buf.length - (isize - 1); off += isize) {
+                const [soundWave, flags, frequency, duration, startVolume, endVolume, endFrequency] = buf.unpack("BBHHHHH", off)
+                const freq = (frequency + endFrequency) >> 1
+                const vol = (startVolume + endVolume) >> 1
+                this.queue.push(new ToneToPlay(tonePayload(freq, duration, vol), timestamp))
+                timestamp += duration
+            }
+            if (startPlay)
+                control.runInBackground(() => this.playLoop())
+        }
+    }
+
     //% fixedInstances
     export class MusicClient extends Client {
         constructor(requiredDevice: string = null) {
             super("mus", jd_class.MUSIC, requiredDevice);
+        }
+
+        private player: JDMelodyPlayer
+        playMelody(melody: music.Melody, volume: number = 255) {
+            if (this.player) this.player.stop()
+            this.player = new JDMelodyPlayer(melody, this)
+            // cancel effects of music.volume()
+            this.player.play(Math.idiv(volume << 8, music.volume()))
         }
 
         /**
@@ -15,9 +82,8 @@ namespace jacdac {
         //% duration.defl=500
         //% weight=76 blockGap=8
         //% group="Music"
-        playTone(frequency: number, ms: number): void {
-            this.sendPackedCommand(
-                JDMusicCommand.PlayTone, "II", [frequency, ms])
+        playTone(frequency: number, ms: number, volume = 255): void {
+            this.sendCommand(JDPacket.from(JDMusicCommand.PlayTone, tonePayload(frequency, ms, volume << 2)))
         }
     }
 
