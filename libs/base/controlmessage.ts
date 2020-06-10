f143df43b12c18f87c26600d8c3e05531efc888c
peli <pelikhan@users.noreply.github.com>
2020-06-10 15:39:24 -0700

    adding general purpose message queues

---
+++ libs/base/controlmessage.ts
@@ -0,0 +1,48 @@
+namespace control.messages {
+    // these events are raised by C++/JS when messages come in
+    export const CONTROL_MESSAGE_EVT_ID = 2999;
+    export const CONTROL_MESSAGE_RECEIVED = 1;
+
+    //% shim=control::messages::send
+    export function send(channel: string, message: Buffer) {
+        // leave empty: this function is implemented in the simulator
+    }
+
+    function peekChannel(): string {
+        // leave empty: this function is implemented in the simulator
+        return null;
+    }
+    
+    //% shim=control::messages::readMessage
+    function readMessage(): Buffer {
+        // leave empty: this function is implemented in the simulator
+        return null;
+    }
+
+    let handlers: { [channel: string] : (msg: Buffer) => void}
+    function consumeMessages() {
+        while(true) {
+            // peek channel of next message
+            const channel = peekChannel();
+            if (!channel) break;
+            // read next message
+            const msg = readMessage();
+            // send to handler
+            const handler = handlers && handlers[channel];
+            if (handler)
+                handler(msg);
+        }
+    }
+
+    /** 
+     * Registers the handler for a message on a given channel 
+     **/
+    export function onReceived(channel: string, handler: (msg: Buffer) => void) {
+        if (!channel) return;
+
+        if (!handlers)
+            handlers = {};
+        handlers[channel] = handler;
+        control.onEvent(CONTROL_MESSAGE_EVT_ID, CONTROL_MESSAGE_RECEIVED, consumeMessages);
+    }
+}
