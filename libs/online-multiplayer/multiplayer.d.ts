6c67aacd5379fb446344ec537d3ad84d6bd7252a
Vivian Li <vivl@microsoft.com>
2020-07-28 14:37:44 -0700

    Add shim

--- libs/online-multiplayer/multiplayer.d.ts
+++ libs/online-multiplayer/multiplayer.d.ts
@@ -2,8 +2,12 @@ declare namespace multiplayer {
     //% shim=multiplayer::postImage
     function postImage(im: Image, goal: string): void;
 
-    function setImage(im: Image): void;
+    //% shim=multiplayer::getCurrentIMage
+    function getCurrentImage(im: Image): void;
 
     //% shim=multiplayer::setIsClient
     function setIsClient(on: boolean): void;
+
+    //% shim=multiplayer::getOrigin
+    function getOrigin(): string;
 }
