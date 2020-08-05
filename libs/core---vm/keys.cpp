869feb1ed38dbd45e61c5013c6274989c99672ca
Michal Moskal <michal@moskal.me>
2020-08-05 15:04:11 +0200

    Add dummy pressureLevelByButtonId() in VM

--- libs/core---vm/keys.cpp
+++ libs/core---vm/keys.cpp
@@ -2,4 +2,17 @@
 
 namespace pxt {
 void initKeys() {}
+
+//% expose
+int pressureLevelByButtonId(int btnId, int codalId) {
+    return 0; // TODO
+}
+
+//% expose
+void setupButton(int buttonId, int key) {
+    (void)buttonId;
+    (void)key;
+    // not needed on RPi
+}
+
 } // namespace pxt
