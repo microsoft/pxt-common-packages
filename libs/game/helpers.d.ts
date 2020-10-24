b171691590d7c24a78533788967d653bd1374944
Joey Wunderlich <joey.wunderlich@gmail.com>
2020-10-24 00:30:47 -0700

    optional data

--- libs/game/helpers.d.ts
+++ libs/game/helpers.d.ts
@@ -1,5 +1,5 @@
 
 declare namespace helpers {
     //% shim=helpers::__postToParent
-    function _postToParent(status: string, data: { [index: string]: string | number }): void;
+    function _postToParent(status: string, data?: { [index: string]: string | number }): void;
 }
