d86e5f2207058d44795533f417153e1b8ce0ff86
Joey Wunderlich <joey.wunderlich@gmail.com>
2021-01-05 14:30:58 -0800

    fix ask block inside on a / b pressed controller event

--- libs/game/ask.ts
+++ libs/game/ask.ts
@@ -9,17 +9,30 @@ namespace game {
     //% blockId=gameask block="ask %title||%subtitle"
     //% group="Prompt"
     export function ask(title: string, subtitle?: string): boolean {
+        controller._setUserEventsEnabled(false);
         game.eventContext(); // initialize the game
         control.pushEventContext();
         game.showDialog(title, subtitle, "A = OK, B = CANCEL");
-        let answer: boolean = null;
         // short pause so that players don't skip through prompt
         pause(500);
 
-        controller.A.onEvent(ControllerButtonEvent.Pressed, () => answer = true);
-        controller.B.onEvent(ControllerButtonEvent.Pressed, () => answer = false);
-        pauseUntil(() => answer !== null);
+        let answer: boolean = null;
+        let aNotHeld = false;
+        let bNotHeld = false;
+        pauseUntil(() => {
+            aNotHeld = aNotHeld || !controller.A.isPressed();
+            bNotHeld = bNotHeld || !controller.B.isPressed();
+
+            if (aNotHeld && controller.A.isPressed()) {
+                answer = true;
+            } else if (bNotHeld && controller.B.isPressed()) {
+                answer = false;
+            }
+            return answer !== null;
+        });
+
         control.popEventContext();
+        controller._setUserEventsEnabled(true);
         return answer;
     }
 }
