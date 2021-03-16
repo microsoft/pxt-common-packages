2feed3d2ad309f3df4af3b57de1df9e318676cc0
James Devine <t-jamdev@microsoft.com>
2021-03-16 18:49:56 +0000

    add protected flash writes/erases for when SD is in use
    Darn soft device requires purview over every little thing...

--- libs/settings/NRF52Flash.cpp
+++ libs/settings/NRF52Flash.cpp
@@ -9,6 +9,30 @@ namespace codal {
 
 #define waitForLast() while ((NRF_NVMC->READY & NVMC_READY_READY_Msk) == 0);
 
+static volatile bool flash_op_complete = false;
+
+#ifdef SOFTDEVICE_PRESENT
+#include "nrf_sdh_soc.h"
+
+static void nvmc_event_handler(uint32_t sys_evt, void *)
+{
+    if (sys_evt == NRF_EVT_FLASH_OPERATION_SUCCESS)
+        flash_op_complete = true;
+}
+
+NRF_SDH_SOC_OBSERVER(nrfflash_soc_observer, 0, nvmc_event_handler, NULL);
+
+#endif
+
+
+static inline uint32_t NRF_PAGE_SIZE() {
+#if defined(FICR_INFO_CODEPAGESIZE_CODEPAGESIZE_Msk)
+    return NRF_FICR->INFO.CODEPAGESIZE;
+#else
+    return NRF_FICR->CODEPAGESIZE;
+#endif
+}
+
 int ZFlash::pageSize(uintptr_t address) {
     (void)address;
 #if defined(FICR_INFO_CODEPAGESIZE_CODEPAGESIZE_Msk)
@@ -30,13 +54,31 @@ int ZFlash::erasePage(uintptr_t address) {
     if (address & (pageSize(address) - 1))
         target_panic(DEVICE_FLASH_ERROR);
 
-    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Een;
-    waitForLast();
-    NRF_NVMC->ERASEPAGE = address;
-    waitForLast();
-    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren;
-    waitForLast();
+#ifdef SOFTDEVICE_PRESENT
+    if (ble_running())
+    {
+        flash_op_complete = false;
+        while(1)
+        {
+            if ( sd_flash_page_erase(((uint32_t)address)/NRF_PAGE_SIZE()) == NRF_SUCCESS)
+                break;
 
+            system_timer_wait_ms(10);
+        }
+
+        while(!flash_op_complete);
+    }
+    else
+#endif
+    {
+        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Een;
+        waitForLast();
+        NRF_NVMC->ERASEPAGE = address;
+        waitForLast();
+        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren;
+        waitForLast();
+    }
+    
     return 0;
 }
 
@@ -56,21 +98,40 @@ int ZFlash::writeBytes(uintptr_t dst, const void *src, uint32_t len) {
 
     len >>= 2;
 
-    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen;
-    waitForLast();
+#ifdef SOFTDEVICE_PRESENT
+    if (ble_running())
+    {
+        flash_op_complete = false;
 
-    while (len-- > 0) {
-        uint32_t v = *sp++;
-        if (v != 0xffffffff) {
-            *dp++ = v;
-            waitForLast();
-        } else {
-            dp++;
+        while(1)
+        {
+            if (sd_flash_write((uint32_t *)dp, (uint32_t *)sp, len) == NRF_SUCCESS)
+                break;
+
+            system_timer_wait_ms(10);
         }
+
+        while(!flash_op_complete);
     }
-    
-    NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren;
-    waitForLast();
+    else
+#endif
+    {
+        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Wen;
+        waitForLast();
+
+        while (len-- > 0) {
+            uint32_t v = *sp++;
+            if (v != 0xffffffff) {
+                *dp++ = v;
+                waitForLast();
+            } else {
+                dp++;
+            }
+        }
+        
+        NRF_NVMC->CONFIG = NVMC_CONFIG_WEN_Ren;
+        waitForLast();
+    }
 
     LOG("WR flash OK");
 
