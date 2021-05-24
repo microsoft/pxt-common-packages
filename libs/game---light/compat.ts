b4ae135eb632c50dbf68922de70bf459a735b3e8
Michal Moskal <michal@moskal.me>
2021-05-24 12:22:49 -0700

    Align handler priorites with game.ts

--- libs/game---light/compat.ts
+++ libs/game---light/compat.ts
@@ -8,8 +8,11 @@ namespace scene {
     export const SCREEN_CLEAR_PRIORITY = 1;
     export const UPDATE_INTERVAL_PRIORITY = 19;
     export const UPDATE_PRIORITY = 20;
-    export const ON_PAINT_PRIORITY = 100;
-    export const ON_SHADE_PRIORITY = 110;
+
+    export const PRE_RENDER_UPDATE_PRIORITY = 55;
+    export const RENDER_BACKGROUND_PRIORITY = 60;
+    export const RENDER_SPRITES_PRIORITY = 90;
+    export const RENDER_DIAGNOSTICS_PRIORITY = 150;
     export const UPDATE_SCREEN_PRIORITY = 200;
 }
 
@@ -30,7 +33,7 @@ namespace game {
 
     export function pushScene() {
         const ctx = control.pushEventContext()
-        ctx.registerFrameHandler(scene.SCREEN_CLEAR_PRIORITY, () => {
+        ctx.registerFrameHandler(scene.RENDER_BACKGROUND_PRIORITY, () => {
             screen.fill(0)
         });
         ctx.registerFrameHandler(scene.UPDATE_SCREEN_PRIORITY, control.__screen.update);
@@ -43,7 +46,7 @@ namespace game {
     export function eventContext() {
         if (!inited) {
             inited = true
-            pushEventContext()
+            pushScene()
         }
         return control.eventContext()
     }
@@ -61,7 +64,7 @@ namespace game {
     //% help=game/paint weight=10 afterOnStart=true
     export function onPaint(a: () => void): void {
         if (!a) return;
-        control.eventContext().registerFrameHandler(scene.ON_PAINT_PRIORITY, a);
+        control.eventContext().registerFrameHandler(scene.RENDER_SPRITES_PRIORITY - 1, a);
     }
 
     /**
@@ -72,7 +75,7 @@ namespace game {
     //% help=game/shade weight=10 afterOnStart=true
     export function onShade(a: () => void): void {
         if (!a) return;
-        control.eventContext().registerFrameHandler(scene.ON_SHADE_PRIORITY, a);
+        control.eventContext().registerFrameHandler(scene.RENDER_SPRITES_PRIORITY, a);
     }
 
 }
