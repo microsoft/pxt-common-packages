879534cb709aa7c9f0a2909b07bca207d3d622a1
Daniel Monroy <dmon@microsoft.com>
2021-07-16 14:21:18 -0700

    Check if a sprite was destroyed before registering another obstacle (#1264)
    Co-authored-by: Joey Wunderlich <jwunderl@users.noreply.github.com>

--- libs/game/physics.ts
+++ libs/game/physics.ts
@@ -428,7 +428,9 @@ class ArcadePhysicsEngine extends PhysicsEngine {
                     );
 
                     for (const tile of collidedTiles) {
-                        s.registerObstacle(collisionDirection, tile, tm);
+                        if(!(s.flags & SPRITE_NO_WALL_COLLISION)) {
+                            s.registerObstacle(collisionDirection, tile, tm);
+                        }
                     }
 
                     if (s.flags & sprites.Flag.DestroyOnWall) {
@@ -507,7 +509,9 @@ class ArcadePhysicsEngine extends PhysicsEngine {
                     );
 
                     for (const tile of collidedTiles) {
-                        s.registerObstacle(collisionDirection, tile, tm);
+                        if(!(s.flags & SPRITE_NO_WALL_COLLISION)) {
+                            s.registerObstacle(collisionDirection, tile, tm);
+                        }
                     }
 
                     if (s.flags & sprites.Flag.DestroyOnWall) {
