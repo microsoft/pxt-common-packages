ac5da09cd0e96e1b295110ea7bc167900568f02e
Daniel Monroy <dmon@microsoft.com>
2021-07-16 13:37:03 -0700

    Update libs/game/physics.ts
    Co-authored-by: Joey Wunderlich <jwunderl@users.noreply.github.com>

--- libs/game/physics.ts
+++ libs/game/physics.ts
@@ -428,7 +428,7 @@ class ArcadePhysicsEngine extends PhysicsEngine {
                     );
 
                     for (const tile of collidedTiles) {
-                        if(!(s.flags & sprites.Flag.Destroyed)) {
+                        if(!(s.flags & SPRITE_NO_WALL_COLLISION)) {
                             s.registerObstacle(collisionDirection, tile, tm);
                         }
                     }
