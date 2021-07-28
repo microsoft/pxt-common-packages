1498262a555a553b76cafdccc55caf906fc2d8b7
riven <4771215@qq.com>
2021-07-28 17:46:31 +0800

    init rp2040 build env

---
+++ libs/core---rp2040/platform.h
@@ -0,0 +1,18 @@
+#ifndef __PXT_PLATFORM_H
+#define __PXT_PLATFORM_H
+
+#include "Image.h"
+#include "Timer.h"
+
+#include "RP2040Pin.h"
+#include "RP2040Spi.h"
+#include "RP2040I2C.h"
+#include "RP2040PWM.h"
+
+#define CODAL_PIN RP2040Pin
+#define CODAL_SPI RP2040SPI
+#define CODAL_I2C RP2040I2C
+#define CODAL_TIMER Timer
+
+
+#endif
