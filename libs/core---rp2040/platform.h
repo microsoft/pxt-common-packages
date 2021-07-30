c1998606a5cbf49fba0f52e9d22ffcc4e359e46d
riven <4771215@qq.com>
2021-07-30 16:53:25 +0800

    add pico pin name

--- libs/core---rp2040/platform.h
+++ libs/core---rp2040/platform.h
@@ -33,5 +33,36 @@ typedef uint8_t PinName;
 // XIP range in 0x1000_0000 ~ 0x1100_0000
 #define PXT_IS_READONLY(v) (isTagged(v) || ((uintptr_t)v & 0x10000000))
 
+#define P0 0
+#define P1 1
+#define P2 2
+#define P3 3
+#define P4 4
+#define P5 5
+#define P6 6
+#define P7 7
+#define P8 8
+#define P9 9
+#define P10 10
+#define P11 11
+#define P12 12
+#define P13 13
+#define P14 14
+#define P15 15
+#define P16 16
+#define P17 17
+#define P18 18
+#define P19 19
+#define P20 20
+#define P21 21
+#define P22 22
+#define P23 23
+#define P24 24
+#define P25 25
+#define P26 26
+#define P27 27
+#define P28 28
+#define P29 29
+#define P30 30
 
 #endif
