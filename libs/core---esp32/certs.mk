ca614d65013f6f39b77d6e0a6c70f278f66ef19f
Michal Moskal <michal@moskal.me>
2021-08-03 16:43:28 -0700

    Update certificate source

--- libs/core---esp32/certs.mk
+++ libs/core---esp32/certs.mk
@@ -1,3 +1,3 @@
 certs/ca-bundle.pem:
 	mkdir -p certs
-	curl https://raw.githubusercontent.com/microsoft/pxt-common-packages/cc0fe4a5985eb92cfd153d31ed88d0721895b001/libs/wifi---esp32/ca-bundle.crt > $@
+	curl https://raw.githubusercontent.com/microsoft/pxt-common-packages/9e8fdb8fd6e21c096f1fe2340652473ddab05d7e/libs/wifi---esp32/ca-bundle.pem > $@
