8bc11782790127b2c26bc3655b5a2c5d2afe2858
Michał Moskal <michal@moskal.me>
2021-08-31 18:02:49 -0700

    azure iot hub fixes/extensions (#1275)
    * Include pxt-common.d.ts for intellisense
    
    * Re-scan wifi at a higher level
    
    * Add net.urlencode/decode
    
    * More logging; once-handlers for mqtt
    
    * Fix connection status checking
    
    * Fix allocateNotifyEvent usage
    
    * Add control.allocateEventSource()
    
    * Twin and sas token handling
    
    * Add azureiot.patchTwin()
    Logging fixes
    
    * Add onTwinUpdate
    
    * Starting on sim support
    
    * fix message defragmentation
    
    * fix iothub url...
    
    * Ignore wifi secrets in sim
    
    * Support for https reqs in sim
    
    * Only enable wifi on esp32 and esp32-s2 boards

--- libs/azureiot/azureiot.ts
+++ libs/azureiot/azureiot.ts
@@ -1,20 +1,21 @@
 const enum AzureIotEvent {
     Connected = 1,
     Disconnected = 2,
-    Error = 3
+    Error = 3,
+    GotTwinResponse = 100,
 }
 
 namespace azureiot {
     export const SECRETS_KEY = "azureiot"
 
-    export let logPriority = ConsolePriority.Silent;
+    export let logPriority = ConsolePriority.Debug;
 
     type SMap<T> = { [s: string]: T; }
     export type Json = any;
 
     let _mqttClient: mqtt.Client;
     let _messageBusId: number;
-    let _receiveHandler: (msg: Json) => void;
+    let _receiveHandler: (msg: Json, sysProps: SMap<string>) => void;
     let _methodHandlers: SMap<(msg: Json) => Json>;
 
     function log(msg: string) {
@@ -27,19 +28,31 @@ namespace azureiot {
         return _mqttClient;
     }
 
+    function generateSasToken(resourceUri: string, signingKey: string, expiresEpoch: number) {
+        const key = Buffer.fromBase64(signingKey)
+        resourceUri = net.urlencode(resourceUri)
+        const toSign = resourceUri + "\n" + expiresEpoch
+        const sig = net.urlencode(crypto.sha256Hmac(key, Buffer.fromUTF8(toSign)).toBase64())
+        const token = `sr=${resourceUri}&se=${expiresEpoch}&sig=${sig}`
+        return token
+    }
+
     function createMQTTClient() {
-        _messageBusId = control.allocateNotifyEvent(); // TODO
+        _messageBusId = control.allocateEventSource();
 
         const connString = settings.programSecrets.readSecret(SECRETS_KEY, true);
         const connStringParts = parsePropertyBag(connString, ";");
         const iotHubHostName = connStringParts["HostName"];
-        const deviceId = connStringParts["DeviceName"];
-        const sasToken = connStringParts["SharedAccessSignature"];
+        const deviceId = connStringParts["DeviceId"];
+        let sasToken = connStringParts["SharedAccessSignature"];
+        if (!sasToken)
+            // token valid until year 2255; in future we may try something more short-lived
+            sasToken = generateSasToken(`${iotHubHostName}/devices/${deviceId}`, connStringParts["SharedAccessKey"], 9000000000)
 
         const opts: mqtt.IConnectionOptions = {
             host: iotHubHostName,
             /* port: 8883, overriden based on platform */
-            username: `${iotHubHostName}/${deviceId}/api-version=2018-06-30`,
+            username: `${iotHubHostName}/${deviceId}/?api-version=2018-06-30`,
             password: "SharedAccessSignature " + sasToken,
             clientId: deviceId
         }
@@ -71,25 +84,20 @@ namespace azureiot {
             return [kv.slice(0, i), kv.slice(i + 1)];
     }
 
-    function parsePropertyBag(msg: string, separator?: string): any {
-        let r: any = {};
+    function parsePropertyBag(msg: string, separator?: string): SMap<string> {
+        let r: SMap<string> = {};
         msg.split(separator || "&")
             .map(kv => splitPair(kv))
-            .forEach(parts => r[parts[0]] = parts[1]);
+            .forEach(parts => r[net.urldecode(parts[0])] = net.urldecode(parts[1]));
         return r;
     }
 
-    function decodeQuery(msg: string, separator?: string): any {
-        const r = parsePropertyBag(msg, separator);
-        // TODO uridecode
-        Object.keys(r).forEach(k => r[k] = JSON.parse(r[k]));
-        return r;
-    }
-
-    function encodeQuery(props: any): string {
-        // TODO uriencode
-        return Object.keys(props)
-            .map(k => `${k}=${JSON.stringify(props[k])}`)
+    function encodeQuery(props: SMap<string>): string {
+        const keys = Object.keys(props)
+        if (keys.length == 0)
+            return ""
+        return "?" + keys
+            .map(k => `${net.urlencode(k)}=${net.urlencode(props[k])}`)
             .join('&');
     }
 
@@ -129,7 +137,7 @@ namespace azureiot {
      * @param msg 
      */
     //%
-    export function publishMessage(msg: any, sysProps?: any) {
+    export function publishMessageJSON(msg: Json, sysProps?: SMap<string>) {
         const c = mqttClient();
         let topic = `devices/${c.opt.clientId}/messages/events/`;
         if (sysProps)
@@ -143,16 +151,13 @@ namespace azureiot {
      * @param handler 
      */
     //%
-    export function onMessageReceived(handler: (msg: any) => void) {
+    export function onMessageReceived(handler: (body: Json, sysProps: SMap<string>) => void) {
         const c = mqttClient();
         if (!_receiveHandler) {
             c.subscribe(`devices/${c.opt.clientId}/messages/devicebound/#`, handleDeviceBound);
 
             /*
-            c.subscribe(`devices/${c.opt.clientId}/messages/events/#`);
             c.subscribe('$iothub/twin/PATCH/properties/desired/#')
-            c.subscribe("$iothub/twin/res/#")
-            c.publish("$iothub/twin/GET/?$rid=foobar")
             */
         }
         _receiveHandler = handler;
@@ -168,8 +173,8 @@ namespace azureiot {
     function handleDeviceBound(packet: mqtt.IMessage) {
         if (!_receiveHandler) return; // nobody's listening
         // TODO this needs some testing
-        const props = parseTopicArgs(packet.topic)
-        _receiveHandler(props);
+        const sysProps = parseTopicArgs(packet.topic)
+        _receiveHandler(JSON.parse(packet.content.toString()), sysProps);
     }
 
     function handleMethod(msg: mqtt.IMessage) {
@@ -189,13 +194,107 @@ namespace azureiot {
                 resp = resp2
             if (resp["_status"] != null) {
                 status = resp["_status"]
-                resp["_status"] = null
+                resp["_status"] = undefined
             }
             log("method: '" + methodName + "' status=" + status)
         }
 
         const c = mqttClient();
         c.publish('$iothub/methods/res/' + status + "/?$rid=" + props["$rid"], JSON.stringify(resp))
+    }
+
+    let twinRespHandlers: SMap<(status: number, body: any) => void>
+
+    // $iothub/twin/res/{status}/?$rid={request id}
+    function twinResponse(msg: mqtt.IMessage) {
+        const args = parseTopicArgs(msg.topic)
+        const h = twinRespHandlers[args["$rid"]]
+        const status = parseInt(msg.topic.slice(17))
+        // log(`twin resp: ${status} ${msg.content.toHex()} ${msg.content.toString()}`)
+        if (h)
+            h(status, JSON.parse(msg.content.toString() || "{}"))
+    }
+
+    export class ValueAwaiter {
+        private evid: number
+        private value: any
+        constructor() {
+            this.evid = control.allocateNotifyEvent()
+        }
+        setValue(v: any) {
+            this.value = v
+            control.raiseEvent(DAL.DEVICE_ID_NOTIFY, this.evid)
+            this.evid = -1
+        }
+        wait() {
+            if (this.evid < 0) return this.value
+            control.waitForEvent(DAL.DEVICE_ID_NOTIFY, this.evid)
+            return this.value
+        }
+    }
+
+    function twinReq(path: string, msg?: string): Json {
+        const c = mqttClient();
+        if (!twinRespHandlers) {
+            twinRespHandlers = {}
+            c.subscribe("$iothub/twin/res/#", twinResponse)
+        }
+        const rid = Math.randomRange(100000000, 900000000) + ""
+        const va = new ValueAwaiter()
+        twinRespHandlers[rid] = (status, body) => {
+            if (status == 204 || status == 200) {
+                va.setValue(body)
+            } else {
+                log(`error on get twin -> ${status} ${JSON.stringify(body)}`)
+                va.setValue(null)
+            }
+        }
+        c.publish(`$iothub/twin/${path}/?$rid=${rid}`, msg)
+        return va.wait()
+    }
+
+    export function getTwin(): Json {
+        return twinReq("GET")
+    }
+
+    export function patchTwin(patch: Json) {
+        twinReq("PATCH/properties/reported", JSON.stringify(patch))
+    }
+
+    function updateJson(trg: Json, patch: Json) {
+        for (const k of Object.keys(patch)) {
+            const v = patch[k]
+            if (v === null) {
+                delete trg[k]
+            } else if (typeof v == "object") {
+                if (!trg[k]) trg[k] = {}
+                updateJson(trg[k], v)
+            } else {
+                trg[k] = v
+            }
+        }
+    }
+
+    export function onTwinUpdate(handler: (twin: Json, patch: Json) => void) {
+        const c = mqttClient()
+        let currTwin: Json = null
+        let lastVersion: number
+        c.subscribe("$iothub/twin/PATCH/properties/desired/#", msg => {
+            if (!currTwin)
+                return
+            const sysProps = parseTopicArgs(msg.topic)
+            const ver = parseInt(sysProps["$version"])
+            if (ver <= lastVersion) {
+                log(`skipping twin update: ${ver}`)
+                return
+            }
+            const update = JSON.parse(msg.content.toString())
+            updateJson(currTwin["desired"], update)
+            handler(currTwin, update)
+        })
+        currTwin = getTwin()
+        lastVersion = currTwin["desired"]["$version"]
+        handler(currTwin, currTwin["desired"])
     }
 
     export function onMethod(methodName: string, handler: (msg: Json) => Json) {
