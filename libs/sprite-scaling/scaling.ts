6da4c87350c293c73a844a4571150687b1a46f61
Eric Anderson <eanders@microsoft.com>
2022-02-03 16:07:52 -0800

    Tweak scale extension block definitions (#1319)
    * tweak scale extension block definitions
    
    * darken the color a little

--- libs/sprite-scaling/scaling.ts
+++ libs/sprite-scaling/scaling.ts
@@ -1,14 +1,15 @@
-//% color="#7B2CBF" weight=99 icon="\u2195" block="Sprite Scaling"
+//% color="#95078E" weight=99 icon="\u2195" block="Sprite Scaling"
 namespace scaling {
-    //% blockId=sprite_set_scale_ex
-    //% block="set $sprite=variables_get(mySprite) scale to $value || $direction anchor $anchor"
+    //% blockId=sprite_scale_to_percent_ex
+    //% block="set $sprite=variables_get(mySprite) scale to $value percent $direction anchor $anchor"
     //% expandableArgumentMode=enabled
     //% inlineInputMode=inline
-    //% value.defl=1
+    //% value.defl=150
     //% direction.defl=ScaleDirection.Uniformly
     //% anchor.defl=ScaleAnchor.Middle
     //% help=sprites/sprite-scaling/scale
-    export function setScale(sprite: Sprite, value: number, direction?: ScaleDirection, anchor?: ScaleAnchor): void {
+    export function scaleToPercent(sprite: Sprite, value: number, direction?: ScaleDirection, anchor?: ScaleAnchor): void {
+        value /= 100;
         direction = direction || ScaleDirection.Uniformly;
         anchor = anchor || ScaleAnchor.Middle;
 
@@ -21,50 +22,38 @@ namespace scaling {
         sprite.setScaleCore(sx, sy, anchor);
     }
 
-    //% blockId=sprite_grow_by_percent_ex
-    //% block="grow $sprite=variables_get(mySprite) by $amount percent || $direction anchor $anchor"
+    //% blockId=sprite_scale_by_percent_ex
+    //% block="change $sprite=variables_get(mySprite) scale by $value percent $direction anchor $anchor"
     //% expandableArgumentMode=enabled
     //% inlineInputMode=inline
-    //% amount.defl=10
+    //% value.defl=50
     //% direction.defl=ScaleDirection.Uniformly
     //% anchor.defl=ScaleAnchor.Middle
     //% help=sprites/sprite-scaling/scale
-    export function growByPercent(sprite: Sprite, amount: number, direction?: ScaleDirection, anchor?: ScaleAnchor): void {
-        amount /= 100;
+    export function scaleByPercent(sprite: Sprite, value: number, direction?: ScaleDirection, anchor?: ScaleAnchor): void {
+        value /= 100;
         direction = direction || ScaleDirection.Uniformly;
         anchor = anchor || ScaleAnchor.Middle;
 
         let sx: number;
         let sy: number;
 
-        if (direction & ScaleDirection.Horizontally) sx = sprite.sx + amount;
-        if (direction & ScaleDirection.Vertically) sy = sprite.sy + amount;
+        if (direction & ScaleDirection.Horizontally) sx = sprite.sx + value;
+        if (direction & ScaleDirection.Vertically) sy = sprite.sy + value;
 
         sprite.setScaleCore(sx, sy, anchor);
     }
 
-    //% blockId=sprite_shrink_by_percent_ex
-    //% block="shrink $sprite=variables_get(mySprite) by $amount percent || $direction anchor $anchor"
+    //% blockId=sprite_scale_to_pixels_ex
+    //% block="set $sprite=variables_get(mySprite) scale to $value pixels $direction anchor $anchor || proportional $proportional"
     //% expandableArgumentMode=enabled
     //% inlineInputMode=inline
-    //% amount.defl=10
-    //% direction.defl=ScaleDirection.Uniformly
-    //% anchor.defl=ScaleAnchor.Middle
-    //% help=sprites/sprite-scaling/scale
-    export function shrinkByPercent(sprite: Sprite, amount: number, direction?: ScaleDirection, anchor?: ScaleAnchor): void {
-        growByPercent(sprite, -amount, direction, anchor);
-    }
-
-    //% blockId=sprite_grow_by_pixels_ex
-    //% block="grow $sprite=variables_get(mySprite) by $amount pixels $direction || anchor $anchor proportional $proportional"
-    //% expandableArgumentMode=enabled
-    //% inlineInputMode=inline
-    //% amount.defl=10
+    //% value.defl=32
     //% direction.defl=ScaleDirection.Horizontally
     //% anchor.defl=ScaleAnchor.Middle
     //% proportional.defl=0
     //% help=sprites/sprite-scaling/scale
-    export function growByPixels(sprite: Sprite, amount: number, direction?: ScaleDirection, anchor?: ScaleAnchor, proportional?: boolean): void {
+    export function scaleToPixels(sprite: Sprite, value: number, direction?: ScaleDirection, anchor?: ScaleAnchor, proportional?: boolean): void {
         direction = direction || ScaleDirection.Horizontally;
         anchor = anchor || ScaleAnchor.Middle;
 
@@ -75,29 +64,49 @@ namespace scaling {
 
         if (direction & ScaleDirection.Horizontally) {
             const imgW = sprite.image.width;
-            const newW = sprite.width + amount;
+            const newW = value;
             sx = newW / imgW;
         }
 
         if (direction & ScaleDirection.Vertically) {
             const imgH = sprite.image.height;
-            const newH = sprite.height + amount;
+            const newH = value;
             sy = newH / imgH;
         }
 
         sprite.setScaleCore(sx, sy, anchor, proportional);
     }
 
-    //% blockId=sprite_shrink_by_pixels_ex
-    //% block="shrink $sprite=variables_get(mySprite) by $amount pixels $direction || anchor $anchor proportional $proportional"
+    //% blockId=sprite_scale_by_pixels_ex
+    //% block="change $sprite=variables_get(mySprite) scale by $value pixels $direction anchor $anchor || proportional $proportional"
     //% expandableArgumentMode=enabled
     //% inlineInputMode=inline
-    //% amount.defl=10
+    //% value.defl=10
     //% direction.defl=ScaleDirection.Horizontally
     //% anchor.defl=ScaleAnchor.Middle
     //% proportional.defl=0
     //% help=sprites/sprite-scaling/scale
-    export function shrinkByPixels(sprite: Sprite, amount: number, direction?: ScaleDirection, anchor?: ScaleAnchor, proportional?: boolean): void {
-        growByPixels(sprite, -amount, direction, anchor, proportional);
+    export function scaleByPixels(sprite: Sprite, value: number, direction?: ScaleDirection, anchor?: ScaleAnchor, proportional?: boolean): void {
+        direction = direction || ScaleDirection.Horizontally;
+        anchor = anchor || ScaleAnchor.Middle;
+
+        if (proportional == null) proportional = direction === ScaleDirection.Uniformly;
+
+        let sx: number;
+        let sy: number;
+
+        if (direction & ScaleDirection.Horizontally) {
+            const imgW = sprite.image.width;
+            const newW = sprite.width + value;
+            sx = newW / imgW;
+        }
+
+        if (direction & ScaleDirection.Vertically) {
+            const imgH = sprite.image.height;
+            const newH = sprite.height + value;
+            sy = newH / imgH;
+        }
+
+        sprite.setScaleCore(sx, sy, anchor, proportional);
     }
 }
