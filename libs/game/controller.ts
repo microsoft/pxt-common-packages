9f931c274aebcbd05ce1ef95b793a7d26f856030
Joey Wunderlich <jwunderl@users.noreply.github.com>
2023-01-27 13:02:57 -0800

    raise events for dicsonnected/connected on scene pop (#1409)

--- libs/game/controller.ts
+++ libs/game/controller.ts
@@ -13,6 +13,30 @@ enum ControllerEvent {
 //% blockGap=8
 namespace controller {
     let _players: Controller[];
+    game.addScenePopHandler(() => {
+        const stateWhenPushed = game.currentScene().controllerConnectionState;
+        if (!stateWhenPushed)
+            return;
+        for (let i = 0; i < stateWhenPushed.length; i++) {
+            const p = _players[i];
+            if (p && (!!stateWhenPushed[i] != !!p.connected)) {
+                // connection state changed while in another scene; raise the event.
+                control.raiseEvent(
+                    p.id,
+                    p.connected ? ControllerEvent.Connected : ControllerEvent.Disconnected
+                );
+            }
+        }
+
+    })
+    game.addScenePushHandler(oldScene => {
+        oldScene.controllerConnectionState = [];
+        for (let i = 0; i < _players.length; i++) {
+            if (_players[i]) {
+                oldScene.controllerConnectionState[i] = _players[i].connected;
+            }
+        }
+    })
 
     function addController(ctrl: Controller) {
         if (!_players) {
