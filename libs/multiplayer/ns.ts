35713a6ac4efb4de3c71ffc87a6ae71dd1f96843
Eric Anderson <eanders@microsoft.com>
2023-01-30 14:59:35 -0800

    add "array of all players" block (#1416) (#1420)
    * add "array of all players" block
    
    * rename
    
    * Update libs/multiplayer/player.ts
    
    
    
    ---------
    
    Co-authored-by: Joey Wunderlich <jwunderl@users.noreply.github.com>

--- libs/multiplayer/ns.ts
+++ libs/multiplayer/ns.ts
@@ -1,6 +1,6 @@
 //% color="#207a77" weight=96 icon="\uf0c0"
 //% blockGap=8
 //% block="Multiplayer"
-//% groups='["Sprites", "Controller", "Info", "Game", "Utility"]'
+//% groups='["Player", "Controller", "Info", "Game", "Utility"]'
 namespace mp {
 }
