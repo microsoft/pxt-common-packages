b8c49550a3c2e5dc11ddcfa4cf6a16980b64aa2e
ganicke <gnickel@aquent.com>
2023-02-13 14:27:12 -0800

    fixup help paths

--- libs/mixer/melody.ts
+++ libs/mixer/melody.ts
@@ -598,6 +598,7 @@ namespace music {
     //% toolboxParentArgument=toPlay
     //% group="Songs"
     //% duplicateShadowOnDrag
+    //% help=music/create-song
     export function createSong(song: Buffer): Playable {
         return new sequencer.Song(song);
     }
