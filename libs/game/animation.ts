ae6e5cc370ab673a42a5f8d2b0c47dadd240f403
Abhijith Chatra <abchatra@microsoft.com>
2023-04-25 16:20:02 -0700

    Update libs/game/animation.ts
    Co-authored-by: Richard Knoll <riknoll@users.noreply.github.com>

--- libs/game/animation.ts
+++ libs/game/animation.ts
@@ -3,7 +3,7 @@
 */
 //% color="#03AA74" weight=100 icon="\uf021" block="Animation"
 //% groups='["Animate", "Advanced"]'
-//% weight = 5
+//% weight=5
 namespace animation {
     const stateNamespace = "__animation";
 
