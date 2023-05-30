de20b9a0d268d563d59a817132df0819a7715f83
Sarah Rietkerk <49178322+srietkerk@users.noreply.github.com>
2023-05-30 15:34:13 -0700

    Microphone board using new microphone state to keep track of level, etc (#1449)

--- libs/microphone/sim/state.ts
+++ libs/microphone/sim/state.ts
@@ -1,6 +1,27 @@
 namespace pxsim {
     export interface MicrophoneBoard {
-        microphoneState: AnalogSensorState;
+        microphoneState: MicrophoneState;
+    }
+
+    export class MicrophoneState extends AnalogSensorState {
+        public onSoundRegistered: boolean = false;
+        public soundLevelRequested: boolean = false;
+        private pingUsed: ReturnType<typeof setTimeout>;
+
+        public pingSoundLevel = () => {
+            if (this.onSoundRegistered) {
+                return;
+            }
+            this.soundLevelRequested = true;
+            runtime.queueDisplayUpdate();
+            clearTimeout(this.pingUsed);
+            this.pingUsed = setTimeout(() => {
+                this.soundLevelRequested = false;
+                runtime.queueDisplayUpdate();
+                this.pingUsed = undefined;
+            }, 100);
+        }
+
     }
 
     export function microphoneState() {
