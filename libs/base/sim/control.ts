8a89f8c76b1f92d5c8f3c72673501be4d8e3c91d
Tom Ball <tballmsft@users.noreply.github.com>
2024-11-12 10:14:54 -0800

    control function to turn off creation of second sim (#1486)
    * add new function
    
    * remove getResume

--- libs/base/sim/control.ts
+++ libs/base/sim/control.ts
@@ -43,6 +43,12 @@ namespace pxsim.control {
         })
         const cb = getResume();
     }
+    export function singleSimulator() {
+        pxsim.Runtime.postMessage(<pxsim.SimulatorCommandMessage>{
+            type: "simulator",
+            command: "single"
+        })
+    }
     export function waitMicros(micros: number) {
         thread.pause(micros / 1000); // it prempts not much we can do here.
     }
