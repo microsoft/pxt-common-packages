f87aff3e396cc30a9969d4cce7782331a8bfa4f8
Richard Knoll <riknoll@users.noreply.github.com>
2025-02-13 15:51:26 -0800

    remove reference to invalid shadow on deprecated block (#1493)

--- libs/game/sprite.ts
+++ libs/game/sprite.ts
@@ -789,7 +789,6 @@ class Sprite extends sprites.BaseSprite {
      */
     //% blockId=spriteobstacle block="%sprite(mySprite) wall hit on %direction"
     //% blockNamespace="scene" group="Locations"
-    //% direction.shadow=tiles_collision_direction_editor
     //% help=sprites/sprite/tile-hit-from
     //% deprecated=1
     tileHitFrom(direction: number): number {
