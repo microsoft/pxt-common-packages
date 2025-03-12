c96dcbc9139c7c715d8ecc997bb7978c97682dee
Tom Ball <tball@microsoft.com>
2025-03-12 11:26:50 -0700

    use new top of flash

--- libs/settings/settings.cpp
+++ libs/settings/settings.cpp
@@ -29,10 +29,7 @@ class WStorage {
 #elif defined(SAMD21)
           fs(flash, 256 * 1024 - SETTINGS_SIZE, SETTINGS_SIZE),
 #elif defined(MICROBIT_CODAL) && MICROBIT_CODAL
-          // micro:bit V2 memory map
-          // https://github.com/lancaster-university/codal-microbit-v2/blob/master/docs/MemoryMap.md
-          // 73000	CODAL scratch page (is used as temporary scratch by MicroBitFlash, MicroBitFileSystem and MicroBitStorage)
-          fs(flash, 0x73000 - size, size),
+          fs(flash, MICROBIT_TOP_OF_FLASH - size, size),
 #elif defined(NRF52_SERIES)
 #define NRF_BOOTLOADER_START *(uint32_t *)0x10001014
           fs(flash,
