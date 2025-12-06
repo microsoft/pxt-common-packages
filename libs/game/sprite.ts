c9f1b6d48a35c3f1610e87a332a150ef1e5dcc5d
Sarah Rietkerk <sarahnrietkerk@gmail.com>
2025-12-06 01:13:04 +0000

    make mysprite the default for snippets that specify using mysprite

--- libs/game/sprite.ts
+++ libs/game/sprite.ts
@@ -1015,6 +1015,7 @@ class Sprite extends sprites.BaseSprite {
     //% group="Physics" weight=10
     //% blockId=spriteFollowOtherSprite
     //% block="set %sprite(myEnemy) follow %target=variables_get(mySprite) || with speed %speed"
+    //% target.defl=mySprite
     //% help=sprites/sprite/follow
     follow(target: Sprite, speed = 100, turnRate = 400) {
         if (target === this) return;
